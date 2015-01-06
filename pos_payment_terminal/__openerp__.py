719610307904c5c8ecf2df4f4d0eabdb3bae35b3
Alexis de Lattre <alexis@via.ecp.fr>
2015-01-06 23:26:30 +0100

    PEP8

--- pos_payment_terminal/__openerp__.py
+++ pos_payment_terminal/__openerp__.py
@@ -29,11 +29,22 @@
 POS Payment Terminal
 ====================
 
-This module adds support for Payment Terminal in the Point of Sale. This module is designed to be installed on the *main Odoo server*. On the *POSbox*, you should install the module  *hw_x* depending on the protocol implemented in your device. Ingenico and Sagem devices support the Telium protocol implemented in the *hw_telium_payment_terminal* module.
+This module adds support for credit card reader and checks printer
+in the Point of Sale. This module is designed to be installed on the
+*main Odoo server*. On the *POSbox*, you should install the module
+*hw_x* depending on the protocol implemented in your device. Ingenico
+and Sagem devices support the Telium protocol implemented in the
+*hw_telium_payment_terminal* module.
 
-This module support two payment methods : cards and checks. The payment method should be configured on the main Odoo server, in the menu Point of Sale > Configuration > Payment Methods.
+This module support two payment methods : cards and checks. The payment
+method should be configured on the main Odoo server, in the menu Point
+of Sale > Configuration > Payment Methods.
 
-This module has been developped during a POS code sprint at Akretion France from July 7th to July 10th 2014. This module is part of the POS project of the Odoo Community Association http://odoo-community.org/. You are invited to become a member and/or get involved in the Association !
+This module has been developped during a POS code sprint at Akretion
+France from July 7th to July 10th 2014. This module is part of the POS
+project of the Odoo Community Association http://odoo-community.org/.
+You are invited to become a member and/or get involved in the
+Association !
     """,
     'author': 'Aurélien DUMAINE',
     'depends': ['point_of_sale'],
