d3da132e02843da2efc19f840126cf05619532b5
Alexis de Lattre <alexis@via.ecp.fr>
2015-01-06 23:26:30 +0100

    PEP8

--- hw_customer_display/__openerp__.py
+++ hw_customer_display/__openerp__.py
@@ -31,29 +31,49 @@
 Hardware Customer Display
 =========================
 
-This module adds support for Customer Display in the Point of Sale. This module is designed to be installed on the *POSbox* (i.e. the proxy on which the USB devices are connected) and not on the main Odoo server. On the main Odoo server, you should install the module *pos_customer_display*.
+This module adds support for Customer Display in the Point of Sale.
+This module is designed to be installed on the *POSbox* (i.e. the
+proxy on which the USB devices are connected) and not on the main
+Odoo server. On the main Odoo server, you should install the module
+*pos_customer_display*.
 
-The configuration of the hardware is done in the configuration file of the Odoo server of the POSbox. You should add the following entries in the configuration file:
+The configuration of the hardware is done in the configuration file of
+the Odoo server of the POSbox. You should add the following entries in
+the configuration file:
 
 * customer_display_device_name (default = /dev/ttyUSB0)
 * customer_display_device_rate (default = 9600)
 * customer_display_device_timeout (default = 2 seconds)
 
-The number of cols of the Customer Display (usually 20) should be configured on the main Odoo server, in the menu Point of Sale > Configuration > Point of Sales. The number of rows is supposed to be 2.
+The number of cols of the Customer Display (usually 20) should be
+configured on the main Odoo server, in the menu Point of Sale >
+Configuration > Point of Sales. The number of rows is supposed to be 2.
 
-It should support most serial and USB-serial LCD displays out-of-the-box or with inheritance of a few functions.
+It should support most serial and USB-serial LCD displays out-of-the-box
+or with inheritance of a few functions.
 
 It has been tested with:
 
-* Bixolon BCD-1100 (Datasheet : http://www.bixolon.com/html/en/product/product_detail.xhtml?prod_id=61)
+* Bixolon BCD-1100 (Datasheet :
+  http://www.bixolon.com/html/en/product/product_detail.xhtml?prod_id=61)
 * Bixolon BCD-1000
 
-To setup the BCD-1100 on Linux, you will find some technical instructions on this page : http://techtuxwords.blogspot.fr/2012/12/linux-and-bixolon-bcd-1100.html
-If you have a kernel >= 3.12, you should also read this : http://www.leniwiec.org/en/2014/06/25/ubuntu-14-04lts-how-to-pass-id-vendor-and-id-product-to-ftdi_sio-driver/
+To setup the BCD-1100 on Linux, you will find some technical instructions
+on this page:
+http://techtuxwords.blogspot.fr/2012/12/linux-and-bixolon-bcd-1100.html
 
-This module has been developped during a POS code sprint at Akretion France from July 7th to July 10th 2014. This module is part of the POS project of the Odoo Community Association http://odoo-community.org/. You are invited to become a member and/or get involved in the Association !
+If you have a kernel >= 3.12, you should also read this:
+http://www.leniwiec.org/en/2014/06/25/ubuntu-14-04lts-how-to-pass-id-ven
+dor-and-id-product-to-ftdi_sio-driver/
 
-Please contact Alexis de Lattre from Akretion <alexis.delattre@akretion.com> for any help or question about this module.
+This module has been developped during a POS code sprint at Akretion
+France from July 7th to July 10th 2014. This module is part of the POS
+project of the Odoo Community Association http://odoo-community.org/.
+You are invited to become a member and/or get involved in the
+Association !
+
+This module has been written by Alexis de Lattre from Akretion
+<alexis.delattre@akretion.com>.
     """,
     'author': 'Akretion',
     'website': 'http://www.akretion.com',
--- pos_customer_display/__openerp__.py
+++ pos_customer_display/__openerp__.py
@@ -29,18 +29,29 @@
 POS Customer Display
 ====================
 
-This module adds support for Customer Display in the Point of Sale. This module is designed to be installed on the *main Odoo server*. On the *POSbox*, you should install the module *hw_customer_display*.
+This module adds support for Customer Display in the Point of Sale. This
+module is designed to be installed on the *main Odoo server*. On the
+*POSbox*, you should install the module *hw_customer_display*.
 
-The number of rows and cols of the Customer Display (usually 2 x 20) should be configured on the main Odoo server, in the menu Point of Sale > Configuration > Point of Sales.
+The number of rows and cols of the Customer Display (usually 2 x 20)
+should be configured on the main Odoo server, in the menu Point of Sale
+> Configuration > Point of Sales.
 
-It has been tested with a Bixolon BCD-1100 (http://www.bixolon.com/html/en/product/product_detail.xhtml?prod_id=61), but should support most serial and USB-serial LCD displays out-of-the-box, cf the module *hw_customer_display* for more info.
+It has been tested with a Bixolon BCD-1100
+(http://www.bixolon.com/html/en/product/product_detail.xhtml?prod_id=61),
+but should support most serial and USB-serial LCD displays
+out-of-the-box, cf the module *hw_customer_display* for more info.
 
-This module has been developped during a POS code sprint at Akretion France from July 7th to July 10th 2014. This module is part of the POS project of the Odoo Community Association http://odoo-community.org/. You are invited to become a member and/or get involved in the Association !
+This module has been developped during a POS code sprint at Akretion
+France from July 7th to July 10th 2014. This module is part of the POS
+project of the Odoo Community Association http://odoo-community.org/.
+You are invited to become a member and/or get involved in the
+Association !
     """,
     'author': 'Aurélien DUMAINE',
     'depends': ['point_of_sale'],
-    'data' : [
-            'pos_customer_display.xml',
-            'customer_display_view.xml',
-            ],
+    'data': [
+        'pos_customer_display.xml',
+        'customer_display_view.xml',
+        ],
 }
