208a5d0351c9e00738df15914553cc073c984387
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-01-28 10:22:44 +0100

    [IMP] description of the module in openerp.py file;

--- pos_product_template/__openerp__.py
+++ pos_product_template/__openerp__.py
@@ -30,17 +30,24 @@ Manage Product Template in Front End Point Of Sale
 
 Functionality:
 --------------
-    *
+    * In Point Of Sale Front End - Products list:
+        * Display only one product per template;
+        * Display template name instead of product name;
+        * Display products quantity instead of price;
+        * Click on template displays an extra screen to select Variant;
 
-Possible improvements and fix:
-------------------------------
-    * Display product template name in ProductList;
-    * Display attributes;
-    * Improve Template display;
+    * In Point Of Sale Front End - Variants list:
+        * Display all the products of the selected variant;
+        * Click on a attribute value filters products;
+        * Click on a product adds it to the current Order or display normal
+          extra screen if it is a weightable product;
 
-NON Covered features:
----------------------
-    *
+Technical Information:
+----------------------
+    * Load extra model in Point Of Sale Front End:
+        * product.template;
+        * product.attribute;
+        * product.attribute.value;
 
 Copyright, Authors and Licence:
 -------------------------------
