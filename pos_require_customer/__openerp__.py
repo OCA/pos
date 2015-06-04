363547cc0363c0158c8af6be28687a4006d4803b
Jos De Graeve <Jos.DeGraeve@apertoso.be>
2015-06-04 22:04:03 +0200

    Added README, moved model and view to their proper location

--- pos_require_customer/__openerp__.py
+++ pos_require_customer/__openerp__.py
@@ -41,7 +41,7 @@ In the backend the customer field is required when needed.
     'depends': ['point_of_sale'],
     'data': [
         'static/src/xml/templates.xml',
-        'pos_view.xml',
+        'views/pos_view.xml',
     ],
     'demo': [],
     'test': [],
