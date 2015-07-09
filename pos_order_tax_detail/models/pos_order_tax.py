f418d88c2172c5be3bb38970b7595933c63b46ff
Antonio Espinosa <antespi@gmail.com>
2015-07-09 17:31:33 +0200

    [FIX] Travis warning

--- pos_order_tax_detail/models/pos_order_tax.py
+++ pos_order_tax_detail/models/pos_order_tax.py
@@ -22,7 +22,7 @@
 #
 ##############################################################################
 
-from openerp import models, fields, api
+from openerp import models, fields
 from openerp.addons import decimal_precision as dp
 
 
