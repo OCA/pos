1cf3f8750dff9f516d775d5abb5571b864a9b84d
Andrius Preimantas <andrius@versada.lt>
2015-09-24 09:42:56 +0300

    [IMP][pos_trade_receivable_autoreconcile] .py file header update

--- pos_trade_receivable_autoreconcile/model/__init__.py
+++ pos_trade_receivable_autoreconcile/model/__init__.py
@@ -1,5 +1,23 @@
-# -*- coding: utf-8 -*-
-# This file is part of OpenERP. The COPYRIGHT file at the top level of
-# this module contains the full copyright notices and license terms.
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2015 UAB Versada
+#    (<http://www.versada.lt>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 from . import point_of_sale
