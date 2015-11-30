44bbd3b7094ad23b39441ecbd6af33c85623b954
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2015-11-30 13:48:39 +0100

    [IMP][pos_analytic_config] Change header

--- pos_analytic_config/__openerp__.py
+++ pos_analytic_config/__openerp__.py
@@ -1,27 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#     This file is part of pos_analytic_config,
-#     an Odoo module.
-#
-#     Copyright (c) 2015 ACSONE SA/NV (<http://acsone.eu>)
-#
-#     pos_analytic_config is free software:
-#     you can redistribute it and/or modify it under the terms of the GNU
-#     Affero General Public License as published by the Free Software
-#     Foundation,either version 3 of the License, or (at your option) any
-#     later version.
-#
-#     pos_analytic_config is distributed
-#     in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-#     even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-#     PURPOSE.  See the GNU Affero General Public License for more details.
-#
-#     You should have received a copy of the GNU Affero General Public License
-#     along with pos_analytic_config.
-#     If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': "POS Analytic Config",
 
