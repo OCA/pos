86e1d302720ef7b10cf639d3a110880d092dcecc
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-01-20 11:47:28 +0100

    [ADD] manage require_customer feature for front-office, if require_customer = 'order';

--- pos_require_customer/static/src/js/pos_require_customer.js
+++ pos_require_customer/static/src/js/pos_require_customer.js
@@ -13,14 +13,14 @@ odoo.define('pos_require_customer.pos_require_customer', function (require) {
     "use strict";
 
     var screens = require('point_of_sale.screens');
+    var gui = require('point_of_sale.gui');
     var core = require('web.core');
     var _t = core._t;
 
     screens.PaymentScreenWidget.include({
         validate_order: function(options) {
-            var currentOrder = this.pos.get('selectedOrder');
-            
-            if( this.pos.config.require_customer != 'no' && !currentOrder.get_client()){
+            if(this.pos.config.require_customer != 'no'
+                    && !this.pos.get('selectedOrder').get_client()){
                 this.gui.show_popup('error',{
                     'title': _t('An anonymous order cannot be confirmed'),
                     'body':  _t('Please select a customer for this order.'),
@@ -30,5 +30,29 @@ odoo.define('pos_require_customer.pos_require_customer', function (require) {
             return this._super(options);
         }
     });
+
+    /*
+        Because of clientlist screen behaviour, it is not possible to simply
+        use: set_default_screen('clientlist') + remove cancel button on
+        customer screen.
+
+        Instead of,
+        - we overload the function : show_screen(screen_name,params,refresh),
+        - and we replace the required screen by the 'clientlist' screen if the
+        current PoS Order has no Customer.
+    */
+
+    var _show_screen_ = gui.Gui.prototype.show_screen;
+    gui.Gui.prototype.show_screen = function(screen_name, params, refresh){
+        if(this.pos.config.require_customer == 'order'
+                && !this.pos.get('selectedOrder').get_client()
+                && screen_name != 'clientlist'){
+            // We call first the original screen, to avoid to break the
+            // 'previous screen' mecanism
+            _show_screen_.call(this, screen_name, params, refresh);
+            screen_name = 'clientlist';
+        }
+        _show_screen_.call(this, screen_name, params, refresh);
+    };
 
 });
