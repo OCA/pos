4281cd02fb2a615d706d7e969bdd639ca6890365
raph <raph@hpar.fr>
2016-02-26 10:20:00 +0100

    Limit lines to < 80 chars

--- pos_default_empty_image/__openerp__.py
+++ pos_default_empty_image/__openerp__.py
@@ -10,11 +10,14 @@
 POS Default empty Image
 =======================
 
-In the point of sale, trying to load known inexistant images is a waste of time.
+In the point of sale, trying to load known inexistant images is
+a waste of time.
 
 
-When you have 8000 products in your Point of Sale and most of them don't have images, 
-you are happy to save thousands of useless requests : the POS load way faster.
+When you have 8000 products in your Point of Sale and most of them
+don't have images, you are happy to save thousands of useless requests :
+the POS load way faster.
+
 
 Technical information
 =====================
@@ -22,15 +25,17 @@ Technical information
 Each time the pos instantiate a product, it will add an
 <img src="/web/.... + imageId"> to DOM.
 The browser will trigger as many requests than there is different url.
-If you have many products, the browser will soon reach his limit of network connections to Odoo server and
-will wait for free slots instead of loading other valuable contents. 
+If you have many products, the browser will soon reach his limit of
+network connections to Odoo server and
+will wait for free slots instead of loading other valuable contents.
 The POS is then very slow to work with.
 
 
-This module adds a field _has_image_ in product.template and will change 
+This module adds a field _has_image_ in product.template and will change
 the product image url to his default placeholder directly in the POS.
-Because there is only one url for this placeholder, you will have only one request for all the products with
-no images.
+Because there is only one url for this placeholder, you will have
+only one request for all the products with no images.
+
 
 Indeed, if the product has an image, it will load normally.
 
@@ -39,6 +44,6 @@ Indeed, if the product has an image, it will load normally.
     'website': "https://akretion.com",
     'license': 'AGPL-3',
     'depends': ['point_of_sale'],
-    'data': [ 'view/view.xml'],
+    'data': ['view/view.xml'],
     'qweb': [],
 }
