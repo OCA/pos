b9221e27a3bd88b1331cf3c573b50d831ff2f269
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-03-04 14:56:19 +0100

    [FIX] pos_pricelist: FP taxes with multi-company. Fixes #84
    The object account.fiscal.position.tax doesn't have multi-company rules,
    which leads to error in environments with multi-company and these records,
    because it finally accesses to the parent fiscal position, which on contrary
    has access rules, provoking an access error.
    
    This commit added the field company_id to the model, and add the corresponding
    access rule.

--- pos_pricelist/models/__init__.py
+++ pos_pricelist/models/__init__.py
@@ -1,20 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-# Point Of Sale - Pricelist for POS Odoo
-# Copyright (C) 2015 Taktik (http://www.taktik.be)
-# @author Adil Houmadi <ah@taktik.be>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 Taktik (http://www.taktik.be) - Adil Houmadi <ah@taktik.be>
+# © 2016 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import account_fiscal_position
 from . import pos_pricelist
 from . import point_of_sale
