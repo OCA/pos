2239fc6baac6f28411c4b9712baccab401e98b07
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-11-02 09:43:17 +0100

    [FIX] various remarks. Courtesy pedrobaeza

--- pos_customer_required/__openerp__.py
+++ pos_customer_required/__openerp__.py
@@ -7,7 +7,7 @@
 
 {
     'name': 'Point of Sale Require Customer',
-    'version': '9.0.2.0.0',
+    'version': '9.0.1.0.0',
     'category': 'Point Of Sale',
     'summary': 'Point of Sale Require Customer',
     'author': 'Apertoso NV, La Louve, Odoo Community Association (OCA)',
