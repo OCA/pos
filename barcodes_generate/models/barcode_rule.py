8fee7d50091840731b52cf778109b0c4f7a78246
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-11-05 20:35:10 +0100

    [FIX] flake8

--- barcodes_generate/models/barcode_rule.py
+++ barcodes_generate/models/barcode_rule.py
@@ -4,7 +4,7 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api, _
+from openerp import models, fields, api, _, exceptions
 
 _GENERATE_TYPE = [
     ('no', 'No generation'),
