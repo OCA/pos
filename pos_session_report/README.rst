b848495c8df699b4fdbd5755593122d9b676271e
Hendrix Costa <hendrixcosta@gmail.com>
2016-12-19 18:16:27 -0200

    [FIX] Functions based on account bank statement lines / Fix some questions

--- pos_session_report/README.rst
+++ pos_session_report/README.rst
@@ -2,37 +2,33 @@
    :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
    :alt: License: AGPL-3
 
-==============
+===========================
 Pos Session Report Detailed
-==============
+===========================
 
-This module extends the functionality of Pos Session Sumary
-and to allow you to show detailed informations about pos session
+This module extents the POS Session Summary functionality, showing detailed
+information about the each transaction payments.
+
+This functionality works better when using together with the module
+pos_payment_term, which allows you to sell using various payment terms.
+
+
+.. image:: /pos_session_report/static/description/pos_session_summary_datailed.png
+
 
 Installation
 ============
 
-To install this module, you need to:
-
-#. Do this ...
+Normal installation.
 
 Configuration
 =============
 
-To configure this module, you need to:
+No configuration is needed.
 
-#. Go to ...
-
-.. figure:: path/to/local/image.png
-   :alt: alternative description
-   :width: 600 px
 
 Usage
 =====
-
-To use this module, you need to:
-
-#. Go to ...
 
 .. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
    :alt: Try me on Runbot
@@ -41,10 +37,7 @@ To use this module, you need to:
 .. repo_id is available in https://github.com/OCA/maintainer-tools/blob/master/tools/repos_with_ids.txt
 .. branch is "8.0" for example
 
-Known issues / Roadmap
-======================
 
-* ...
 
 Bug Tracker
 ===========
@@ -52,7 +45,11 @@ Bug Tracker
 Bugs are tracked on `GitHub Issues
 <https://github.com/OCA/{project_repo}/issues>`_. In case of trouble, please
 check there if your issue has already been reported. If you spotted it first,
-help us smashing it by providing a detailed and welcomed feedback.
+help us smashing it by providing a detailed and welcomed `feedback
+<https://github.com/OCA/
+pos/issues/new?body=module:%20
+pos_session_summary%0Aversion:%20
+9.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
 
 Credits
 =======
--- pos_session_report/report/details_pos_session_report.py
+++ pos_session_report/report/details_pos_session_report.py
@@ -9,46 +9,67 @@ from openerp import models
 
 class DetailsPosSessionReport(report_sxw.rml_parse):
 
-    def _get_qtd_payments(self, line):
+    def _get_quantity_payments(self, line):
         if line.amount < 0:
             return ''
+        quantity_payments = 0
+        for statement in line.pos_statement_id.statement_ids:
+            if statement.amount > 0 and statement.statement_id.id == line.\
+                    statement_id.id:
+                quantity_payments += 1
+        return quantity_payments
 
-        for statement_bank_line in line.pos_statement_id.statement_ids:
-            if statement_bank_line.amount < 0:
-                return len(line.pos_statement_id.statement_ids)-1
-        return len(line.pos_statement_id.statement_ids)
+    def _get_lines_from_statements_lines(self, line):
+        stats_ids = []
+        for stats in line.pos_statement_id.statement_ids:
+            if stats.journal_id.id == line.journal_id.id:
+                stats_ids.append(stats.id)
+        return stats_ids
+
+    def _get_total_by_statement_line(self, line):
+        total = 0
+        if line.amount < 0:
+            return line.amount
+        for stats in line.pos_statement_id.statement_ids:
+            if stats.journal_id.id == line.journal_id.id and stats.amount > 0:
+                total += stats.amount
+        return total
 
     def _get_account_bank_statement_lines(self):
         pos_session_obj = self.pool.get('pos.session').browse(
-            self.cr, self.uid,self.ids, context='')
+            self.cr, self.uid, self.ids, context='')
         statements_lines = []
         lines_done = []
 
         for pos_session in pos_session_obj:
             for statement in pos_session.statement_ids:
                 for line in statement.line_ids:
-                    if (not (line.id in lines_done) or line.amount < 0):
-                        result = {
-                            'id': line.id,
-                            'name': line.name,
-                            'date': line.date,
-                            'ref': line.ref,
-                            'amount': line.amount,
-                            'partner_id': line.partner_id,
-                            'journal_id': line.journal_id,
-                            'pos_statement_id': line.pos_statement_id,
-                            'qtd_payments': self._get_qtd_payments(line)
-                        }
-                        statements_lines.append(result)
-                        if line.amount < 0:
-                            lines_done.append(line.id)
-                        else:
-                            lines_done.extend(
-                                line.pos_statement_id.statement_ids.ids
-                            )
+                    if not line.amount == 0:
+                        if (not (line.id in lines_done) or line.amount < 0):
+                            result = {
+                                'id': line.id,
+                                'name': line.name,
+                                'date': line.date,
+                                'ref': line.ref,
+                                'amount':
+                                    self._get_total_by_statement_line(line),
+                                'sub_total': line.amount,
+                                'partner_id': line.partner_id,
+                                'journal_id': line.journal_id,
+                                'pos_statement_id': line.pos_statement_id,
+                                'quantity_payments':
+                                    self._get_quantity_payments(line)
+                            }
+                            statements_lines.append(result)
+                            if line.amount < 0:
+                                lines_done.append(line.id)
+                            else:
+                                lines_done.extend(
+                                    self._get_lines_from_statements_lines(line)
+                                )
         lines_sorted = sorted(
             statements_lines,
-            key=lambda lines_sorted: lines_sorted['qtd_payments']
+            key=lambda lines_sorted: lines_sorted['quantity_payments']
         )
         return lines_sorted
 
