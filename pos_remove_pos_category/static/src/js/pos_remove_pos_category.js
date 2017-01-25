5ca4b9984a6fe073ce66feaf01cf0e3cf0566e6f
Simone Orsi <simahawk@gmail.com>
2017-01-25 18:07:37 +0100

    migrate JS

--- pos_remove_pos_category/static/src/js/pos_remove_pos_category.js
+++ pos_remove_pos_category/static/src/js/pos_remove_pos_category.js
@@ -1,31 +1,38 @@
 /******************************************************************************
- * Copyright (C) 2015-TODAY Akretion (<http://www.akretion.com>).
+ * Copyright (C) 2015-2016 Akretion (<http://www.akretion.com>).
+ * Copyright (C) 2017-TODAY Camptocamp SA (<http://www.camptocamp.com>).
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  ******************************************************************************/
 
-openerp.pos_remove_pos_category = function(instance, local) {
-    module = instance.point_of_sale;
+odoo.define('pos_remove_pos_category.remove_pos_category', function (require) {
+    "use strict";
 
-    var initialize_original = module.PosModel.prototype.initialize;
-    module.PosModel = module.PosModel.extend({
+    var core = require('web.core');
+    var pos_models = require('point_of_sale.models');
+    var pos_screens = require('point_of_sale.screens');
+    var Model = require('web.DataModel');
+    var _t = core._t;
 
+    var _pos_super = pos_models.PosModel.prototype;
+    pos_models.PosModel = pos_models.PosModel.extend({
         initialize: function(session, attributes) {
-
             for (var i = 0 ; i < this.models.length; i++){
                 if (this.models[i].model == 'pos.category') {
                     this.models[i].model = 'product.category';
                     this.models[i].domain = [['available_in_pos', '=', true]];
                 }
             }
-            return initialize_original.call(this, session, attributes);
-        }
-   });
-
-    //override method js POS (widgets.js)
-    //change pos.category by product.category
-    module.ProductCategoriesWidget = module.ProductCategoriesWidget.extend({
-        get_image_url: function(category){
-            return window.location.origin + '/web/binary/image?model=product.category&field=image_medium&id='+category.id;
+            return _pos_super.initialize.apply(this, arguments);
         }
     });
-};
+
+    // override method js POS (widgets.js)
+    // change pos.category by product.category
+    pos_screens.ProductCategoriesWidget.include({
+        get_image_url: function(category){
+            var image_url = '/web/binary/image?model=product.category&field=image_medium&id=';
+            return window.location.origin + image_url + category.id;
+        }
+    });
+
+});
