ae80145a3c5de48d58ca12e0fae652d14caf48b9
Simone Orsi <simahawk@gmail.com>
2017-01-25 16:53:40 +0100

    short headers

--- pos_remove_pos_category/static/src/js/pos_remove_pos_category.js
+++ pos_remove_pos_category/static/src/js/pos_remove_pos_category.js
@@ -1,19 +1,6 @@
 /******************************************************************************
- *    Point Of Sale - Pricelist for POS Odoo
- *    Copyright (C) 2015-TODAY Akretion (http://www.akretion.com)
- *    @author Sylvain Calador <sylvain.calador@akretion.com>
- *
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU Affero General Public License as
- *    published by the Free Software Foundation, either version 3 of the
- *    License, or (at your option) any later version.
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU Affero General Public License for more details.
- *    You should have received a copy of the GNU Affero General Public License
- *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * Copyright (C) 2015-TODAY Akretion (<http://www.akretion.com>).
+ * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  ******************************************************************************/
 
 openerp.pos_remove_pos_category = function(instance, local) {
