b3ba98805abcda7c9b230ab511656d84342b38fe
Simone Orsi <simahawk@users.noreply.github.com>
2017-02-28 18:28:44 +0100

    [9.0][MIG] pos_remove_pos_category (#148)
    * migrate to v9 and add italian translation
    
    * short headers
    
    * migrate JS
    
    * [fix] be defensive: do not assume we have a product category

--- pos_remove_pos_category/static/src/js/pos_remove_pos_category.js
+++ pos_remove_pos_category/static/src/js/pos_remove_pos_category.js
@@ -1,44 +1,38 @@
 /******************************************************************************
- *    Point Of Sale - Pricelist for POS Odoo
- *    Copyright (C) 2015-TODAY Akretion (http://www.akretion.com)
- *    @author Sylvain Calador <sylvain.calador@akretion.com>
- *
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU Affero General Public License as
- *    published by the Free Software Foundation, either version 3 of the
- *    License, or (at your option) any later version.
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU Affero General Public License for more details.
- *    You should have received a copy of the GNU Affero General Public License
- *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * Copyright (C) 2015-2016 Akretion (<http://www.akretion.com>).
+ * Copyright (C) 2017-TODAY Camptocamp SA (<http://www.camptocamp.com>).
+ * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  ******************************************************************************/
 
-openerp.pos_remove_pos_category = function(instance, local) {
-    module = instance.point_of_sale;
+odoo.define('pos_remove_pos_category.remove_pos_category', function (require) {
+    "use strict";
 
-    var initialize_original = module.PosModel.prototype.initialize;
-    module.PosModel = module.PosModel.extend({
+    var core = require('web.core');
+    var pos_models = require('point_of_sale.models');
+    var pos_screens = require('point_of_sale.screens');
+    var Model = require('web.DataModel');
+    var _t = core._t;
 
+    var _pos_super = pos_models.PosModel.prototype;
+    pos_models.PosModel = pos_models.PosModel.extend({
         initialize: function(session, attributes) {
-
             for (var i = 0 ; i < this.models.length; i++){
                 if (this.models[i].model == 'pos.category') {
                     this.models[i].model = 'product.category';
                     this.models[i].domain = [['available_in_pos', '=', true]];
                 }
             }
-            return initialize_original.call(this, session, attributes);
-        }
-   });
-
-    //override method js POS (widgets.js)
-    //change pos.category by product.category
-    module.ProductCategoriesWidget = module.ProductCategoriesWidget.extend({
-        get_image_url: function(category){
-            return window.location.origin + '/web/binary/image?model=product.category&field=image_medium&id='+category.id;
+            return _pos_super.initialize.apply(this, arguments);
         }
     });
-};
+
+    // override method js POS (widgets.js)
+    // change pos.category by product.category
+    pos_screens.ProductCategoriesWidget.include({
+        get_image_url: function(category){
+            var image_url = '/web/binary/image?model=product.category&field=image_medium&id=';
+            return window.location.origin + image_url + category.id;
+        }
+    });
+
+});
