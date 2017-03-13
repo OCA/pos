c73a01f1cb6e013bb334e5619963b9eebfa6771a
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2017-03-13 16:13:28 +0100

    [MIG] pos_remove_pos_category: Migrated to 10.0

--- pos_remove_pos_category/__manifest__.py
+++ pos_remove_pos_category/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'POS Remove POS Category',
-    'version': '9.0.0.1.0',
+    'version': '10.0.0.1.0',
     'author': 'Akretion, Camptocamp SA, Odoo Community Association (OCA)',
     'category': 'Sales Management',
     'depends': [
