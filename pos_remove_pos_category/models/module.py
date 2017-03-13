fe68bbb48cf13bae5d43bf998536f4902ebc2858
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2017-03-13 16:13:28 +0100

    [MIG] pos_remove_pos_category: Migrated to 10.0

--- pos_remove_pos_category/models/module.py
+++ pos_remove_pos_category/models/module.py
@@ -2,33 +2,31 @@
 # Copyright (C) 2015-TODAY Akretion (<http://www.akretion.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models
+from odoo import models
 
 
 class Module(models.Model):
 
     _inherit = 'ir.module.module'
 
-    def module_uninstall(self, cr, uid, ids, context=None):
+    def module_uninstall(self):
 
-        context = context or {}
-
-        for module in self.browse(cr, uid, ids, context=context):
+        for module in self:
             if module.name == 'pos_remove_pos_category':
 
                 # As we have loose previous POS categs restore them
                 # in a sane empty state
 
-                cr.execute('UPDATE product_template SET pos_categ_id=NULL')
-
+                self._cr.execute(
+                    'UPDATE product_template SET pos_categ_id=NULL')
                 # And restore original constraint
-                cr.execute('''
+                self._cr.execute('''
                     ALTER TABLE product_template
                     DROP CONSTRAINT IF EXISTS
                     product_template_pos_categ_id_fkey
                 ''')
 
-                cr.execute('''
+                self._cr.execute('''
                     ALTER TABLE product_template ADD CONSTRAINT
                     "product_template_pos_categ_id_fkey"
                     FOREIGN KEY (pos_categ_id)
@@ -37,7 +35,7 @@ class Module(models.Model):
 
                 # Restore POS category menu action
                 # in SQL because pool/env is not available here
-                cr.execute('''
+                self._cr.execute('''
                     UPDATE ir_act_window iaw SET res_model='pos.category'
                     FROM ir_model_data imd
                     WHERE
@@ -48,6 +46,4 @@ class Module(models.Model):
 
                 break
 
-        return super(Module, self).module_uninstall(
-            cr, uid, ids, context=context
-        )
+        return super(Module, self).module_uninstall()
