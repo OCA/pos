a11d7052c1c353c0e2777a3d5a3497da7d5b975d
Sylvain LE GAL <sylvain.legal@grap.coop>
2017-04-17 00:08:39 +0200

    [PORT] 10.0 pos_margin

--- pos_margin/models/pos_order.py
+++ pos_margin/models/pos_order.py
@@ -3,7 +3,7 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 import openerp.addons.decimal_precision as dp
 
 
@@ -13,7 +13,7 @@ class PosOrder(models.Model):
     # Columns Section
     margin = fields.Float(
         'Margin', compute='_compute_margin', store=True,
-        digits_compute=dp.get_precision('Product Price'),
+        digits=dp.get_precision('Product Price'),
         help="It gives profitability by calculating the difference between"
         " the Unit Price and the cost price.")
 
