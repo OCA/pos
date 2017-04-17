bc0906a7b8f7bf5968d74d85a01558f1e42de385
Sylvain LE GAL <sylvain.legal@grap.coop>
2017-04-17 11:09:36 +0200

    [IMP] pos_default_empty_image courtesy @invitu

--- pos_default_empty_image/static/src/js/pos_default_empty_image.js
+++ pos_default_empty_image/static/src/js/pos_default_empty_image.js
@@ -1,17 +1,42 @@
 odoo.define('pos_default_empty_image', function (require) {
-"use strict";
+    "use strict";
 
     var models = require('point_of_sale.models');
     var screens = require('point_of_sale.screens');
+
+    var core = require('web.core');
+
+    var QWeb = core.qweb;
 
     //don't try to get an image if we know the product ain't one
     var ProductListImageWidget = screens.ProductListWidget.include({
         get_product_image_url: function(product){
             if (product.has_image)
                 return this._super(product);
+        },
 
-            return '/web/static/src/img/placeholder.png';
-        }
+        // Change product display if product has no image;
+        render_product: function(product){
+            if (product.has_image){
+                return this._super(product);
+            }
+            else {
+                var cached = this.product_cache.get_node(product.id);
+                if(!cached){
+                    var image_url = this.get_product_image_url(product);
+                    var product_html = QWeb.render('ProductNoImage',{
+                        widget:  this,
+                        product: product,
+                    });
+                    var product_node = document.createElement('div');
+                    product_node.innerHTML = product_html;
+                    product_node = product_node.childNodes[1];
+                    this.product_cache.cache_node(product.id,product_node);
+                    return product_node;
+                }
+                return cached;
+            }
+        },
     });
 
     var _super_posmodel = models.PosModel.prototype;
