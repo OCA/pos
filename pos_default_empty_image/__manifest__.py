5aa50f199a59ca9652e5a2a3dae24efb38f41e61
Sylvain LE GAL <sylvain.legal@grap.coop>
2017-04-18 12:35:06 +0200

    [FIX] wording + remove useless JS code

--- pos_default_empty_image/__manifest__.py
+++ pos_default_empty_image/__manifest__.py
@@ -3,9 +3,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'POS Default empty image',
-    'version': '10.0.0.1.0',
+    'version': '10.0.1.0.0',
     'category': 'Point Of Sale',
-    'summary': 'Optimise load time for products with no image',
+    'summary': 'Optimize loading time for products without image',
     'author': "Akretion, GRAP, Odoo Community Association (OCA)",
     'website': "https://akretion.com",
     'license': 'AGPL-3',
