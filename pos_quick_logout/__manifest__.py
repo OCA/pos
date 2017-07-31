262f4153487c2e792c8b07f066a2a2bcafe44d19
Invitu <contact@invitu.com>
2017-07-30 21:45:36 -1000

    [FIX] Fix Travis

--- pos_quick_logout/__manifest__.py
+++ pos_quick_logout/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Point of Sale - Quick Logout',
-    'version': '1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Point Of Sale',
     'summary': 'Allow PoS user to logout quickly after user changed',
     'author': 'La Louve, Odoo Community Association (OCA)',
--- pos_quick_logout/static/src/js/pos_quick_logout.js
+++ pos_quick_logout/static/src/js/pos_quick_logout.js
@@ -20,7 +20,7 @@ chrome.Chrome
                 self.pos.set_cashier(self.pos.user);
                 self.widget.username.renderElement();
             });
-        },
+        }
     });
 
 /* ********************************************************
@@ -29,12 +29,12 @@ chrome.Chrome
     chrome.UsernameWidget.include({
         renderElement: function(){
             this._super();
-             if (this.pos.user.id != this.pos.get_cashier().id){
+             if (this.pos.user.id !== this.pos.get_cashier().id){
                 $('#pos-quick-logout').show();
             }else{
                 $('#pos-quick-logout').hide();
             }
-        },
+        }
     });
 
 });
