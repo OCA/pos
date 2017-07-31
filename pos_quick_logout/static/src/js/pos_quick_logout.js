f987981291d8f11588d9aa3e8021155c5cbf6243
Invitu <contact@invitu.com>
2017-07-30 21:07:44 -1000

    [WIP] add autologout to pos_quick_logout. TODO : how to catch events to reset timer ?

--- pos_quick_logout/static/src/js/pos_quick_logout.js
+++ pos_quick_logout/static/src/js/pos_quick_logout.js
@@ -8,11 +8,41 @@ odoo.define('pos_quick_logout.pos_quick_logout', function (require) {
     "use strict";
 
     var chrome = require('point_of_sale.chrome');
+    var core = require('web.core');
 
 /* ********************************************************
 chrome.Chrome
 ******************************************************** */
     chrome.Chrome.include({
+        init: function(){
+            var self = this;
+            this._super();
+            var idleTime = 0;
+            //Increment the idle time counter every minute.
+            setInterval(function(){
+                idleTime = idleTime + 1;
+                console.log(idleTime);
+                if (self.widget.username && idleTime > self.pos.config.logout_timeout) {
+                    self.pos.set_cashier(self.pos.user);
+                    self.widget.username.renderElement();
+                };
+            }, 1000); // 1 second
+
+            //Zero the idle timer on mouse movement.
+            this.$el.mousemove(function (e) {
+                console.log('mouse');
+                idleTime = 0;
+            });
+            this.$el.keypress(function (e) {
+                console.log('keypress');
+                idleTime = 0;
+            });
+            this.$el.click(function (e) {
+                console.log('click');
+                idleTime = 0;
+            });
+            console.log(this);
+        },
         renderElement: function(){
             this._super();
             var self = this;
@@ -36,5 +66,4 @@ chrome.Chrome
             }
         },
     });
-
 });
