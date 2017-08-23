b37cc09a8893c1e69eece40d083162f64c5ba2b8
Cédric Pigeon <cedric.pigeon@acsone.eu>
2017-08-23 13:03:49 +0200

    Revert "[REM] remove pos_remove_pos_category Fix : #200"
    This reverts commit 5825f0f6a2779aa6801483f9238891e63b6bc8b8.

---
+++ pos_remove_pos_category/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015-TODAY Akretion (<http://www.akretion.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import models
