bae4135f08a15ac578a71c84278e169694b231da
Luiz Felipe do Divino <luiz.divino@kmee.com.br>
2017-08-30 12:55:10 -0300

    [FIX] PEP8

--- pos_payment_term/models/account_journal.py
+++ pos_payment_term/models/account_journal.py
@@ -2,7 +2,7 @@
 # (c) 2016 Kmee - Fernando Marcato
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields, api
+from openerp import models, fields
 
 
 class AccountJournal(models.Model):
