857194498516edab6cae6b3e684d97a7c0bd7a84
Invitu <contact@invitu.com>
2017-11-04 07:47:26 -1000

    [MIG] pos_price_to_weight: Migration to 10.0

--- pos_price_to_weight/__manifest__.py
+++ pos_price_to_weight/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Point of Sale - Price to Weight',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Point Of Sale',
     'summary': 'Compute weight based on barcodes with prices',
     'author': 'La Louve, GRAP, Odoo Community Association (OCA)',
