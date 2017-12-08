1a638e94b42198cc15e6ac614f688af654c30c01
Hpar <raph@hpar.fr>
2017-12-08 12:20:25 +0100

    fix copyright

--- pos_backend_partner/models/partner.py
+++ pos_backend_partner/models/partner.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-# @author Raphael Reverdy <raphael.reverdy@akretion.com>
+# Copyright 2017 Raphael Reverdy <raphael.reverdy@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, api
