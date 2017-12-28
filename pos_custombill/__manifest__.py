e4212e571fa707e56acb1eee36d49dd545a994f0
Cristian Salamea <ovnicraft@gmail.com>
2017-12-28 09:27:08 -0500

    [IMP] custom bill module

---
+++ pos_custombill/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Custom Bill Ticket",
+    'summary': """
+    Custom Bill Ticket
+    """,
+    'description': """
+    Custom Bill Ticket
+    """,
+    'author': "Cristian Salamea",
+    'website': "http://www.prisehub.com",
+    'category': 'POS',
+    'version': '10.0.0.1.0',
+    'depends': [
+        'pos_restaurant'
+    ],
+    'qweb': [
+        'static/src/xml/bill.xml'
+    ]
+}
