8159368054379023c664b2791fc69ae97990e10e
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-03-01 17:26:40 +0100

    [FIX] in backoffice, set correct sale taxes on pos order lines

--- pos_pricelist/models/point_of_sale.py
+++ pos_pricelist/models/point_of_sale.py
@@ -73,6 +73,16 @@ class PosOrderLine(models.Model):
     price_subtotal = fields.Float(compute="_amount_line_all", store=True)
     price_subtotal_incl = fields.Float(compute="_amount_line_all", store=True)
 
+    @api.multi
+    def onchange_product_id(
+            self, pricelist, product_id, qty=0, partner_id=False):
+        product_obj = self.env['product.product']
+        res = super(PosOrderLine, self).onchange_product_id(
+            pricelist, product_id, qty=qty, partner_id=partner_id)
+        if product_id:
+            product = product_obj.browse(product_id)
+            res['value']['tax_ids'] = product.taxes_id.ids
+        return res
 
 class PosOrder(models.Model):
     _inherit = "pos.order"
