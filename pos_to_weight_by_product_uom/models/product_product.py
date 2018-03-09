fa8eb077eb4d48d77e5cdf68884172ad79558e9a
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-03-09 18:14:09 +0100

    [FIX] make the module working for product variants

--- pos_to_weight_by_product_uom/models/product_product.py
+++ pos_to_weight_by_product_uom/models/product_product.py
@@ -1,16 +1,17 @@
 # coding: utf-8
-# Copyright (C) 2017 - Today: GRAP (http://www.grap.coop)
-# @author: Quentin DUPONT <quentin.dupont@grap.coop>
+# Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openerp import api, models
 
 
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
 
     @api.multi
     def onchange_uom(self, uom_id, uom_po_id):
-        res = super(ProductTemplate, self).onchange_uom(uom_id, uom_po_id)
+        res = super(ProductProduct, self).onchange_uom(uom_id, uom_po_id)
         if uom_id:
             res and res or {}
             if not res.get('value', False):
