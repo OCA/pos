8e5a11540eecd361a1cfddd66b506b366c280307
Wolfgang Pichler <wpichler@callino.at>
2018-04-12 18:50:07 +0200

    [MIG] Backported pos_pricelist from 10.0 to 9.0

--- pos_pricelist/__openerp__.py
+++ pos_pricelist/__openerp__.py
@@ -1,46 +1,29 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-# Point Of Sale - Pricelist for POS Odoo
-# Copyright (C) 2014 Taktik (http://www.taktik.be)
-# @author Adil Houmadi <ah@taktik.be>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2018 Tecnativa - Jairo Llopis
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
 {
     'name': 'POS Pricelist',
-    'version': '8.0.1.2.0',
+    'version': '9.0.1.0.1',
     'category': 'Point Of Sale',
-    'sequence': 1,
-    'author': "Adil Houmadi @Taktik,Odoo Community Association (OCA)",
+    'author': "Tecnativa, "
+              "Odoo SA, "
+              "Odoo Community Association (OCA)"
+              "Callino Software Entwicklung",
     'summary': 'Pricelist for Point of sale',
+    'license': 'LGPL-3',
+    'post_init_hook': 'post_init_hook',
     'depends': [
         "point_of_sale",
     ],
+    "external_dependencies": {
+    },
     'data': [
-        "views/pos_pricelist_template.xml",
-        "views/pos_pricelist_views.xml",
-        "views/point_of_sale_view.xml",
-        "report/report_receipt.xml",
         "security/ir.model.access.csv",
-    ],
-    'demo': [
-        'demo/pos_pricelist_demo.yml',
+        "templates/assets.xml",
+        "views/pos_config_view.xml",
     ],
     'qweb': [
         'static/src/xml/pos.xml'
     ],
-    'post_init_hook': "set_pos_line_taxes",
-    'installable': False,
-    'application': False,
-    'auto_install': False,
 }
--- pos_pricelist/static/src/js/screens.js
+++ pos_pricelist/static/src/js/screens.js
@@ -1,32 +1,167 @@
-/******************************************************************************
-*    Point Of Sale - Pricelist for POS Odoo
-*    Copyright (C) 2014 Taktik (http://www.taktik.be)
-*    @author Adil Houmadi <ah@taktik.be>
-*
-*    This program is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU Affero General Public License as
-*    published by the Free Software Foundation, either version 3 of the
-*    License, or (at your option) any later version.
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU Affero General Public License for more details.
-*    You should have received a copy of the GNU Affero General Public License
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-******************************************************************************/
-function pos_pricelist_screens(instance, module) {
+/* Copyright 2018 Tecnativa - Jairo Llopis
+   License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl). */
 
-    module.ClientListScreenWidget = module.ClientListScreenWidget.extend({
-        save_changes: function () {
-            this._super();
-            if (this.has_client_changed()) {
-                var currentOrder = this.pos.get('selectedOrder');
-                var orderLines = currentOrder.get('orderLines').models;
-                var partner = currentOrder.get_client();
-                this.pos.pricelist_engine.update_products_ui(partner);
-                this.pos.pricelist_engine.update_ticket(partner, orderLines);
+odoo.define("pos_pricelist.screens", function (require) {
+    "use strict";
+
+    var core = require("web.core");
+    var screens = require("point_of_sale.screens");
+    var _t = core._t;
+    var exports = {};
+
+    screens.ScaleScreenWidget.include({
+        _get_active_pricelist: function() {
+            var current_order = this.pos.get_order();
+            var current_pricelist = this.pos.default_pricelist;
+            if (current_order) {
+                current_pricelist = current_order.pricelist;
             }
-        }
+            return current_pricelist;
+        },
+
+        get_product_price: function () {
+            var result = this._super.apply(this, arguments);
+            var product = this.get_product();
+            if (product) {
+                var pricelist = this._get_active_pricelist();
+                result = product.get_price(pricelist, this.weight) || result;
+            }
+            return result;
+        },
     });
-}
+
+    screens.DomCache.include({
+        init: function () {
+            this._super.apply(this, arguments);
+            this.key_wrapper = function (key) {
+                return key;
+            };
+        },
+
+        cache_node: function () {
+            arguments[0] = this.key_wrapper(arguments[0]);
+            return this._super.apply(this, arguments);
+        },
+
+        clear_node: function() {
+            arguments[0] = this.key_wrapper(arguments[0]);
+            return this._super.apply(this, arguments);
+        },
+
+        get_node: function() {
+            arguments[0] = this.key_wrapper(arguments[0]);
+            return this._super.apply(this, arguments);
+        },
+    });
+
+    screens.ProductListWidget.include({
+        init: function () {
+            this._super.apply(this, arguments);
+            this.pos.get('orders').bind(
+                'add remove change',
+                $.proxy(this, "renderElement")
+            );
+            this.pos.bind(
+                'change:selectedOrder',
+                $.proxy(this, "renderElement")
+            );
+            this.product_cache.key_wrapper = $.proxy(
+                this,
+                "calculate_cache_key"
+            );
+        },
+
+        calculate_cache_key: function (product) {
+            var result = product + ',' + this._get_active_pricelist().id;
+            return result;
+        },
+
+        _get_active_pricelist: function () {
+            var current_order = this.pos.get_order();
+            var current_pricelist = this.pos.default_pricelist;
+            if (current_order) {
+                current_pricelist = current_order.pricelist;
+            }
+            return current_pricelist;
+        },
+    });
+
+    screens.ClientListScreenWidget.include({
+        save_changes: function () {
+            if (this.has_client_changed()) {
+                var order = this.pos.get_order(),
+                    pricelist = false;
+                if (this.new_client) {
+                    pricelist = _.findWhere(
+                        this.pos.pricelists,
+                        {'id': this.new_client.property_product_pricelist[0]}
+                    );
+                }
+                order.set_pricelist(pricelist || this.pos.default_pricelist);
+            }
+            return this._super.apply(this, arguments);
+        },
+    });
+
+    exports.set_pricelist_button = screens.ActionButtonWidget.extend({
+        template: 'SetPricelistButton',
+        init: function (parent, options) {
+            this._super(parent, options);
+
+            this.pos.get('orders').bind('add remove change', function () {
+                this.renderElement();
+            }, this);
+
+            this.pos.bind('change:selectedOrder', function () {
+                this.renderElement();
+            }, this);
+        },
+
+        button_click: function () {
+            var self = this;
+
+            var pricelists = _.map(self.pos.pricelists, function (pricelist) {
+                return {
+                    label: pricelist.name,
+                    item: pricelist
+                };
+            });
+
+            self.gui.show_popup('selection', {
+                title: _t('Select pricelist'),
+                list: pricelists,
+                confirm: function (pricelist) {
+                    var order = self.pos.get_order();
+                    order.set_pricelist(pricelist);
+                },
+                is_selected: function (pricelist) {
+                    return pricelist.id === self.pos.get_order().pricelist.id;
+                }
+            });
+        },
+
+        get_current_pricelist_name: function () {
+            var name = _t('Pricelist');
+            var order = this.pos.get_order();
+
+            if (order) {
+                var pricelist = order.pricelist;
+
+                if (pricelist) {
+                    name = pricelist.display_name;
+                }
+            }
+            return name;
+        },
+    });
+
+    screens.define_action_button({
+        'name': 'set_pricelist',
+        'widget': exports.set_pricelist_button,
+        'condition': function () {
+            return this.pos.pricelists.length > 1;
+        },
+    });
+
+    return exports;
+});
