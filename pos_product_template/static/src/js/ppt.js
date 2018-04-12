b8b6bc1f17a6694b316265d0bf477e5fe230f555
hparfr <raph@hpar.fr>
2018-04-12 12:06:06 +0200

    Add readme, update manifest,
    switch to short copyright headers

--- pos_product_template/static/src/js/ppt.js
+++ pos_product_template/static/src/js/ppt.js
@@ -1,21 +1,9 @@
-/******************************************************************************
-    Point Of Sale - Product Template module for Odoo
-    Copyright (C) 2014-Today Akretion (http://www.akretion.com)
+/* Copyright (C) 2014-Today Akretion (https://www.akretion.com) 
     @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+    @author Navarromiguel (https://github.com/navarromiguel)
+    License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+*/
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-******************************************************************************/
 odoo.define("pos_product_template.pos_product_template", function(require){
     "use strict";
     
