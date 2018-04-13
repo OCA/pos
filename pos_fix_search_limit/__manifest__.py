ebab1a6776366611c58fb55854386ae0c88aa33a
hparfr <raph@hpar.fr>
2018-04-13 12:07:33 +0200

    Add pos_fix_search_limit module

---
+++ pos_fix_search_limit/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Akretion
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Pos Fix Search Limit",
+    "summary": "Increase search in the PoS",
+    "version": "10.0.1.0.0",
+    "category": "Point of Sale",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "point_of_sale",
+    ],
+    "data": [
+        "views/pos_fix_limit.xml",
+    ]
+}
