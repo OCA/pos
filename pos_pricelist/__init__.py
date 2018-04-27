8f9b6c5ee980d0a8accb152106822b02ecadadab
Hpar <raph@hpar.fr>
2018-04-27 13:13:38 +0200

    Protect crash at runtime if oca-decorator not installed (#254)
    Fail silently if @foreach not loaded

--- pos_pricelist/__init__.py
+++ pos_pricelist/__init__.py
@@ -2,4 +2,9 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 from .hooks import post_init_hook
-from . import models
+try:
+    from . import models
+except ImportError:  # pragma: no-cover
+    import logging
+    _logger = logging.getLogger(__name__)
+    _logger.warn("Missing dependency", exc_info=True)
