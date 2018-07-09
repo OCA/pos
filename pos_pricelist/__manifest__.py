2796bc256ed812ded295b366eb54033fa8e2e02c
David <david.vidal@tecnativa.com>
2018-07-09 12:48:35 +0200

    [FIX] pos_pricelist: right operator in default pricelist

--- pos_pricelist/__manifest__.py
+++ pos_pricelist/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'POS Pricelist',
-    'version': '10.0.1.0.1',
+    'version': '10.0.1.0.2',
     'category': 'Point Of Sale',
     'author': "Tecnativa, "
               "Odoo SA, "
