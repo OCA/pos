77dd05932821bb962fb1069ac9734c29ef86512e
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-07-13 13:38:16 +0200

    [FIX] disable pylint / flake8 Travis check for barcodes module backported from Odoo SA repository

--- barcodes/__init__.py
+++ barcodes/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2004-Today Odoo S.A.
 # License LGPLv3 (https://github.com/odoo/odoo/blob/9.0/LICENSE).
-# flake8: noqa
 
 import barcodes
 
--- barcodes/models/barcode_events_mixin.py
+++ barcodes/models/barcode_events_mixin.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2004-Today Odoo S.A.
 # License LGPLv3 (https://github.com/odoo/odoo/blob/9.0/LICENSE).
-# flake8: noqa
-# pylint: skip-file
 
 from openerp import models, fields, api
 
