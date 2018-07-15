ec05f23bb94c87d6a5c2b6978c1e1939f5f5c02f
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-07-15 16:05:51 +0200

    [FIX] pylint

--- barcodes/models/barcode_events_mixin.py
+++ barcodes/models/barcode_events_mixin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2004-Today Odoo S.A.
 # License LGPLv3 (https://github.com/odoo/odoo/blob/9.0/LICENSE).
+# flake8: noqa
+# pylint: skip-file
+# pylint: disable-all
 
 from openerp import models, fields, api
 
