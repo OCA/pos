8ff6d46e0651973fe6e472ab8766431d9caba394
Kiril Vangelovski <kiril@lambda-is.com>
2018-09-28 21:39:06 +0200

    [MIG] pos_loyalty: Migration to 11.0

--- pos_loyalty/models/pos_order.py
+++ pos_loyalty/models/pos_order.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2004-2010 OpenERP SA
 # Copyright 2017 RGB Consulting S.L. (https://www.rgbconsulting.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
---
+++ pos_loyalty/readme/CONTRIBUTORS.rst
@@ -0,0 +1,3 @@
+* RGB Consulting SL (http://www.rgbconsulting.com)
+* Forward-port from Odoo SA saas-6 branch
+* Kiril Vangelovski <kiril@lambda-is.com>
