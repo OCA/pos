7a2e29249e84d5317d684e9ce013bd125599883c
David Vidal <david.vidal@tecnativa.com>
2018-10-16 13:05:00 +0200

    [11.0][ADD] pos_order_mgmt: New module

---
+++ pos_order_mgmt/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright 2018 GRAP - Sylvain LE GAL
+# Copyright 2018 Tecnativa S.L. - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'POS Frontend Orders Management',
+    'summary': 'Manage old POS Orders from the frontend',
+    'version': '11.0.1.0.0',
+    'category': 'Point of Sale',
+    'author': 'GRAP, '
+              'Tecnativa, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/pos',
+    'license': 'AGPL-3',
+    'depends': [
+        'pos_order_return',
+    ],
+    'data': [
+        'views/assets.xml',
+        'views/view_pos_config.xml',
+    ],
+    'qweb': [
+        'static/src/xml/pos.xml'
+    ],
+    'application': False,
+    'installable': True,
+}
---
+++ pos_order_mgmt/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module extends the functionality of the PoS frontend allowing to load
+already done PoS Orders in order to be able to operate over them, being able to
+reprint past tickets or return them.
