f7ee1f90a0e2eb9f4391511e6cf6bdb07d21afeb
Invitu <contact@invitu.com>
2018-10-25 21:30:33 -1000

    [MIG] pos_margin:Migration to 11.0

--- pos_margin/__manifest__.py
+++ pos_margin/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2017 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'POS Margin',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Point Of Sale',
     'sequence': 1,
     'author': "GRAP,"
