1ba7f66a8bdcc7bf4d676302cccf677d44213f71
GabbasovDinar <gabbasov@it-projects.info>
2018-10-29 15:10:46 +0500

    [MIG] pos_access_right: Migration to 12.0
    [DOC] update README
    
    [FIX] fix variable name
    
    [REF] the pos.config model load all fields
    
    [DOC] update website link
    
    [LINT] fix lint errors
    
    [LINT] fix lint errors

--- pos_access_right/__manifest__.py
+++ pos_access_right/__manifest__.py
@@ -1,25 +1,24 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2016-Today: La Louve (<http://www.lalouve.net/>)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Point of Sale - Extra Access Right',
-    'version': '9.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Point Of Sale',
     'summary': 'Point of Sale - Extra Access Right for certain actions',
     'author': 'La Louve, GRAP, Odoo Community Association (OCA)',
-    'website': 'http://www.lalouve.net/',
+    'website': 'http://www.github.com/OCA/pos',
     'license': 'AGPL-3',
     'depends': [
         'point_of_sale',
     ],
     'data': [
-        'security/res_groups.yml',
+        'security/res_groups.xml',
         'static/src/xml/templates.xml',
     ],
     'demo': [
-        'demo/res_groups.yml',
+        'demo/res_groups.xml',
     ],
     'installable': True,
 }
