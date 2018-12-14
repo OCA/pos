7c8384c5780999b4b59e623389105bbecfc550c6
Przemek Kaminski <cgenie@gmail.com>
2018-12-14 07:15:32 +0100

    [MIG] pos_default_payment_method: Migration to 11.0

--- pos_default_payment_method/__manifest__.py
+++ pos_default_payment_method/__manifest__.py
@@ -2,8 +2,8 @@
 # Copyright (C) 2017-TODAY Camptocamp SA (<http://www.camptocamp.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'POS Default payment mehotd',
-    'version': '10.0.1.0.0',
+    'name': 'POS Default payment method',
+    'version': '11.0.1.0.0',
     'author': 'Camptocamp,Odoo Community Association (OCA)',
     'category': 'Sales Management',
     'depends': [
