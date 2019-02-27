aa48b99f5c67a2e755d67f8a2f1db2827b2de73d
robin.keunen <robin@keunen.net>
2019-02-27 15:17:09 +0100

    [ADD] pos_default_quantity: sets default orderline quantity
    fixup! [ADD] pos_default_quantity: sets default orderline quantity

---
+++ pos_default_quantity/models/product.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# © 2016 Robin Keunen, Coop IT Easy SCRL fs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from openerp import models, fields
+
+
+class ProductUomCategory(models.Model):
+    _inherit = 'product.uom.categ'
+
+    pos_default_qty = fields.Float(
+        string='POS Default Quantity',
+        default=0,
+    )
