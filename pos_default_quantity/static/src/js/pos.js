6cb05bcbe3ecf57e5f6c709303ddc778fff0cf87
robin.keunen <robin@keunen.net>
2019-02-27 15:17:09 +0100

    [ADD] pos_default_quantity: sets default orderline quantity
    fixup! [ADD] pos_default_quantity: sets default orderline quantity
    
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

---
+++ pos_default_quantity/static/src/js/pos.js
@@ -0,0 +1,43 @@
+/*
+  Copyright 2019 Coop IT Easy SCRLfs
+    Robin Keunen <robin@coopiteasy.be>
+  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+*/
+
+odoo.define(
+    'pos_default_quantity.pos_default_quantity',
+
+    function (require) {
+        "use strict";
+
+        var models = require('point_of_sale.models');
+
+        models.load_models({
+            model: 'product.uom.categ',
+            fields: ['name','pos_default_qty'],
+            loaded: function (self, unit_categories) {
+                self.unit_categories = unit_categories;
+                var unit_categories_by_id = {};
+                for (var i = 0, len = unit_categories.length; i < len; i++) {
+                    unit_categories_by_id[unit_categories[i].id] = unit_categories[i]; // eslint-disable-line
+                }
+                self.unit_categories_by_id = unit_categories_by_id;
+            },
+        });
+
+        var orderline_prototype = models.Orderline.prototype;
+        models.Orderline = models.Orderline.extend({
+            initialize: function (attr, options) {
+                orderline_prototype.initialize.call(this, attr, options);
+
+                var unit = this.get_unit();
+                var category = (
+                    this.pos
+                        .unit_categories_by_id[unit.category_id[0]]);
+                if (this.pos.config.set_default_product_quantity) {
+                    this.set_quantity(category.pos_default_qty);
+                }
+            },
+        });
+    }
+);
