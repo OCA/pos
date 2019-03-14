f22773f4d23e463f8718572a41374f23dc707999
Pierrick Brun <pierrick.brun@akretion.com>
2019-03-14 16:31:02 +0100

    [MIG] pos_ticket_logo version 12.0

--- pos_ticket_logo/__manifest__.py
+++ pos_ticket_logo/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Pos Ticket Logo",
     "category": "Point Of Sale",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/pos",
