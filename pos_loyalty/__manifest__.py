c64cdfaf9816b27aad69c661e40d3c11a0bee938
Pierrick Brun <pierrick.brun@akretion.com>
2019-05-07 15:40:14 +0200

    [MIG] pos_loyalty to 12.0

--- pos_loyalty/__manifest__.py
+++ pos_loyalty/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Loyalty Program',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Point of Sale',
     'license': 'AGPL-3',
     'author': "OpenERP SA, "
