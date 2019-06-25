dcb7b7b5a995b4e0be0f1460ba54fb9a3c2f3cf8
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-06-25 13:12:37 +0200

    [12.0][MIG] pos_session_pay_invoice

--- pos_session_pay_invoice/__manifest__.py
+++ pos_session_pay_invoice/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2017 Creu Blanca
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     'name': 'POS Session Pay invoice',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Point Of Sale',
     'author': "Creu Blanca,"
               "Odoo Community Association (OCA)",
---
+++ pos_session_pay_invoice/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This modules allows to pay an existing Supplier Invoice / Customer Refund, or
+to collect payment for an existing Customer Invoice, from within a POS Session.
