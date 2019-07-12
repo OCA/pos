f3c1155b2a2fa198d6a146a7a388393b3aa71dbf
Éloi Rivard <azmeuk@gmail.com>
2019-07-12 14:09:48 +0200

    [8.0][FIX] pos_payment_terminal

--- pos_payment_terminal/static/src/js/pos_payment_terminal.js
+++ pos_payment_terminal/static/src/js/pos_payment_terminal.js
@@ -11,9 +11,10 @@ openerp.pos_payment_terminal = function(instance){
     module = instance.point_of_sale;
 
     module.ProxyDevice = module.ProxyDevice.extend({
-        payment_terminal_transaction_start: function(line, currency_iso){
+        payment_terminal_transaction_start: function(line, currency_iso, currency_decimals){
             var data = {'amount' : line.get_amount(),
                         'currency_iso' : currency_iso,
+                        'currency_decimals': currency_decimals,
                         'payment_mode' : line.cashregister.journal.payment_mode};
             this.message('payment_terminal_transaction_start', {'payment_info' : JSON.stringify(data)});
         },
@@ -25,7 +26,9 @@ openerp.pos_payment_terminal = function(instance){
             var self = this;
             if (line.cashregister.journal.payment_mode && this.pos.config.iface_payment_terminal){
                 el_node.querySelector('.payment-terminal-transaction-start')
-                    .addEventListener('click', function(){self.pos.proxy.payment_terminal_transaction_start(line, self.pos.currency.name)});
+                    .addEventListener('click', function(){
+                        self.pos.proxy.payment_terminal_transaction_start(line, self.pos.currency.name, self.pos.currency.decimals);
+                    });
                 }
             return el_node;
         },
