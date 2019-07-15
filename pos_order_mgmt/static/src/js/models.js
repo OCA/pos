1e6908c40d8cb3537527b68bd87d110c1731d65d
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-07-15 14:47:37 +0200

    [FIX] loaded orders date to local https://github.com/OCA/pos/pull/361 cortesy @chienandalu [FIX] compatibility with pos_restaurant https://github.com/OCA/pos/pull/339 cortesy @carlosDomatix [FIX] add _compute_refund_order_qty [ADD] pos order view form. Add links to original returned order and refund orders. [IMP] when refunding an order via the 'Refund' button in back office, set the correct returned_order_id. [IMP] Add helper on each button [ADD] python test [REF] JS remove useless code [REF] Python remove useless code [FIX] remove bad oldname [FIX] remove gap in bill display, when it is a refund [FIX] when reprinting a bill, the returned order name is displayed, if any [FIX] JS lint, with OCA eslint file [ADD] french translation

--- pos_order_mgmt/static/src/js/models.js
+++ pos_order_mgmt/static/src/js/models.js
@@ -6,40 +6,24 @@ odoo.define('pos_order_mgmt.models', function (require) {
 
     var models = require('point_of_sale.models');
 
-    var _PosModel_push_order = models.PosModel.prototype.push_order;
-    models.PosModel.prototype.push_order = function () {
-        var res = _PosModel_push_order.apply(this, arguments);
-        if (arguments.length && arguments[0] && arguments[0].uid) {
-            var order = this.db.get_order(arguments[0].uid);
-            if (order && order.data) {
-                var data = _.extend({}, order.data);
-                var partner = this.db.get_partner_by_id(data.partner_id);
-                if (partner && partner.id && partner.name) {
-                    data.partner_id = [partner.id, partner.name];
-                }
-                data.date_order = moment(order.data.creation_date)
-                    .format('YYYY-MM-DD HH:mm:ss');
-                this.gui.screen_instances.orderlist.orders.unshift(data);
-            }
-        }
-        return res;
-    };
-
     var order_super = models.Order.prototype;
+
     models.Order = models.Order.extend({
         init_from_JSON: function (json) {
             order_super.init_from_JSON.apply(this, arguments);
-            this.return = json.return;
             this.returned_order_id = json.returned_order_id;
-            this.origin_name = json.origin_name;
+            this.returned_order_reference = json.returned_order_reference;
         },
         export_as_JSON: function () {
             var res = order_super.export_as_JSON.apply(this, arguments);
-            if (this.return) {
-                res.origin_name = this.origin_name;
-                res.returned_order_id = this.returned_order_id;
-                res.return = this.return;
-            }
+            res.returned_order_id = this.returned_order_id;
+            res.returned_order_reference = this.returned_order_reference;
+            return res;
+        },
+        export_for_printing: function () {
+            var res = order_super.export_for_printing.apply(this, arguments);
+            res.returned_order_id = this.returned_order_id;
+            res.returned_order_reference = this.returned_order_reference;
             return res;
         },
     });
