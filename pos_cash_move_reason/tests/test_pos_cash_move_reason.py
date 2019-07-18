dd1c183b74a36aae4612df83052d805386e2d51c
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-07-18 17:35:45 +0200

    [MIG][12.0] port pos_cash_move_reason [REF] Don't use ``product.product`` model for Reasons, because Odoo remove the fields ``expense_pdt`` ``and income_pdt`` from the model. Use instead a new model ``pos.move.reason`` for this purpose. [REF] Doesn't inherit from ``cash.box.in`` and ``cash.box.out`` model, as there are bad designed and doesn't allow clean inheritance. Instead, use new transient model ``wizard.pos.move.reason``. [REF] Oca convention. add readme folder.

--- pos_cash_move_reason/tests/test_pos_cash_move_reason.py
+++ pos_cash_move_reason/tests/test_pos_cash_move_reason.py
@@ -1,99 +1,54 @@
-# -*- coding: utf-8 -*-
 # © 2015 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.tests import common
+from odoo.tests import common
 
 
 class TestPosCashMoveReason(common.TransactionCase):
 
     def setUp(self):
         super(TestPosCashMoveReason, self).setUp()
-        self.pos_session_obj = self.env['pos.session']
-        self.aml_obj = self.env['account.move.line']
-        self.cash_in_obj = self.env['cash.box.in']
-        self.cash_out_obj = self.env['cash.box.out']
-        self.cash_move_reason_obj = self.env['product.template']
-        self.main_config = self.env.ref('point_of_sale.pos_config_main')
-        self.cash_journal = self.env.ref('account.cash_journal')
-        self.income_account = self.env.ref('account.o_income')
-        self.expense_account = self.env.ref('account.a_expense')
-        self.income_reason = self.env.ref('pos_cash_move_reason.income_reason')
-        self.expense_reason = self.env.ref(
-            'pos_cash_move_reason.expense_reason')
+        self.PosSession = self.env['pos.session']
+        self.WizardReason = self.env['wizard.pos.move.reason']
+        self.AccountMoveLine = self.env['account.move.line']
 
-    def test01(self):
-        # I set cash control on cash journal
-        self.cash_journal.cash_control = True
-        # I create and open a new session
-        self.session_01 = self.pos_session_obj.create(
-            {'config_id': self.main_config.id})
-        ctx = self.env.context.copy()
-        # context is updated in open_cb
-        # -> Need to call with old api to give unfrozen context
-        self.registry['pos.session'].open_cb(
-            self.cr, self.uid, [self.session_01.id], context=ctx)
-        ctx['active_ids'] = self.session_01.id
-        ctx['active_model'] = self.session_01._name
-        # I put the session in validation control
-        self.session_01.signal_workflow('cashbox_control')
-        ctx['active_ids'] = self.session_01.id
-        ctx['active_model'] = self.session_01._name
-        # I create a cash in
-        cash_in = self.cash_in_obj.with_context(ctx).create(
-            {'name': 'Initialization',
-             'product_id': self.income_reason.id,
-             'amount': 500.0})
-        cash_in.with_context(ctx).run()
-        # I close the session
-        self.session_01.signal_workflow('close')
-        # I get the statement from the session
-        statement = self.env['account.bank.statement'].search(
-            [('pos_session_id', '=', self.session_01.id),
-             ('journal_id', '=', self.cash_journal.id)])
-        # I get all move lines of this statement
-        move_line_ids = statement.move_line_ids.ids
-        move_line = self.env['account.move.line'].search(
-            [('account_id', '=', self.income_account.id),
-             ('credit', '=', 500.0),
-             ('id', 'in', move_line_ids)])
-        # I check the created move line from the cash in
-        self.assertEquals(len(move_line.ids), 1)
+        self.config = self.env.ref('point_of_sale.pos_config_main').copy()
+        self.cash_journal = self.env['account.journal'].search([
+            ('type', '=', 'cash'),
+            ('company_id', '=', self.env.ref('base.main_company').id),
+        ])[0]
+        self.deposit_reason = self.env.ref(
+            'pos_cash_move_reason.bank_out_reason')
 
-    def test02(self):
-        # I set cash control on cash journal
-        self.cash_journal.cash_control = True
-        # I create and open a new session
-        self.session_01 = self.pos_session_obj.create(
-            {'config_id': self.main_config.id})
-        ctx = self.env.context.copy()
-        # context is updated in open_cb
-        # -> Need to call with old api to give unfrozen context
-        self.registry['pos.session'].open_cb(
-            self.cr, self.uid, [self.session_01.id], context=ctx)
-        ctx['active_ids'] = self.session_01.id
-        ctx['active_model'] = self.session_01._name
-        # I put the session in validation control
-        self.session_01.signal_workflow('cashbox_control')
-        ctx['active_ids'] = self.session_01.id
-        ctx['active_model'] = self.session_01._name
-        # I create a cash out
-        cash_out = self.cash_out_obj.with_context(ctx).create(
-            {'name': 'Miscellaneous expense',
-             'product_id': self.expense_reason.id,
-             'amount': 500.0})
-        cash_out.with_context(ctx).run()
-        # I close the session
-        self.session_01.signal_workflow('close')
-        # I get the statement from the session
-        statement = self.env['account.bank.statement'].search(
-            [('pos_session_id', '=', self.session_01.id),
-             ('journal_id', '=', self.cash_journal.id)])
+    def test_take_money(self):
+        # Open New Session
+        self.config.open_session_cb()
+        session = self.PosSession.search([
+            ('state', '=', 'opened'),
+            ('config_id', '=', self.config.id),
+        ])
+
+        # Get Cash Statement
+        statement = session.statement_ids.filtered(
+            lambda x: x.journal_id == self.cash_journal)
+
+        # Take money to put in Bank
+        wizard = self.WizardReason.with_context(
+            active_id=session.id,
+            default_move_type='expense').create({
+                'move_reason_id': self.deposit_reason.id,
+                'journal_id': self.cash_journal.id,
+                'statement_id': statement.id,
+                'amount': 500,
+                'name': 'Test Bank Deposit',
+            })
+        wizard.apply()
+        session.action_pos_session_closing_control()
+
         # I get all move lines of this statement
-        move_line_ids = statement.move_line_ids.ids
         move_line = self.env['account.move.line'].search(
-            [('account_id', '=', self.expense_account.id),
+            [('account_id', '=', self.deposit_reason.expense_account_id.id),
              ('debit', '=', 500.0),
-             ('id', 'in', move_line_ids)])
+             ('id', 'in', statement.move_line_ids.ids)])
         # I check the created move line from the cash in
         self.assertEquals(len(move_line.ids), 1)
---
+++ pos_cash_move_reason/wizard/wizard_pos_move_reason.py
@@ -0,0 +1,100 @@
+# © 2015 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+
+
+class WizardPosMoveReason(models.TransientModel):
+    _name = 'wizard.pos.move.reason'
+    _description = 'PoS Move Reasons Wizard'
+
+    def _default_move_type(self):
+        return self.env.context.get('default_move_type', 'expense')
+
+    def _default_session_id(self):
+        return self.env.context.get('active_id', False)
+
+    _MOVE_TYPE_SELECTION = [
+        ('income', 'Put Money In'),
+        ('expense', 'Take Money Out'),
+    ]
+
+    move_type = fields.Selection(
+        selection=_MOVE_TYPE_SELECTION, string='Move type',
+        default=_default_move_type)
+
+    move_reason_id = fields.Many2one(
+        comodel_name='pos.move.reason', string='Move Reason', required=True)
+
+    journal_id = fields.Many2one(
+        comodel_name='account.journal', string="Journal",
+        domain="[('id', 'in', journal_ids)]", required=True)
+
+    session_id = fields.Many2one(
+        comodel_name='pos.session', string="Current Session",
+        default=_default_session_id, required=True, readonly=True)
+
+    statement_id = fields.Many2one(
+        comodel_name='account.bank.statement', string='Bank Statement',
+        compute='_compute_statement_id')
+
+    journal_ids = fields.Many2many(
+        comodel_name='account.journal', related='move_reason_id.journal_ids')
+
+    name = fields.Char(string='Reason', required=True)
+
+    amount = fields.Float(string='Amount', required=True)
+
+    @api.onchange('move_type')
+    def onchange_move_type(self):
+        if self.move_type == 'income':
+            return {'domain': {
+                'move_reason_id': [('is_income_reason', '=', True)]}}
+        else:
+            return {'domain': {
+                'move_reason_id': [('is_expense_reason', '=', True)]}}
+
+    @api.onchange('move_reason_id')
+    def onchange_reason(self):
+        if len(self.journal_ids) == 1:
+            self.journal_id = self.journal_ids[0].id
+        self.name = self.move_reason_id.name
+
+    @api.constrains('amount')
+    def _check_amount(self):
+        for wizard in self.filtered(lambda x: x.amount <= 0):
+            raise UserError(_("Invalid Amount"))
+
+    @api.depends('journal_id', 'session_id')
+    def _compute_statement_id(self):
+        for wizard in self:
+            if wizard.session_id and wizard.journal_id:
+                statements = wizard.session_id.statement_ids.filtered(
+                    lambda x: x.journal_id == wizard.journal_id)
+                wizard.statement_id = statements and statements[0]
+
+    @api.multi
+    def apply(self):
+        self.ensure_one()
+        AccountBankStatementLine = self.env['account.bank.statement.line']
+        AccountBankStatementLine.create(self._prepare_statement_line())
+
+    @api.multi
+    def _prepare_statement_line(self):
+        self.ensure_one()
+        if self.move_type == 'income':
+            account = self.move_reason_id.income_account_id
+            amount = self.amount
+        else:
+            account = self.move_reason_id.expense_account_id
+            amount = - self.amount
+        return {
+            'date': fields.Datetime.now(),
+            'statement_id': self.statement_id.id,
+            'journal_id': self.journal_id.id,
+            'amount': amount,
+            'account_id': account.id,
+            'name': self.name,
+            'ref': self.session_id.name,
+        }
