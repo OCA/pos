0a925d2e02a47241bd14265d0185f32a48e4f74a
David <david.vidal@tecnativa.com>
2019-07-22 10:18:07 +0200

    [MIG] pos_stock_picking_invoice_link: Migration to 11.0

--- pos_stock_picking_invoice_link/models/__init__.py
+++ pos_stock_picking_invoice_link/models/__init__.py
@@ -1,3 +1,2 @@
-# -*- coding: utf-8 -*-
-
 from . import pos_order
+from . import stock_move
--- pos_stock_picking_invoice_link/models/pos_order.py
+++ pos_stock_picking_invoice_link/models/pos_order.py
@@ -1,12 +1,14 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 Tecnativa - David Vidal
+# Copyright 2018-19 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from odoo import models
+from odoo import fields, models
 
 
 class PosOrder(models.Model):
     _inherit = 'pos.order'
+
+    def create_picking(self):
+        self_ctx = self.with_context(merge_pos_order_line=True)
+        return super(PosOrder, self_ctx).create_picking()
 
     def _prepare_invoice(self):
         res = super(PosOrder, self)._prepare_invoice()
@@ -20,11 +22,15 @@ class PosOrder(models.Model):
             PosOrder, self)._action_create_invoice_line(line, invoice_id)
         if not line:
             return invoice_line
-        move = self.env['stock.move'].search([
-            ('picking_id', '=', self.picking_id.id),
-            ('name', '=', line.name)])
-        if move:
-            invoice_line.write({
-                'move_line_ids': [(6, 0, move.ids)],
-            })
+        invoice_line.move_line_ids |= line.stock_move_ids
         return invoice_line
+
+
+class PosOrderLine(models.Model):
+    _inherit = 'pos.order.line'
+
+    stock_move_ids = fields.One2many(
+        comodel_name='stock.move',
+        inverse_name='pos_order_line_id',
+        string='Related Stock Moves',
+    )
