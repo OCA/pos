fa624c5eb3d74155e74b42036ae7e7b7aa1d4e21
Pierrick Brun <pierrick.brun@akretion.com>
2019-09-30 17:58:19 +0200

    [MIG] version 12.0

--- pos_payment_terminal/__manifest__.py
+++ pos_payment_terminal/__manifest__.py
@@ -1,14 +1,14 @@
-# -*- coding: utf-8 -*-
 # © 2014-2016 Aurélien DUMAINE
 # © 2015-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2019 Akretion (Pierrick Brun <pierrick.brun@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'POS Payment Terminal',
-    'version': '10.0.0.1.1',
+    'version': '12.0.0.1.0',
     'category': 'Point Of Sale',
     'summary': 'Manage Payment Terminal device from POS front end',
-    'author': "Aurélien DUMAINE,Akretion,Odoo Community Association (OCA)",
+    'author': "Akretion, Odoo Community Association (OCA)",
     'license': 'AGPL-3',
     'depends': ['point_of_sale'],
     'data': [
