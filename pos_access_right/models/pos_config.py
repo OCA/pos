3eb824b2bbc132bf76cec2cbb0555fef1261ac65
Ammar Officewala <aofficewala4@gmail.com>
2019-10-05 15:52:01 +0530

    [MIG] pos_access_right : Migaration to 13.0

--- pos_access_right/models/pos_config.py
+++ pos_access_right/models/pos_config.py
@@ -2,7 +2,7 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models, api
+from odoo import fields, models
 
 
 class PosConfig(models.Model):
@@ -50,37 +50,31 @@ class PosConfig(models.Model):
         help="This field is there to pass the id of the 'PoS - Payment'"
         " Group to the Point of Sale Frontend.")
 
-    @api.multi
     def _compute_group_negative_qty_id(self):
         for config in self:
             self.group_negative_qty_id = \
                 self.env.ref('pos_access_right.group_negative_qty')
 
-    @api.multi
     def _compute_group_discount_id(self):
         for config in self:
             self.group_discount_id = \
                 self.env.ref('pos_access_right.group_discount')
 
-    @api.multi
     def _compute_group_change_unit_price_id(self):
         for config in self:
             self.group_change_unit_price_id = \
                 self.env.ref('pos_access_right.group_change_unit_price')
 
-    @api.multi
     def _compute_group_multi_order_id(self):
         for config in self:
             self.group_multi_order_id = \
                 self.env.ref('pos_access_right.group_multi_order')
 
-    @api.multi
     def _compute_group_delete_order_id(self):
         for config in self:
             self.group_delete_order_id = \
                 self.env.ref('pos_access_right.group_delete_order')
 
-    @api.multi
     def _compute_group_payment_id(self):
         for config in self:
             self.group_payment_id = \
