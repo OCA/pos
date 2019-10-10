1c598dc54f0849a6ba8bc921b674098270399d86
MurtazaMithaiwala <mithaiwalam52@gmail.com>
2019-10-11 00:43:36 +0530

    [MIG] pos_margin: Migration to 13.0

--- pos_margin/__manifest__.py
+++ pos_margin/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'PoS Order Margin',
     'summary': 'Margin on PoS Order',
-    'version': '12.0.2.0.0',
+    'version': '13.0.1.0.0',
     'category': 'Point Of Sale',
     'author': "GRAP,"
               "Odoo Community Association (OCA)",
--- pos_margin/readme/CONTRIBUTORS.rst
+++ pos_margin/readme/CONTRIBUTORS.rst
@@ -1,2 +1,3 @@
 * Sylvain LE GAL (https://twitter.com/legalsylvain)
 * Wolfgang Pichler
+* Murtaza Mithaiwala (https://twitter.com/MurtazaMithaiw4)
