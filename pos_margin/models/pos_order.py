f4badd2217c2eaefa2ce28dd1b3727129e78bbfb
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-12-12 11:18:15 +0100

    [ADD] percent margin on pos.order and pos.order.line (following same logic as OCA/margin-analysis/account_invoice_margin module ; [ADD] margin and margin percent on tree pos order view ; [UPD] Description and translation

--- pos_margin/models/pos_order.py
+++ pos_margin/models/pos_order.py
@@ -11,14 +11,29 @@ class PosOrder(models.Model):
 
     # Columns Section
     margin = fields.Float(
-        'Margin', compute='_compute_margin', store=True,
+        string='Margin',
+        compute='_compute_margin',
+        store=True,
         digits=dp.get_precision('Product Price'),
         help="It gives profitability by calculating the difference between"
         " the Unit Price and the cost price.")
 
+    margin_percent = fields.Float(
+        string='Margin (%)',
+        compute='_compute_margin',
+        store=True,
+        digits=dp.get_precision('Product Price'),
+    )
+
     # Compute Section
     @api.multi
-    @api.depends('lines.margin')
+    @api.depends('lines.margin', 'lines.price_subtotal')
     def _compute_margin(self):
         for order in self:
-            order.margin = sum(order.mapped('lines.margin'))
+            tmp_margin = sum(order.mapped('lines.margin'))
+            tmp_price_subtotal = sum(order.mapped('lines.price_subtotal'))
+            order.update({
+                'margin': tmp_margin,
+                'margin_percent': tmp_price_subtotal and (
+                    tmp_margin / tmp_price_subtotal * 100),
+            })
