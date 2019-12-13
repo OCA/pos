5fcb34b1b8ec5248a0842d976d11670a51eb8c24
Roberto Fichera <robyf@tekno-soft.it>
2019-12-13 19:32:12 +0100

    [NEW][10.0] POS partner firstname support

---
+++ pos_partner_firstname/models/res_partner.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Roberto Fichera
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.model
+    def create_from_ui(self, partner):
+        if 'is_company' in partner:
+            partner['is_company'] = partner['is_company'] == 'true'
+        return super(ResPartner, self).create_from_ui(partner)
---
+++ pos_partner_firstname/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+The module adds the support of the fields of the partner_firstname module within the point of sale interface,
+allowing to view and edit them in the given customer view.
