408740e2acd8d4f285040a4f1a12969a0caa5894
eLBati <lb@takobi.online>
2019-12-20 15:26:11 +0100

    porting pos_partner_firstname to 12

--- pos_partner_firstname/__manifest__.py
+++ pos_partner_firstname/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Roberto Fichera
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "POS Partner Firstname",
     "summary": "POS Support of partner firstname",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "development_status": "Beta",
     "category": "Point Of Sale",
     "website": "https://github.com/OCA/pos",
