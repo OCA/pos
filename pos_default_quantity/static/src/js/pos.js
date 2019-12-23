517202fa89c737bfec0a082689f79181d0d7213a
robin.keunen <robin@keunen.net>
2019-12-23 13:26:22 +0100

    [FIX] pos_default_quantity: don't set default quantity on restored lines
    When restoring the order from a json, previous code overwrote quantity with default quantity

--- pos_default_quantity/static/src/js/pos.js
+++ pos_default_quantity/static/src/js/pos.js
@@ -30,10 +30,16 @@ odoo.define(
             initialize: function (attr, options) {
                 orderline_prototype.initialize.call(this, attr, options);
 
+                // don't set default quantity on restored lines
+                if (options.json) {
+                    return;
+                }
+
                 var unit = this.get_unit();
                 var category = (
                     this.pos
                         .unit_categories_by_id[unit.category_id[0]]);
+
                 if (this.pos.config.set_default_product_quantity) {
                     this.set_quantity(category.pos_default_qty);
                 }
