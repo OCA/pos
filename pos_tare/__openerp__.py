354cca3ec3f6803bed654a799e13abe7a8705381
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-01-08 11:27:12 +0100

    [REF] pos_tare: Black python code

--- pos_tare/__openerp__.py
+++ pos_tare/__openerp__.py
@@ -4,24 +4,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Point Of Sale - Tare',
-    'summary': 'Manage Tare in Point Of Sale module',
-    'version': '8.0.1.0.0',
-    'category': 'Point Of Sale',
-    'author': 'GRAP',
-    'website': 'http://www.grap.coop',
-    'license': 'AGPL-3',
-    'depends': [
-        'point_of_sale',
-    ],
-    'data': [
-        'views/templates.xml',
-    ],
-    'demo': [
-        'demo/pos_config.xml',
-    ],
-    'qweb': [
-        'static/src/xml/pos_tare.xml',
-    ],
-    'installable': False,
+    "name": "Point Of Sale - Tare",
+    "summary": "Manage Tare in Point Of Sale module",
+    "version": "8.0.1.0.0",
+    "category": "Point Of Sale",
+    "author": "GRAP",
+    "website": "http://www.grap.coop",
+    "license": "AGPL-3",
+    "depends": ["point_of_sale",],
+    "data": ["views/templates.xml",],
+    "demo": ["demo/pos_config.xml",],
+    "qweb": ["static/src/xml/pos_tare.xml",],
+    "installable": False,
 }
