e0ba8e3b0d82040d6f48940ea59537e2abddf08e
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-01-09 09:52:11 +0100

    [REF] pos_customer_display: Black python code

--- pos_customer_display/__manifest__.py
+++ pos_customer_display/__manifest__.py
@@ -4,17 +4,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'POS Customer Display',
-    'version': '10.0.1.0.1',
-    'category': 'Point Of Sale',
-    'summary': 'Manage Customer Display device from POS front end',
-    'author': "Aurélien DUMAINE,Akretion,Odoo Community Association (OCA)",
-    'license': 'AGPL-3',
-    'depends': ['point_of_sale'],
-    'data': [
-        'views/pos_customer_display.xml',
-        'views/customer_display_view.xml',
-        ],
-    'demo': ['demo/pos_customer_display_demo.xml'],
-    'installable': True,
+    "name": "POS Customer Display",
+    "version": "10.0.1.0.1",
+    "category": "Point Of Sale",
+    "summary": "Manage Customer Display device from POS front end",
+    "author": "Aurélien DUMAINE,Akretion,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["point_of_sale"],
+    "data": [
+        "views/pos_customer_display.xml",
+        "views/customer_display_view.xml",
+    ],
+    "demo": ["demo/pos_customer_display_demo.xml"],
+    "installable": True,
 }
