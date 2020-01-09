3b3b46ed90dba1f5d6b6978cd9d6b58b499ec525
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-01-09 09:52:11 +0100

    [MIG] pos_customer_display: Migration to 12.0 [REF] OCA Convention - rename files. - split js file into many files. (models, screens, etc.) [FIX] make default "welcome" and "good bye" message translatable [REF] add the possibility to have other format of LCD Customer display.
    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! [MIG] pos_customer_display: Migration to 12.0 [REF] OCA Convention. rename files.
    
    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! [MIG] pos_customer_display: Migration to 12.0 [REF] OCA Convention. rename files.
    
    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! [MIG] pos_customer_display: Migration to 12.0 [REF] OCA Convention. rename files.
    
    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! [MIG] pos_customer_display: Migration to 12.0 [REF] OCA Convention. rename files.
    
    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! [MIG] pos_customer_display: Migration to 12.0 [REF] OCA Convention. rename files.
    
    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! [MIG] pos_customer_display: Migration to 12.0 [REF] OCA Convention. rename files.

--- pos_customer_display/__manifest__.py
+++ pos_customer_display/__manifest__.py
@@ -1,20 +1,19 @@
-# -*- coding: utf-8 -*-
 # © 2014-2016 Aurélien DUMAINE
 # © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "POS Customer Display",
-    "version": "10.0.1.0.1",
+    "name": "Point of Sale - LED Customer Display",
+    "version": "12.0.1.0.0",
     "category": "Point Of Sale",
-    "summary": "Manage Customer Display device from POS front end",
-    "author": "Aurélien DUMAINE,Akretion,Odoo Community Association (OCA)",
+    "summary": "Manage LED Customer Display device from POS front end",
+    "author": "Aurélien DUMAINE,GRAP,Akretion,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["point_of_sale"],
     "data": [
-        "views/pos_customer_display.xml",
-        "views/customer_display_view.xml",
+        "views/assets.xml",
+        "views/view_pos_config.xml",
     ],
-    "demo": ["demo/pos_customer_display_demo.xml"],
+    "demo": ["demo/pos_config.xml"],
     "installable": True,
 }
---
+++ pos_customer_display/readme/ROADMAP.rst
@@ -0,0 +1,1 @@
+* Support other format, for exemple, 4 lines of 30 characters, for the Bixolon BDC-2000.
