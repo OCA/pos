9e2f99925280a784208dda3b1bd09532f3ff5021
David Beal <david.beal@akretion.com>
2020-01-10 09:50:50 +0100

    ADD module pos_default_partner

---
+++ pos_default_partner/__manifest__.py
@@ -0,0 +1,21 @@
+# copyright 2020 Akretion David BEAL
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Point Of Sale Default Partner",
+    "summary": "Add a default customer in pos order",
+    "license": "AGPL-3",
+    "version": "12.0.1.0.0",
+    "author": "Akretion,"
+              "Odoo Community Association (OCA)",
+    "maintainer": "Akretion",
+    "category": "Point of sale",
+    "depends": [
+        "point_of_sale",
+    ],
+    "data": [
+        "views/config_view.xml"
+    ],
+    "website": "https://github.com/OCA/pos",
+    "installable": True,
+}
