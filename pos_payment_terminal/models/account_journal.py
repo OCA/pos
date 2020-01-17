b0b19120712cc0de435351de61963cd5475c604b
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-01-17 20:20:50 +0100

    [MIG] pos_payment_terminal

--- pos_payment_terminal/models/account_journal.py
+++ pos_payment_terminal/models/account_journal.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2014-2016 Aurélien DUMAINE
 # © 2015-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
