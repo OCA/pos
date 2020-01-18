4dfab3f4b7fc9d365305fd45896b9baae04c7739
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-01-18 18:23:25 +0100

    [MIG] pos_order_to_sale_order: Migration to 12.0

--- pos_order_to_sale_order/models/sale_order.py
+++ pos_order_to_sale_order/models/sale_order.py
@@ -1,90 +1,61 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2017 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api, _
+from odoo import _, api, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     @api.model
-    def _prepare_order_field_from_pos(self, order_data):
-        session_obj = self.env["pos.session"]
-        session = session_obj.browse(order_data["pos_session_id"])
-        res = self.onchange_partner_id(order_data["partner_id"])["value"]
-        res.update(
-            {
-                "partner_id": order_data["partner_id"] or False,
-                "origin": _("Point of Sale %s") % (session.name),
-                "client_order_ref": order_data["name"],
-                "user_id": order_data["user_id"] or False,
-                "order_line": [],
-            }
-        )
-        if self.env.context.get("is_pos_pricelist"):
-            res.update(
-                {"pricelist_id": order_data["pricelist_id"],}
-            )
-        for line_data in order_data["lines"]:
-            res["order_line"].append(
-                [
-                    0,
-                    False,
-                    self._prepare_order_line_field_from_pos(line_data[2], res),
-                ]
-            )
-        return res
+    def _prepare_from_pos(self, order_data):
+        PosSession = self.env["pos.session"]
+        session = PosSession.browse(order_data["pos_session_id"])
+        return {
+            "partner_id": order_data["partner_id"],
+            "origin": _("Point of Sale %s") % (session.name),
+            "client_order_ref": order_data["name"],
+            "user_id": order_data["user_id"],
+            "pricelist_id": order_data["pricelist_id"],
+            "fiscal_position_id": order_data["fiscal_position_id"],
+        }
 
     @api.model
-    def _prepare_order_line_field_from_pos(self, line_data, sale_order_data):
-        line_obj = self.env["sale.order.line"]
-        res = line_obj.product_id_change(
-            sale_order_data["pricelist_id"],
-            line_data["product_id"],
-            qty=line_data["qty"],
-            partner_id=sale_order_data["partner_id"],
-        )["value"]
-        res.update(
-            {
-                "product_id": line_data["product_id"],
-                "product_uom_qty": line_data["qty"],
-                "discount": line_data["discount"],
-                "tax_id": [(6, False, res["tax_id"])],
-            }
-        )
-        if self.env.context.get("is_pos_pricelist"):
-            res.update(
-                {
-                    "price_unit": line_data["price_unit"],
-                    "tax_id": line_data["tax_ids"],
-                }
-            )
-        return res
+    def create_order_from_pos(self, order_data, action):
+        SaleOrderLine = self.env["sale.order.line"]
 
-    @api.model
-    def create_order_from_pos(self, order_data):
-        is_pos_pricelist = len(
-            self.env["ir.module.module"].search(
-                [("name", "=", "pos_pricelist"), ("state", "=", "installed")]
-            )
-        )
         # Create Draft Sale order
-        sale_order = self.create(
-            self.with_context(
-                is_pos_pricelist=is_pos_pricelist
-            )._prepare_order_field_from_pos(order_data)
-        )
+        order_vals = self._prepare_from_pos(order_data)
+        sale_order = self.create(order_vals.copy())
+        sale_order.onchange_partner_id()
+        # we rewrite data, because onchange could alter some
+        # custom data (like pricelist)
+        sale_order.write(order_vals)
+
+        # create Sale order lines
+        for order_line_data in order_data["lines"]:
+            # Create Sale order lines
+            order_line_vals = SaleOrderLine._prepare_from_pos(
+                sale_order, order_line_data[2])
+            sale_order_line = SaleOrderLine.create(
+                order_line_vals.copy())
+            sale_order_line.product_id_change()
+            # we rewrite data, because onchange could alter some
+            # data (like quantity, or price)
+            sale_order_line.write(order_line_vals)
 
         # Confirm Sale Order
-        if order_data["sale_order_state"] in ["confirmed", "delivered"]:
-            sale_order.action_button_confirm()
+        if action in ["confirmed", "delivered"]:
+            sale_order.action_confirm()
 
         # mark picking as delivered
-        if order_data["sale_order_state"] == "delivered":
-            sale_order.picking_ids.force_assign()
-            sale_order.picking_ids.do_transfer()
+        if action == "delivered":
+            # Mark all moves are delivered
+            for move in sale_order.mapped(
+                    "picking_ids.move_ids_without_package"):
+                move.quantity_done = move.product_uom_qty
+            sale_order.mapped("picking_ids").button_validate()
 
         return {
             "sale_order_id": sale_order.id,
--- pos_order_to_sale_order/readme/CONFIGURE.rst
+++ pos_order_to_sale_order/readme/CONFIGURE.rst
@@ -1,5 +1,3 @@
-To configure this module, you need to:
-
 * Go to Point Of Sale / Configuration / Point of Sale
 * Check the box 'Create Sale Orders'
 * Select the desired default behaviour
