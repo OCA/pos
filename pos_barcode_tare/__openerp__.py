776fd955ed441802699c6cdca0a7e6cc3e620049
François Kawala <francois.kawala@gmail.com>
2020-01-27 20:53:25 +0100

    OCA guidelines.

--- pos_barcode_tare/__openerp__.py
+++ pos_barcode_tare/__openerp__.py
@@ -1,36 +1,21 @@
 # -*- coding: utf-8 -*-
+# @author: François Kawala
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': "pos_barecode_tare",
-
-    'summary': """
-        Allows to scan a barcode to tare the latest product.
-    """,
-
-    'description': """
-        This add-on enable POS to read and print tare bar codes. A tare bar code is used to sell unpackaged goods in a
-         BYOC (bring your own container) scheme. This scheme has four steps:
-         1. The cashier weights the container and sticks the tare bar code onto the customer's container. 
-         2. The customer takes the desired quantity of whatever good s-he wants.
-         3. The cashier weights the filled container and good, POS gives the corresponding price.
-         4. The cashier scans the tare bar code, POS removes the container's weight from the latest product of the order.
-    """,
-
-    'author': "Le Nid",
-    'website': "http://www.lenid.ch",
-
+    'name': "Point of Sale - Tare barecode labels for loose goods",
+    'version': '9.0.0.0.1',
     'category': 'Point of Sale',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'summary': 'Point of Sale - Print and scan tare barecodes labels to sell loose goods.',
+    'author': "Le Nid, Odoo Community Association (OCA)",
+    'website': "https://github.com/OCA/pos",
+    'license': 'AGPL-3',
     'depends': ['point_of_sale'],
-
-    # always loaded
     'data': [
-        'views/templates.xml',
+        'views/pos_config_view.xml',
+        'views/pos_barcode_tare.xml',
     ],
     'qweb': [
-        'static/src/xml/tare_screen.xml',
-        'static/src/xml/open_tare_screen_button.xml',
-
+        'static/src/xml/pos_barcode_tare.xml',
     ],
+    'installable': True,
 }
---
+++ pos_barcode_tare/models/pos_config.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields
+
+
+class PosConfig(models.Model):
+    _inherit = 'pos.config'
+    tare_label_button = fields.Boolean('Show tare label button', help="Print tare labels with this POS")
