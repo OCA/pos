4ae35f4bd1c6d9ee3c04c78f01ea7092e5df9977
Francois Kawala <francois.kawala@gmail.com>
2020-01-28 10:48:35 +0100

    Fix PEP8.

--- pos_barcode_tare/models/pos_config.py
+++ pos_barcode_tare/models/pos_config.py
@@ -5,4 +5,7 @@ from openerp import models, fields
 
 class PosConfig(models.Model):
     _inherit = 'pos.config'
-    tare_label_button = fields.Boolean('Show tare label button', help="Print tare labels with this POS")
+    tare_label_button = fields.Boolean(
+        'Show tare label button',
+        help="Print tare labels with this POS"
+    )
