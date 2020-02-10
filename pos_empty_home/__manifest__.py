947e201c7c93b2c38242f85c3fd942c4e1e10cf5
Ajay Javiya <javiyaajay1987@gmail.com>
2020-02-10 14:15:23 +0530

    [MIG] pos_empty_home: Migration to 12.0

--- pos_empty_home/__manifest__.py
+++ pos_empty_home/__manifest__.py
@@ -1,13 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2017-Today: La Louve (<http://www.lalouve.net/>)
+# Copyright (C) 2019-Today: Druidoo (<https://www.druidoo.io>)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Point of Sale - Empty Home',
-    'version': '9.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Point Of Sale',
-    'summary': 'Hide products at the start of the Point of Sale',
+    'summary': '''
+        Point of Sale - Hide products at the start of the Point of Sale''',
     'author': 'La Louve, GRAP, Odoo Community Association (OCA)',
     'website': 'http://www.lalouve.net/',
     'license': 'AGPL-3',
@@ -15,10 +16,10 @@
         'point_of_sale',
     ],
     'data': [
-        'static/src/xml/templates.xml',
+        'views/assets.xml',
     ],
     'qweb': [
         'static/src/xml/pos_empty_home.xml',
     ],
-    'installable': True,
+    'installable': True
 }
