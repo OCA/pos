cd587c5e08d95c0a66a096864147de893b800962
Iván Todorovich <ivan.todorovich@gmail.com>
2020-03-03 11:26:21 +0100

    [FIX] No need to translate selection fields

--- pos_price_to_weight/models/barcode_rule.py
+++ pos_price_to_weight/models/barcode_rule.py
@@ -2,12 +2,12 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import _, fields, models
+from odoo import models, fields
 
 
 class BarcodeRule(models.Model):
     _inherit = 'barcode.rule'
 
     type = fields.Selection(selection_add=[
-        ('price_to_weight', _('Priced Product (Computed Weight)'))
+        ('price_to_weight', 'Priced Product (Computed Weight)')
     ])
