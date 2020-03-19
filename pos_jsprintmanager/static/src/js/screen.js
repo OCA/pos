92c09596ea82f412fca74b158a5b24581fcb8f31
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2020-03-19 12:30:03 +0100

    add esc-pos

--- pos_jsprintmanager/static/src/js/screen.js
+++ pos_jsprintmanager/static/src/js/screen.js
@@ -46,24 +46,65 @@ odoo.define("pos_jsprintmanager.screen", function (require) {
 
         print_web: function() {
             if (this.jspmWSStatus) {
-                //generate an image of HTML content through html2canvas utility
-                html2canvas(document.getElementsByClassName('pos-sale-ticket')[0], { scale: 5 }).then(function (canvas) {
-
+                var outputFormat = 'esc-pos'
+                if (outputFormat == 'esc-pos'){
                     //Create a ClientPrintJob
-                    var cpj = new JSPM.ClientPrintJob();
+                    var cpj = new JSPM.ClientPrintJob()
                     cpj.clientPrinter = new JSPM.DefaultPrinter();
                     //Set content to print...
-                    var b64Prefix = "data:image/png;base64,";
-                    var imgBase64DataUri = canvas.toDataURL("image/png");
-                    var imgBase64Content = imgBase64DataUri.substring(b64Prefix.length, imgBase64DataUri.length);
+                    //Create ESP/POS commands for sample label
+                    var esc = '\x1B'; //ESC byte in hex notation
+                    var newLine = '\x0A'; //LF byte in hex notation
 
-                    var myImageFile = new JSPM.PrintFile(imgBase64Content, JSPM.FileSourceType.Base64, 'myFileToPrint.png', 1);
-                    //add file to print job
-                    cpj.files.push(myImageFile);
+                    var cmds = esc + "@"; //Initializes the printer (ESC @)
+                    cmds += esc + '!' + '\x38'; //Emphasized + Double-height + Double-width mode selected (ESC ! (8 + 16 + 32)) 56 dec => 38 hex
+                    cmds += 'BEST DEAL STORES'; //text to print
+                    cmds += newLine + newLine;
+                    cmds += esc + '!' + '\x00'; //Character font A selected (ESC ! 0)
+                    cmds += 'COOKIES                   5.00';
+                    cmds += newLine;
+                    cmds += 'MILK 65 Fl oz             3.78';
+                    cmds += newLine + newLine;
+                    cmds += 'SUBTOTAL                  8.78';
+                    cmds += newLine;
+                    cmds += 'TAX 5%                    0.44';
+                    cmds += newLine;
+                    cmds += 'TOTAL                     9.22';
+                    cmds += newLine;
+                    cmds += 'CASH TEND                10.00';
+                    cmds += newLine;
+                    cmds += 'CASH DUE                  0.78';
+                    cmds += newLine + newLine;
+                    cmds += esc + '!' + '\x18'; //Emphasized + Double-height mode selected (ESC ! (16 + 8)) 24 dec => 18 hex
+                    cmds += '# ITEMS SOLD 2';
+                    cmds += esc + '!' + '\x00'; //Character font A selected (ESC ! 0)
+                    cmds += newLine + newLine;
+                    cmds += '11/03/13  19:53:17';
 
+                    cpj.printerCommands = cmds;
                     //Send print job to printer!
                     cpj.sendToClient();
-                });
+                } else {
+                    //generate an image of HTML content through html2canvas utility
+                    var ticket = document.getElementsByClassName('pos-sale-ticket')[0]
+                    html2canvas(ticket, {scale: 10, width: 900}).then(function (canvas) {
+                        //Create a ClientPrintJob
+                        var cpj = new JSPM.ClientPrintJob();
+                        cpj.clientPrinter = new JSPM.DefaultPrinter();
+                        //Set content to print...
+                        var b64Prefix = "data:image/png;base64,";
+                        var imgBase64DataUri = canvas.toDataURL("image/png");
+                        var imgBase64Content = imgBase64DataUri.substring(b64Prefix.length, imgBase64DataUri.length);
+
+                        var myImageFile = new JSPM.PrintFile(imgBase64Content, JSPM.FileSourceType.Base64, 'myFileToPrint.png', 1);
+                        //add file to print job
+                        cpj.files.push(myImageFile);
+
+                        //Send print job to printer!
+                        cpj.sendToClient();
+                    });
+                }
+
             }
             this.pos.get_order()._printed = true;
         },
