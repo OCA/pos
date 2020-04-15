e920da67bbfdc61e37d8b12b7465b76fc624524d
Vincent Van Rossem <vvrossem@gmail.com>
2020-04-15 15:57:46 +0200

    [MIG] pos_default_quantity: migration to 10.0 - followed oca guidelines - not tested on odoo 10.0
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- pos_default_quantity/__manifest__.py
+++ pos_default_quantity/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Set Default Product Quantity in POS",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "author": "Coop IT Easy SCRLfs, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/pos",
     "license": "AGPL-3",
