22af398515239c9b9ddcb82193b89a242093efb9
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2020-05-13 16:52:46 +0200

    [MIG] pos_require_product_quantity: v9 to v12

--- pos_require_product_quantity/models/pos_config.py
+++ pos_require_product_quantity/models/pos_config.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-# © 2016 Robin Keunen, Coop IT Easy SCRL fs
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-
-from openerp import models, fields
+from odoo import models, fields
 
 
 class PosConfig(models.Model):
