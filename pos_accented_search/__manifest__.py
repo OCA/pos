50b43e957d34d720d0ceee0a997f4040a7a1ce12
François Kawala <francois.kawala@gmail.com>
2020-05-13 16:05:40 +0200

    V12 migration.

--- pos_accented_search/__manifest__.py
+++ pos_accented_search/__manifest__.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
-# @author: François Kawala
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': "Point of Sale - Accented Product Search",
-    'version': '9.0.0.0.2',
+    'version': '12.0.0.0.2',
     'category': 'Point of Sale',
     'summary': 'Point of Sale - Product search works regardless of accented characters',
     'author': "Le Nid, Odoo Community Association (OCA)",
