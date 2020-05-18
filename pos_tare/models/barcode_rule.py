05a49b8132059d792a384095e220b93fc7928af5
François Kawala <francois.kawala@gmail.com>
2020-05-18 08:25:00 +0200

    Fix flake8.

--- pos_tare/models/barcode_rule.py
+++ pos_tare/models/barcode_rule.py
@@ -1,9 +1,9 @@
 from odoo import _, models, fields
 
+
 class BarcodeRule(models.Model):
     _inherit = 'barcode.rule'
 
     type = fields.Selection(selection_add=[
         ('tare', _('Tare'))
     ])
-
--- pos_tare/models/pos_config.py
+++ pos_tare/models/pos_config.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 
 class PosConfig(models.Model):
     _inherit = 'pos.config'
-    
+
     iface_tare_method = fields.Selection([
         ('Manual', 'Input the tare manually'),
         ('Barcode', 'Scan a barcode to set the tare'),
@@ -15,4 +15,4 @@ class PosConfig(models.Model):
         "* 'Manual (the scale screen has an extra tare input field)';\n"
         "* 'Barecode (scan a barcode to tare the selected order line)';\n"
         "* 'Both manual input and barcode methods are enabled';",
-    ) 
+    )
