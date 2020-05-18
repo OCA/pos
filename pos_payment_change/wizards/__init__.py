396b7a97837145de93290cf437f30526742ef085
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-05-18 19:32:28 +0200

    [FIX] error in balance computation if change is done during closing control ; [ADD] display old payment lines

--- pos_payment_change/wizards/__init__.py
+++ pos_payment_change/wizards/__init__.py
@@ -1,2 +1,3 @@
 from . import pos_payment_change_wizard
-from . import pos_payment_change_wizard_line
+from . import pos_payment_change_wizard_new_line
+from . import pos_payment_change_wizard_old_line
