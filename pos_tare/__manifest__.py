cc060cacbf88d3a478beb75320046ba142e20c5d
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-05-19 14:06:30 +0200

    [REF] cosmetic changes

--- pos_tare/__manifest__.py
+++ pos_tare/__manifest__.py
@@ -3,21 +3,21 @@
 {
     "name": "Point Of Sale - Tare",
     "summary": "Manage Tare in Point Of Sale module",
-    'version': '12.0.1.0.0',
-    'category': 'Point of Sale',
-    'author': "GRAP, Le Nid, Odoo Community Association (OCA)",
-    'website': "https://github.com/OCA/pos",
-    'license': 'AGPL-3',
-    'maintainers': ['fkawala'],
-    'depends': ['point_of_sale'],
-    'demo': ['demo/pos_tare_demo.xml'],
-    'data': [
-        'views/templates.xml',
-        'views/pos_config_view.xml',
-        'data/barcode_rule.xml',
+    "version": "12.0.1.0.0",
+    "category": "Point of Sale",
+    "author": "GRAP, Le Nid, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pos",
+    "license": "AGPL-3",
+    "maintainers": ["fkawala"],
+    "depends": ["point_of_sale"],
+    "demo": ["demo/uom_uom.xml"],
+    "data": [
+        "views/templates.xml",
+        "views/pos_config_view.xml",
+        "data/barcode_rule.xml",
     ],
-    'qweb': [
-        'static/src/xml/pos_tare.xml',
+    "qweb": [
+        "static/src/xml/pos_tare.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
