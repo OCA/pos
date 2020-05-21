eec909c07706ba4f8a46149496ac53551f8e9b30
Joan <joan.segui@qubiq.es>
2020-05-21 09:28:28 +0200

    [IMP] pos_order_remove_line: black, isort, prettier

--- pos_order_remove_line/__manifest__.py
+++ pos_order_remove_line/__manifest__.py
@@ -2,19 +2,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': "POS Order Remove Line",
-    'summary': "Add button to remove POS order line.",
-    'author': "Roberto Fichera, Odoo Community Association (OCA)",
-    'website': 'https://www.github.com/OCA/pos',
-    'category': 'Point of Sale',
+    "name": "POS Order Remove Line",
+    "summary": "Add button to remove POS order line.",
+    "author": "Roberto Fichera, Odoo Community Association (OCA)",
+    "website": "https://www.github.com/OCA/pos",
+    "category": "Point of Sale",
     "maintainers": ["robyf70"],
-    'version': '12.0.1.0.0',
-    'license': 'LGPL-3',
-    'depends': ['point_of_sale'],
-    'data': [
-        'views/assets.xml'
-    ],
-    'qweb': [
-        'static/src/xml/order_line.xml'
-    ]
+    "version": "13.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["point_of_sale"],
+    "data": ["views/assets.xml"],
+    "qweb": ["static/src/xml/order_line.xml"],
 }
