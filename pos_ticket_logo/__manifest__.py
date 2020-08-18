f2481c80684801f194e24eb2d09a76be4340f716
Esteban Monge <estebanmonge@riseup.net>
2020-08-18 13:37:54 -0600

    Port to 13.0 pos_ticket_logo

---
+++ pos_ticket_logo/__manifest__.py
@@ -0,0 +1,21 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Pos Ticket Logo",
+    "category": "Point Of Sale",
+    "version": "12.0.1.0.0",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pos",
+    "license": "AGPL-3",
+    "depends": [
+        "point_of_sale",
+    ],
+    "data": [
+        "templates/assets.xml",
+    ],
+    "qweb": [
+        "static/src/xml/pos.xml",
+    ],
+    "installable": True,
+}
