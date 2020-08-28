637eef9e3473e76e735bf982296acb44de19c586
François Honoré <francois.honore@acsone.eu>
2020-08-28 10:23:08 +0200

    [IMP] pos_restricted_customer_list: black, isort, prettier

--- pos_restricted_customer_list/models/res_partner.py
+++ pos_restricted_customer_list/models/res_partner.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017-2019 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
@@ -6,15 +5,11 @@ from openerp import fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    available_in_pos = fields.Boolean(
-        string="Available for POS",
-        default=False,
-    )
+    available_in_pos = fields.Boolean(string="Available for POS", default=False,)
 
     def create_from_ui(self, cr, uid, partner, context=None):
-        if not partner.get('available_in_pos'):
-            partner['available_in_pos'] = True
-        return super(ResPartner, self).create_from_ui(
-            cr, uid, partner, context)
+        if not partner.get("available_in_pos"):
+            partner["available_in_pos"] = True
+        return super(ResPartner, self).create_from_ui(cr, uid, partner, context)
