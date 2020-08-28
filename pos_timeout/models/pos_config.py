f8b7ad39b196ab9d8ca40ab932eb96d860fa6c45
François Honoré <francois.honore@acsone.eu>
2020-08-28 08:18:47 +0200

    [IMP] pos_timeout: black, isort, prettier

--- pos_timeout/models/pos_config.py
+++ pos_timeout/models/pos_config.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -7,10 +6,11 @@ from odoo import fields, models
 
 
 class PosConfig(models.Model):
-    _inherit = 'pos.config'
+    _inherit = "pos.config"
 
     pos_order_timeout = fields.Integer(
-        string='PoS Order(s) Timeout', help="Defines the value of the"
+        string="PoS Order(s) Timeout",
+        help="Defines the value of the"
         " client-side timeout for the creation of PoS Order(s)"
         " from the POS UI.\n"
         " The value is expressed in seconds, for a single order.\n"
@@ -19,4 +19,5 @@ class PosConfig(models.Model):
         " (after a long disconnection period for example, or if the previous"
         " call raised the timeout),\n the effective timeout value applied will"
         " be equal to the defined timeout value multiplied by the number of"
-        " orders.\n")
+        " orders.\n",
+    )
