1f00db494c21eed0d3d0fe0a388ee1b467bcd03b
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-09-09 11:01:33 +0200

    [ADD] pos_hide_banknote_button

---
+++ pos_hide_banknote_button/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2020 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Point of Sale - Hide Banknote Buttons',
+    'version': '12.0.1.0.0',
+    'category': 'Point Of Sale',
+    'summary': 'Hide useless Banknote buttons in the PoS (+10, +20, +50)',
+    'author': 'GRAP, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/pos',
+    'license': 'AGPL-3',
+    'depends': [
+        'point_of_sale',
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+}
