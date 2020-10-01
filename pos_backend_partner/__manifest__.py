1664ce1dea583be1c6265f17b7ddc56b1c51c5aa
hparfr <raph@hpar.fr>
2020-10-01 14:37:36 +0200

    Bump version

--- pos_backend_partner/__manifest__.py
+++ pos_backend_partner/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "POS Backend Customer",
     "summary": "Choose point of sale customers in backend",
-    "version": "10.0.1.0.2",
+    "version": "12.0.1.0.2",
     "category": "Point of sale",
     "website": "http://www.akretion.com",
     'author': "Akretion,Odoo Community Association (OCA)",
