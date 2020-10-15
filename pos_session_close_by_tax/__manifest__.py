a1de231c078401fe22becdf08bc2d9e8ddc20c34
eLBati <lb@takobi.online>
2020-10-15 20:42:51 +0200

    ADD pos_session_close_by_tax

---
+++ pos_session_close_by_tax/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2020 Lorenzo Battistini @ TAKOBI
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Point of sale: closure by tax",
+    "summary": "Group journal items by tax when closing session",
+    "version": "12.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Point Of Sale",
+    "website": "https://github.com/OCA/pos",
+    "author": "TAKOBI, Odoo Community Association (OCA)",
+    "maintainers": ["eLBati"],
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "point_of_sale",
+    ],
+}
