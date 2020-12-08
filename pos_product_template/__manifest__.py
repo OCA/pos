7b9802349681243f66666f2e8cdecf3830fdf987
hparfr <raph@hpar.fr>
2020-12-08 12:32:02 +0100

    [IMP] pos_product_template: black, isort, prettier

--- pos_product_template/__manifest__.py
+++ pos_product_template/__manifest__.py
@@ -1,29 +1,28 @@
-# -*- encoding: utf-8 -*-
 {
-    'name': 'POS - Product Template',
-    'version': '10.0.2.1.0',
-    'category': 'Point Of Sale',
-    'author': "Akretion,Odoo Community Association (OCA)",
-    'summary': 'Manage Product Template in Front End Point Of Sale',
-    'website': 'http://www.akretion.com',
-    'license': 'AGPL-3',
-    'depends': [
-        'point_of_sale',
-        'pos_fix_search_limit',
+    "name": "POS - Product Template",
+    "version": "10.0.2.1.0",
+    "category": "Point Of Sale",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "summary": "Manage Product Template in Front End Point Of Sale",
+    "website": "https://github.com/OCA/pos",
+    "license": "AGPL-3",
+    "depends": [
+        "point_of_sale",
+        "pos_fix_search_limit",
     ],
-    'data': [
-        'view/view.xml',
+    "data": [
+        "view/view.xml",
     ],
-    'qweb': [
-        'static/src/xml/ppt.xml',
+    "qweb": [
+        "static/src/xml/ppt.xml",
     ],
-    'demo': [
-        'demo/product_attribute_value.yml',
-        'demo/product_product.yml',
-        'demo/res_groups.yml',
+    "demo": [
+        "demo/product_attribute_value.yml",
+        "demo/product_product.yml",
+        "demo/res_groups.yml",
     ],
-    'images': [
-        'static/src/img/screenshots/pos_product_template.png',
+    "images": [
+        "static/src/img/screenshots/pos_product_template.png",
     ],
-    'installable': True,
+    "installable": True,
 }
