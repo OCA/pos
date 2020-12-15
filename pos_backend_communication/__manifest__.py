c20c0aefbf9eb2da586fdf5315b30462ceb6145a
hparfr <raph@hpar.fr>
2020-12-15 11:06:58 +0100

    [MIG] pos_backend_communication: Migration to 14.0

--- pos_backend_communication/__manifest__.py
+++ pos_backend_communication/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Akretion (http://www.akretion.com).
 # @author Raphaël Reverdy <raphael.reverdy@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -6,17 +5,18 @@
 {
     "name": "POS Backend Communication",
     "summary": "Communicate with odoo's backend from POS.",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Point of Sale",
-    "website": "http://www.akretion.com",
-    'author': 'Akretion, Odoo Community Association (OCA)',
+    "website": "https://github.com/OCA/pos",
+    "author": "Akretion, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
-    'installable': True,
+    "installable": True,
     "depends": [
         "point_of_sale",
     ],
     "data": [
-        'views/assets.xml',
+        "security/ir.model.access.csv",
+        "views/assets.xml",
     ],
 }
--- pos_backend_communication/static/src/js/back.js
+++ pos_backend_communication/static/src/js/back.js
@@ -1,49 +1,47 @@
-'use strict';
-
-odoo.define('pos_backend_communication.back', function (require) {
-    var tools = require('pos_backend_communication.tools');
-    var ActionManager = require('web.ActionManager');
-    var core = require('web.core')
+odoo.define("pos_backend_communication.back", function (require) {
+    "use strict";
+    const tools = require("pos_backend_communication.tools");
+    const ActionManager = require("web.ActionManager");
+    const core = require("web.core");
 
     function is_tied_to_pos() {
-        return (!!window.opener);
-        //TODO : add test location.origin
+        return Boolean(window.opener);
+        // TODO : add test location.origin
     }
 
     function sendMessage(a) {
-        //send message to pos
+        // Send message to pos
         if (is_tied_to_pos()) {
-            //can only work if the backoffice is opened by the POS
+            // Can only work if the backoffice is opened by the POS
             window.opener.postMessage(a, location.origin);
         }
     }
 
     if (is_tied_to_pos()) {
-        //set up action 'act_tell_pos' called by .py
+        // Set up action 'act_tell_pos' called by .py
         ActionManager.include({
             _handleAction: function (action, options) {
-                if (action['type'] == 'ir.actions.tell_pos') {
+                if (action.type === "ir.actions.tell_pos") {
                     return this._executeTellPOSAction(action, options);
-                } else {
-                    return this._super.apply(this, arguments);
                 }
+                return this._super.apply(this, arguments);
             },
             _executeTellPOSAction: function (action, options) {
+                /* eslint no-unused-vars: ["error", { "args": "none" }]*/
                 sendMessage(action.params);
                 return $.when();
             },
         });
-        //when page is fully loaded
-        core.bus.on('web_client_ready', null, function () {
-            //this class hides menus
-            $('body').addClass('pos_backend_communication');
+        // When page is fully loaded
+        core.bus.on("web_client_ready", null, function () {
+            // This class hides menus
+            $("body").addClass("pos_backend_communication");
         });
     }
 
     return {
-      'sendMessage': sendMessage,
-      'callbacks': tools.callbacks,
-      'is_tied_to_pos': is_tied_to_pos,
+        sendMessage: sendMessage,
+        callbacks: tools.callbacks,
+        is_tied_to_pos: is_tied_to_pos,
     };
-
 });
