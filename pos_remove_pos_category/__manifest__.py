b76e909525d879bedd58a333f4f66b3da95d934d
hparfr <raph@hpar.fr>
2020-12-17 10:33:19 +0100

    [MIG] pos_remove_pos_category: Migration to 14.0

--- pos_remove_pos_category/__manifest__.py
+++ pos_remove_pos_category/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'POS Remove POS Category',
-    'version': '12.0.2.0.0',
+    'version': "14.0.1.0.0",
     'author': 'Akretion, Camptocamp SA, ACSONE SA/NV, '
               'Odoo Community Association (OCA)',
     'category': 'Point of Sale',
