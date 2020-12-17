a9cf4e7dc8564770fa581f6afbe3833ba7c8e558
hparfr <raph@hpar.fr>
2020-12-17 15:31:13 +0100

    pos_remove_pos_category: black, isort, ...

--- pos_remove_pos_category/models/product.py
+++ pos_remove_pos_category/models/product.py
@@ -1,34 +1,36 @@
 # Copyright (C) 2015-TODAY Akretion (<http://www.akretion.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api, tools
+from odoo import api, fields, models, tools
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
-    pos_categ_id = fields.Many2one('product.category',
-                                   store=False,
-                                   related='categ_id',
-                                   search='_search_pos_categ_id')
+    pos_categ_id = fields.Many2one(
+        "product.category",
+        store=False,
+        related="categ_id",
+        search="_search_pos_categ_id",
+    )
 
     def _search_pos_categ_id(self, operator, value):
-        return [('categ_id', operator, value)]
+        return [("categ_id", operator, value)]
 
     @api.model
     def create(self, vals):
-        if 'categ_id' in vals:
-            vals['pos_categ_id'] = vals['categ_id']
+        if "categ_id" in vals:
+            vals["pos_categ_id"] = vals["categ_id"]
         return super().create(vals)
 
     def write(self, vals):
-        if 'pos_categ_id' in vals and not vals['pos_categ_id']:
-            del vals['pos_categ_id']
+        if "pos_categ_id" in vals and not vals["pos_categ_id"]:
+            del vals["pos_categ_id"]
         return super().write(vals)
 
 
 class ProductCategory(models.Model):
-    _inherit = 'product.category'
+    _inherit = "product.category"
 
     image_128 = fields.Image("Image", max_width=128, max_height=128)
 
@@ -36,5 +38,6 @@ class ProductCategory(models.Model):
         string="Available in the Point of Sale",
         default=True,
         help="Check if you want this category to appear in Point Of Sale.\n"
-             "If you uncheck, children categories will becomes invisible too, "
-             "whatever their checkbox state.")
+        "If you uncheck, children categories will becomes invisible too, "
+        "whatever their checkbox state.",
+    )
--- pos_remove_pos_category/static/src/js/models.js
+++ pos_remove_pos_category/static/src/js/models.js
@@ -1,4 +1,4 @@
-/******************************************************************************
+/** ****************************************************************************
  * Copyright (C) 2015-2016 Akretion (<http://www.akretion.com>).
  * Copyright (C) 2017-TODAY Camptocamp SA (<http://www.camptocamp.com>).
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -11,14 +11,14 @@ odoo.define("pos_remove_pos_category.models", function (require) {
 
     var _pos_super = pos_models.PosModel.prototype;
     pos_models.PosModel = pos_models.PosModel.extend({
-        initialize: function(session, attributes) {
-            for (var i = 0 ; i < this.models.length; i++){
+        initialize: function (session, attributes) {
+            for (var i = 0; i < this.models.length; i++) {
                 if (this.models[i].model == "pos.category") {
                     this.models[i].model = "product.category";
                     this.models[i].domain = [["available_in_pos", "=", true]];
                 }
             }
             return _pos_super.initialize.apply(this, arguments);
-        }
+        },
     });
 });
