8222128d71944aa50f6fdae16f0ed5d294931336
Alexis de Lattre <alexis.delattre@akretion.com>
2020-12-17 10:08:18 +0100

    pos_payment_terminal: black, isort and pre-commit stuff

--- pos_payment_terminal/static/src/js/payment_terminal.js
+++ pos_payment_terminal/static/src/js/payment_terminal.js
@@ -4,13 +4,13 @@
     License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 */
 
-odoo.define('pos_payment_terminal.payment', function (require) {
+odoo.define("pos_payment_terminal.payment", function (require) {
     "use strict";
 
-    var core = require('web.core');
-    var rpc = require('web.rpc');
-    var PaymentInterface = require('point_of_sale.PaymentInterface');
-    const { Gui } = require('point_of_sale.Gui');
+    var core = require("web.core");
+    var rpc = require("web.rpc");
+    var PaymentInterface = require("point_of_sale.PaymentInterface");
+    const {Gui} = require("point_of_sale.Gui");
 
     var _t = core._t;
 
@@ -20,7 +20,6 @@ odoo.define('pos_payment_terminal.payment', function (require) {
             return this._oca_payment_terminal_pay();
         },
 
-
         _oca_payment_terminal_pay: function () {
             var self = this;
             var order = this.pos.get_order();
@@ -28,48 +27,53 @@ odoo.define('pos_payment_terminal.payment', function (require) {
             var currency = this.pos.currency;
             if (pay_line.amount < 0) {
                 // TODO check if it's possible or not
-                this._show_error(_t('Cannot process transactions with negative amount.'));
+                this._show_error(
+                    _t("Cannot process transactions with negative amount.")
+                );
                 return Promise.resolve();
             }
             var data = {
-                'amount': pay_line.amount,
-                'currency_iso': currency.name,
-                'currency_decimals': currency.decimals,
-                'payment_mode': this.payment_method.oca_payment_terminal_mode,
-                'payment_id': pay_line.cid,
-                'order_id': order.name,
-                };
+                amount: pay_line.amount,
+                currency_iso: currency.name,
+                currency_decimals: currency.decimals,
+                payment_mode: this.payment_method.oca_payment_terminal_mode,
+                payment_id: pay_line.cid,
+                order_id: order.name,
+            };
             // TODO improve behavior when response is true
-            this._oca_payment_terminal_proxy_request(data).then(function(response) {
+            this._oca_payment_terminal_proxy_request(data).then(function (response) {
                 if (response === false) {
-                    self._show_error(_t('Failed to send the amount to pay to the payment terminal. Press the red button on the payment terminal and try again.'));
-                    }
-                else if (response === true) {
-                    pay_line.set_payment_status('force_done');
+                    self._show_error(
+                        _t(
+                            "Failed to send the amount to pay to the payment terminal. Press the red button on the payment terminal and try again."
+                        )
+                    );
+                } else if (response === true) {
+                    pay_line.set_payment_status("force_done");
                 }
             });
         },
 
         _oca_payment_terminal_proxy_request: function (data) {
-            var promise = this.pos.proxy.message(
-                'payment_terminal_transaction_start',
-                {'payment_info' : JSON.stringify(data)}
-            ).then(function(response) {
-                return response;
-            });
+            var promise = this.pos.proxy
+                .message("payment_terminal_transaction_start", {
+                    payment_info: JSON.stringify(data),
+                })
+                .then(function (response) {
+                    return response;
+                });
             return promise;
         },
 
         _show_error: function (msg, title) {
             if (!title) {
-                title =  _t('Payment Terminal Error');
+                title = _t("Payment Terminal Error");
             }
-            Gui.showPopup('ErrorPopup', {
-                'title': title,
-                'body': msg,
+            Gui.showPopup("ErrorPopup", {
+                title: title,
+                body: msg,
             });
         },
-
     });
     return OCAPaymentTerminal;
 });
--- pos_payment_terminal/tests/test_transactions.py
+++ pos_payment_terminal/tests/test_transactions.py
@@ -5,45 +5,62 @@ from odoo.addons.point_of_sale.tests.common import TestPointOfSaleCommon
 
 
 class TestTransactions(TestPointOfSaleCommon):
-
     def test_matching(self):
-        card_journal_id = self.env['account.journal'].create({
-            'name': 'Card Journal',
-            'code': 'CARD',
-            'type': 'bank',
-            'pos_terminal_payment_mode': 'card',
-        }).id
+        card_journal_id = (
+            self.env["account.journal"]
+            .create(
+                {
+                    "name": "Card Journal",
+                    "code": "CARD",
+                    "type": "bank",
+                    "pos_terminal_payment_mode": "card",
+                }
+            )
+            .id
+        )
         cash_journal_id = 0
         pos_order = {
-            'pos_session_id': self.pos_order_session0.id,
-            'statement_ids': [
-                (0, 0, {
-                    'name': 'Payment1',
-                    'amount': 45.2,
-                    'journal_id': card_journal_id,
-                }),
-                (0, 0, {
-                    'name': 'Payment2',
-                    'amount': 10.5,
-                    'journal_id': card_journal_id,
-                }),
-                (0, 0, {
-                    'name': 'Payment3',
-                    'amount': 22.0,
-                    'journal_id': cash_journal_id,
-                }),
+            "pos_session_id": self.pos_order_session0.id,
+            "statement_ids": [
+                (
+                    0,
+                    0,
+                    {
+                        "name": "Payment1",
+                        "amount": 45.2,
+                        "journal_id": card_journal_id,
+                    },
+                ),
+                (
+                    0,
+                    0,
+                    {
+                        "name": "Payment2",
+                        "amount": 10.5,
+                        "journal_id": card_journal_id,
+                    },
+                ),
+                (
+                    0,
+                    0,
+                    {
+                        "name": "Payment3",
+                        "amount": 22.0,
+                        "journal_id": cash_journal_id,
+                    },
+                ),
             ],
-            'transactions': [
+            "transactions": [
                 {
-                    'reference': 'ABCDE',
-                    'amount_cents': 1050,
+                    "reference": "ABCDE",
+                    "amount_cents": 1050,
                 },
                 {
-                    'reference': 'XPTO',
-                    'amount_cents': 4520,
+                    "reference": "XPTO",
+                    "amount_cents": 4520,
                 },
-            ]
+            ],
         }
-        self.env['pos.order']._match_transactions_to_payments(pos_order)
-        self.assertEquals(pos_order['statement_ids'][0][2]['note'], 'XPTO')
-        self.assertEquals(pos_order['statement_ids'][1][2]['note'], 'ABCDE')
+        self.env["pos.order"]._match_transactions_to_payments(pos_order)
+        self.assertEquals(pos_order["statement_ids"][0][2]["note"], "XPTO")
+        self.assertEquals(pos_order["statement_ids"][1][2]["note"], "ABCDE")
