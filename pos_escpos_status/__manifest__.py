57f80347ab4e9ef61715af7a6301c5bd1c19b642
Pierrick Brun <pierrick.brun@akretion.com>
2020-12-22 13:58:55 +0100

    [ADD] pos_escpos_status module

---
+++ pos_escpos_status/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 Akretion (https://www.akretion.com).
+# @author Pierrick Brun <pierrick.brun@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "POS ESC/Pos printer Status",
+    "version": "14.0.1.0.0",
+    "category": "Point Of Sale",
+    "summary": "Point of sale: fetch status for 'escpos' driver",
+    "author": "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pos",
+    "license": "AGPL-3",
+    "depends": ["point_of_sale"],
+    "data": ["views/assets.xml"],
+    "installable": True,
+}
