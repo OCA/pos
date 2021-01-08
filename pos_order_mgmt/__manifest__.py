95e17ab26a0e4de48749dd98c42457593b1706f0
David Alonso <david.alonso@solvos.es>
2021-01-08 10:15:08 +0100

    [IMP] pos_order_mgmt: black, isort, prettier

--- pos_order_mgmt/__manifest__.py
+++ pos_order_mgmt/__manifest__.py
@@ -3,26 +3,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'POS Frontend Orders Management',
-    'summary': 'Manage old POS Orders from the frontend',
-    'version': '12.0.1.1.3',
-    'category': 'Point of Sale',
-    'author': 'GRAP, '
-              'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/pos',
-    'license': 'AGPL-3',
-    'depends': [
-        'point_of_sale',
+    "name": "POS Frontend Orders Management",
+    "summary": "Manage old POS Orders from the frontend",
+    "version": "12.0.1.1.3",
+    "category": "Point of Sale",
+    "author": "GRAP, " "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pos",
+    "license": "AGPL-3",
+    "depends": ["point_of_sale",],
+    "data": [
+        "views/assets.xml",
+        "views/view_pos_config.xml",
+        "views/view_pos_order.xml",
     ],
-    'data': [
-        'views/assets.xml',
-        'views/view_pos_config.xml',
-        'views/view_pos_order.xml',
-    ],
-    'qweb': [
-        'static/src/xml/pos.xml'
-    ],
-    'application': False,
-    'installable': True,
+    "qweb": ["static/src/xml/pos.xml"],
+    "application": False,
+    "installable": True,
 }
--- pos_order_mgmt/readme/CONTRIBUTORS.rst
+++ pos_order_mgmt/readme/CONTRIBUTORS.rst
@@ -1,6 +1,6 @@
 * David Vidal <david.vidal@tecnativa.com>
 * Sylvain LE GAL (https://twitter.com/legalsylvain)
-* Carlos Mart??nez <carlos@domatix.com>
+* Carlos Martínez <carlos@domatix.com>
 * Pierrick Brun <pierrick.brun@akretion.com>
-* Iv??n Todorovich <ivan.todorovich@druidoo.io>
+* Iván Todorovich <ivan.todorovich@druidoo.io>
 * David Alonso <david.alonso@solvos.es>
