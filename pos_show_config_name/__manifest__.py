b1866b6cee0cc7823c406a58f3970637bf036657
Pierrick Brun <pierrick.brun@akretion.com>
2021-01-25 17:18:55 +0100

    [ADD] pos_show_config_name
    (cherry picked from commit f897a35dd2e96ebd8bdbbce66c52f0b09cab7b50)

---
+++ pos_show_config_name/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Akretion (https://www.akretion.com).
+# @author Pierrick Brun <pierrick.brun@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "PoS show config name",
+    "version": "14.0.1.0.0",
+    "category": "Point Of Sale",
+    "summary": "Point of sale: show pos config name",
+    "author": "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pos",
+    "license": "AGPL-3",
+    "depends": ["point_of_sale"],
+    "data": [],
+    "qweb": ["static/src/xml/CashierName.xml"],
+    "installable": True,
+}
