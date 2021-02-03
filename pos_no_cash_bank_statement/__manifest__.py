d3239a761bf1976cd9651e4a9617df8328c4af19
Alexis de Lattre <alexis.delattre@akretion.com>
2021-02-03 02:05:21 +0100

    Add module pos_no_cash_bank_statement

---
+++ pos_no_cash_bank_statement/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "POS No Cash Bank Statement",
+    "version": "14.0.1.0.0",
+    "category": "Sales/Point of Sale",
+    "license": "AGPL-3",
+    "summary": "Generate bank statements for all payment methods, not only cash",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "website": "https://github.com/OCA/pos",
+    "depends": ["point_of_sale"],
+    "data": [
+        "views/pos_payment_method.xml",
+    ],
+    "installable": True,
+}
