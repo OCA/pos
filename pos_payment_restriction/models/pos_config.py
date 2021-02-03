1075342eaa963bea37a0fa887eb5883bb2c1e8c6
hda <hughes.damry@acsone.eu>
2021-02-03 19:37:01 +0100

    [MIG] pos_payment_restriction: Migration to 13.0

--- pos_payment_restriction/models/pos_config.py
+++ pos_payment_restriction/models/pos_config.py
@@ -2,13 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from openerp import models, fields
+from odoo import fields, models
 
 
 class PosConfig(models.Model):
 
-    _inherit = 'pos.config'
+    _inherit = "pos.config"
 
     payment_amount_readonly = fields.Boolean(
         help="Payment lines amount would be readonly. This should be used"
-             "with the 'Prefill Cash Payment' option")
+        "with the 'Prefill Cash Payment' option"
+    )
