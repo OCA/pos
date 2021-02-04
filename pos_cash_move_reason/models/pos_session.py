ad7cd078ec0e006a22fa79a426b844774dc10e42
François Honoré <francois.honore@acsone.eu>
2021-02-04 14:12:25 +0100

    [IMP] pos_cash_move_reason: black, isort, prettier

--- pos_cash_move_reason/models/pos_session.py
+++ pos_cash_move_reason/models/pos_session.py
@@ -6,34 +6,36 @@ from odoo import api, fields, models
 
 
 class PosSession(models.Model):
-    _inherit = 'pos.session'
+    _inherit = "pos.session"
 
-    display_move_reason_income = fields.Boolean(
-        compute='_compute_display_move_reason')
+    display_move_reason_income = fields.Boolean(compute="_compute_display_move_reason")
 
-    display_move_reason_expense = fields.Boolean(
-        compute='_compute_display_move_reason')
+    display_move_reason_expense = fields.Boolean(compute="_compute_display_move_reason")
 
     @api.multi
     def _compute_display_move_reason(self):
-        MoveReason = self.env['pos.move.reason']
+        MoveReason = self.env["pos.move.reason"]
         for session in self:
             # Get all reasons
-            reasons = MoveReason.search([
-                ('company_id', '=', session.config_id.company_id.id)])
+            reasons = MoveReason.search(
+                [("company_id", "=", session.config_id.company_id.id)]
+            )
             session.display_move_reason_income = len(
-                reasons.filtered(lambda x: x.is_income_reason))
+                reasons.filtered(lambda x: x.is_income_reason)
+            )
             session.display_move_reason_expense = len(
-                reasons.filtered(lambda x: x.is_expense_reason))
+                reasons.filtered(lambda x: x.is_expense_reason)
+            )
 
     def button_move_income(self):
-        return self._button_move_reason('income')
+        return self._button_move_reason("income")
 
     def button_move_expense(self):
-        return self._button_move_reason('expense')
+        return self._button_move_reason("expense")
 
     def _button_move_reason(self, move_type):
         action = self.env.ref(
-            'pos_cash_move_reason.action_wizard_pos_move_reason').read()[0]
-        action['context'] = {'default_move_type': move_type}
+            "pos_cash_move_reason.action_wizard_pos_move_reason"
+        ).read()[0]
+        action["context"] = {"default_move_type": move_type}
         return action
