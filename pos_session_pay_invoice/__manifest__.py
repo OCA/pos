4c6eacb5bd29875745a76a5263fd5bbaf4528f58
François Honoré <francois.honore@acsone.eu>
2021-02-05 08:53:07 +0100

    [IMP] pos_session_pay_invoice: black, isort, prettier

--- pos_session_pay_invoice/__manifest__.py
+++ pos_session_pay_invoice/__manifest__.py
@@ -2,18 +2,15 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    'name': 'POS Session Pay invoice',
-    'version': '12.0.1.0.1',
-    'category': 'Point Of Sale',
-    'author': "Creu Blanca,"
-              "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/pos',
-    'summary': 'Pay and receive invoices from PoS Session',
+    "name": "POS Session Pay invoice",
+    "version": "12.0.1.0.1",
+    "category": "Point Of Sale",
+    "author": "Creu Blanca," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pos",
+    "summary": "Pay and receive invoices from PoS Session",
     "license": "LGPL-3",
-    'depends': [
-        "point_of_sale", "account_cash_invoice"
-    ],
-    'data': [
+    "depends": ["point_of_sale", "account_cash_invoice"],
+    "data": [
         "wizard/cash_invoice_out.xml",
         "wizard/cash_invoice_in.xml",
         "views/pos_session.xml",
