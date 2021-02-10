27d580e7d47fb0184e6963e2727b33f1aedd7ebf
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-02-10 10:04:20 -0300

    [IMP] Simplify code by using underscore methods
    This improves readability
    Also, avoid setting the click event handler if the option is disabled in pos.config

--- pos_require_product_quantity/static/src/js/screens.js
+++ pos_require_product_quantity/static/src/js/screens.js
@@ -16,39 +16,28 @@ odoo.define('pos_require_product_quantity.screens', function (require) {
             var self = this;
             this._super();
 
-            this.$('.pay').click(function () {
-
-                if (self.pos.config.require_product_quantity) {
-
-                    var orderlines = self.pos.get_order().orderlines;
-                    var qty_unset_list = [];
-
-                    for (var i = 0; i < orderlines.length; i++) {
-                        var line = orderlines.models[i];
-                        if (line.quantity === 0) {
-                            qty_unset_list.push(line);
-                        }
-                    }
-                    if (qty_unset_list.length > 0) {
+            if (self.pos.config.require_product_quantity) {
+                this.$('.pay').click(function () {
+                    var lines_without_qty = _.filter(
+                        self.pos.get_order().get_orderlines(),
+                        function(line) { return line.quantity === 0 }
+                    );
+                    if (lines_without_qty.length > 0) {
                         self.gui.back();
-                        var body = _t('No quantity set for products:');
-                        for (var j = 0; j < qty_unset_list.length; j++) {
-                            body = (
-                                body
-                                + '  - '
-                                + qty_unset_list[j].product.display_name
-                            );
-                        }
                         self.gui.show_popup(
                             'alert',
                             {
                                 'title': _t('Missing quantities'),
-                                'body': body,
+                                'body': (
+                                    _t('No quantity set for products:')
+                                    + '\n'
+                                    + _.map(lines_without_qty, function(line) { return ' - ' + line.product.display_name }).join('\n')
+                                ),
                             },
                         );
                     }
-                }
-            });
+                });
+            }
         }
     });
 
