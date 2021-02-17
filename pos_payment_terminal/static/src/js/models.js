1cbf7911adcf553970b9a5282ddad29aec266d10
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-02-17 17:08:31 +0100

    [IMP] pos_payment_terminal: handle terminal transaction statuses

--- pos_payment_terminal/static/src/js/models.js
+++ pos_payment_terminal/static/src/js/models.js
@@ -4,6 +4,8 @@
     Copyright (C) 2014-2020 Akretion (www.akretion.com)
     @author: Aurélien DUMAINE
     @author: Alexis de Lattre <alexis.delattre@akretion.com>
+    @author: Denis Roussel <denis.roussel@acsone.eu>
+    @author: Stéphane Bidoul <stephane.bidoul@acsone.eu>
     License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 */
 
@@ -11,7 +13,40 @@ odoo.define("pos_payment_terminal.models", function (require) {
     "use strict";
 
     var models = require("point_of_sale.models");
+
     var OCAPaymentTerminal = require("pos_payment_terminal.payment");
     models.register_payment_method("oca_payment_terminal", OCAPaymentTerminal);
     models.load_fields("pos.payment.method", "oca_payment_terminal_mode");
+
+    var _paymentlineproto = models.Paymentline.prototype;
+    models.Paymentline = models.Paymentline.extend({
+        initialize: function () {
+            _paymentlineproto.initialize.apply(this, arguments);
+            // Id of the terminal transaction, used to find the payment
+            // line corresponding to a terminal transaction status coming
+            // from the terminal driver.
+            this.terminal_transaction_id = null;
+            // Success: null: in progress, false: failed: true: succeeded
+            this.terminal_transaction_success = null;
+            // Human readable transaction status, set if the transaction failed.
+            this.terminal_transaction_status = null;
+            // Additional information about the transaction status.
+            this.terminal_transaction_status_details = null;
+        },
+        init_from_JSON: function (json) {
+            _paymentlineproto.init_from_JSON.apply(this, arguments);
+            this.terminal_transaction_id = json.terminal_transaction_id;
+            this.terminal_transaction_success = json.terminal_transaction_success;
+            this.terminal_transaction_status = json.terminal_transaction_status;
+            this.terminal_transaction_status_details =
+                json.terminal_transaction_status_details;
+        },
+        export_as_JSON: function () {
+            var vals = _paymentlineproto.export_as_JSON.apply(this, arguments);
+            vals.terminal_transaction_id = this.terminal_transaction_id;
+            vals.terminal_transaction_success = this.terminal_transaction_success;
+            vals.terminal_transaction_status_details = this.terminal_transaction_status_details;
+            return vals;
+        },
+    });
 });
