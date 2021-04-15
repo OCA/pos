4ef5c5253ea77a3d598fa06e1edf6522fd559a72
dsolanki <dhara.solanki@initos.com>
2021-04-15 11:46:40 +0530

    [IMP] pos_timeout: black, isort, prettier

--- pos_timeout/static/src/js/pos_timeout.js
+++ pos_timeout/static/src/js/pos_timeout.js
@@ -4,7 +4,7 @@
     License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  *****************************************************************************/
 
-odoo.define("pos_timeout.models", function(require) {
+odoo.define("pos_timeout.models", function (require) {
     "use strict";
     var models = require("point_of_sale.models");
 
@@ -15,7 +15,7 @@ odoo.define("pos_timeout.models", function(require) {
     var PosModelParent = models.PosModel.prototype;
     models.PosModel = models.PosModel.extend({
         // eslint-disable-next-line no-unused-vars
-        _save_to_server: function(orders, options) {
+        _save_to_server: function (orders, options) {
             // Get PoS Config Settings
             var timeout = this.config.pos_order_timeout;
             if (timeout > 0 && orders && orders.length) {
