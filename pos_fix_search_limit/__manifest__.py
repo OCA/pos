d4fd618b5d58e7a2994e7ebb6ee99eea7d972e8d
dsolanki <dhara.solanki@initos.com>
2021-04-16 15:50:41 +0530

    [MIG] Migrate pos_fix_search_limit to v13.

--- pos_fix_search_limit/__manifest__.py
+++ pos_fix_search_limit/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Pos Fix Search Limit",
     "summary": "Increase search in the PoS",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Point of Sale",
     "author": "Akretion, Odoo Community Association (OCA)",
     "license": "AGPL-3",
