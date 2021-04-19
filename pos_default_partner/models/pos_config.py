67b6e818186d163b72a36bac98690c2089066b82
fshah <foram.shah@initos.com>
2021-04-19 16:25:11 +0530

    [IMP] pos_default_partner: isort, black, prettier

--- pos_default_partner/models/pos_config.py
+++ pos_default_partner/models/pos_config.py
@@ -9,5 +9,6 @@ class PosConfig(models.Model):
     _inherit = "pos.config"
 
     default_partner_id = fields.Many2one(
-        comodel_name="res.partner", string="Default Customer",
+        comodel_name="res.partner",
+        string="Default Customer",
     )
