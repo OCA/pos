12b3a33fcc3dd4f75e5ed62c3cb3513ae139facf
fshah <foram.shah@initos.com>
2021-04-19 16:29:13 +0530

    [MIG] Migrate module pos_default_partner to v14.

--- pos_default_partner/__manifest__.py
+++ pos_default_partner/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Point Of Sale Default Partner",
     "summary": "Add a default customer in pos order",
     "license": "AGPL-3",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "author": "Akretion," "Odoo Community Association (OCA)",
     "maintainer": "Akretion",
     "category": "Point of sale",
