cf6911f25e5b6e4b4088d19d4d3b65abb81174ef
dsolanki <dhara.solanki@initos.com>
2021-04-28 15:02:22 +0530

    [IMP] pos_supplierinfo_search: isort, black, prettier.

--- pos_supplierinfo_search/models/product.py
+++ pos_supplierinfo_search/models/product.py
@@ -1,23 +1,26 @@
 import json
-from odoo import models, fields, api
+
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     # technical field used in POS frontend
     supplier_data_json = fields.Char(
-        "Supplier data dict", readonly=True,
-        compute="_compute_supplier_data_json")
+        "Supplier data dict", readonly=True, compute="_compute_supplier_data_json"
+    )
 
     @api.multi
     def _compute_supplier_data_json(self):
         for t in self:
             res = []
             for s in t.seller_ids:
-                res.append({
-                    'supplier_name': s.name.display_name,
-                    'supplier_product_code': s.product_code or '',
-                    'supplier_product_name': s.product_name or '',
-                })
+                res.append(
+                    {
+                        "supplier_name": s.name.display_name,
+                        "supplier_product_code": s.product_code or "",
+                        "supplier_product_name": s.product_name or "",
+                    }
+                )
             t.supplier_data_json = json.dumps(res)
