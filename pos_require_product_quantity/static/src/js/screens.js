2f3a8a176767eb600943c74da5749d5410577759
fshah <foram.shah@initos.com>
2021-04-29 17:57:50 +0530

    [MIG] Migrate module pos_require_product_quantity to v14.

--- pos_require_product_quantity/static/src/js/screens.js
+++ pos_require_product_quantity/static/src/js/screens.js
@@ -7,39 +7,38 @@
 odoo.define("pos_require_product_quantity.screens", function (require) {
     "use strict";
 
-    var screens = require("point_of_sale.screens");
+    const ProductScreen = require("point_of_sale.ProductScreen");
+    const Registries = require("point_of_sale.Registries");
+    const {Gui} = require("point_of_sale.Gui");
     var core = require("web.core");
     var _t = core._t;
 
-    screens.ActionpadWidget.include({
-        renderElement: function () {
-            var self = this;
-            this._super();
-
-            if (self.pos.config.require_product_quantity) {
-                this.$(".pay").click(function () {
-                    var lines_without_qty = _.filter(
-                        self.pos.get_order().get_orderlines(),
-                        function (line) {
-                            return line.quantity === 0;
-                        }
-                    );
-                    if (lines_without_qty.length > 0) {
-                        self.gui.back();
-                        self.gui.show_popup("alert", {
-                            title: _t("Missing quantities"),
-                            body:
-                                _t("No quantity set for products:") +
-                                "\n" +
-                                _.map(lines_without_qty, function (line) {
-                                    return " - " + line.product.display_name;
-                                }).join("\n"),
-                        });
+    const CustomProductScreen = (ProductScreen) =>
+        class extends ProductScreen {
+            _onClickPay() {
+                const result = super._onClickPay(...arguments);
+                var lines_without_qty = _.filter(
+                    this.env.pos.get_order().get_orderlines(),
+                    function (line) {
+                        return line.quantity === 0;
                     }
-                });
+                );
+                if (lines_without_qty.length > 0) {
+                    this.showScreen("ProductScreen");
+                    Gui.showPopup("ConfirmPopup", {
+                        title: _t("Missing quantities"),
+                        body:
+                            _t("No quantity set for products:") +
+                            "\n" +
+                            _.map(lines_without_qty, function (line) {
+                                return " - " + line.product.display_name;
+                            }).join("\n"),
+                    });
+                }
+                return result;
             }
-        },
-    });
+        };
 
-    return screens;
+    Registries.Component.extend(ProductScreen, CustomProductScreen);
+    return ProductScreen;
 });
