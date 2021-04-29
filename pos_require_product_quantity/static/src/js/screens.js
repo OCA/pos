fa7c86f86139f958fed35cd8f5b1f270cfaf14d7
fshah <foram.shah@initos.com>
2021-04-29 17:54:54 +0530

    [IMP] pos_require_product_quantity: isort, black, prettier.

--- pos_require_product_quantity/static/src/js/screens.js
+++ pos_require_product_quantity/static/src/js/screens.js
@@ -4,11 +4,11 @@
   License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 */
 
-odoo.define('pos_require_product_quantity.screens', function (require) {
+odoo.define("pos_require_product_quantity.screens", function (require) {
     "use strict";
 
     var screens = require("point_of_sale.screens");
-    var core = require('web.core');
+    var core = require("web.core");
     var _t = core._t;
 
     screens.ActionpadWidget.include({
@@ -17,28 +17,28 @@ odoo.define('pos_require_product_quantity.screens', function (require) {
             this._super();
 
             if (self.pos.config.require_product_quantity) {
-                this.$('.pay').click(function () {
+                this.$(".pay").click(function () {
                     var lines_without_qty = _.filter(
                         self.pos.get_order().get_orderlines(),
-                        function(line) { return line.quantity === 0 }
+                        function (line) {
+                            return line.quantity === 0;
+                        }
                     );
                     if (lines_without_qty.length > 0) {
                         self.gui.back();
-                        self.gui.show_popup(
-                            'alert',
-                            {
-                                'title': _t('Missing quantities'),
-                                'body': (
-                                    _t('No quantity set for products:')
-                                    + '\n'
-                                    + _.map(lines_without_qty, function(line) { return ' - ' + line.product.display_name }).join('\n')
-                                ),
-                            },
-                        );
+                        self.gui.show_popup("alert", {
+                            title: _t("Missing quantities"),
+                            body:
+                                _t("No quantity set for products:") +
+                                "\n" +
+                                _.map(lines_without_qty, function (line) {
+                                    return " - " + line.product.display_name;
+                                }).join("\n"),
+                        });
                     }
                 });
             }
-        }
+        },
     });
 
     return screens;
