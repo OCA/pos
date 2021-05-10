fcf612bf25133b89ebb458d7667c3648249368bc
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-05-10 17:47:36 -0300

    [IMP] Make empty home configurable on each pos.config

--- pos_empty_home/static/src/js/db.js
+++ pos_empty_home/static/src/js/db.js
@@ -12,10 +12,10 @@ odoo.define("pos_empty_home.db", function(require) {
 
     PosDB.include({
         get_product_by_category: function(category_id) {
-            if (category_id != 0) {
-                return this._super(category_id);
+            if (window.posmodel.config.iface_empty_home && category_id == 0) {
+                return [];
             }
-            return [];
+            return this._super.apply(this, arguments);
         },
     });
 
