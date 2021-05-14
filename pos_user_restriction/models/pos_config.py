ff249b71219a891c04671b35082a842860b69e91
hkapatel <helly.kapatel@initos.com>
2021-05-15 00:55:26 +0530

    [IMP]pos_user_restriction: black, isort, prettier

--- pos_user_restriction/models/pos_config.py
+++ pos_user_restriction/models/pos_config.py
@@ -1,10 +1,12 @@
-from odoo import models, fields
+from odoo import fields, models
 
 
 class PosConfig(models.Model):
-    _inherit = 'pos.config'
+    _inherit = "pos.config"
     assigned_user_ids = fields.Many2many(
-        "res.users", string="Assigned users",
+        "res.users",
+        string="Assigned users",
         help="Restrict some users to only access their assigned points of sale. "
-             "In order to apply the restriction, the user needs the "
-             "'User: Assigned POS Only' group")
+        "In order to apply the restriction, the user needs the "
+        "'User: Assigned POS Only' group",
+    )
