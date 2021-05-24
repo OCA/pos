66fd02e878950bbfac95de5566667da283ad2f22
dsolanki <dhara.solanki@initos.com>
2021-05-24 16:24:03 +0530

    [IMP] pos_reset_search: isort, black, prettier.

--- pos_reset_search/static/src/js/reset.js
+++ pos_reset_search/static/src/js/reset.js
@@ -1,6 +1,6 @@
-odoo.define('product_click_search_reset', function (require) {
+odoo.define("product_click_search_reset", function (require) {
     "use strict";
-    var screens = require('point_of_sale.screens');
+    var screens = require("point_of_sale.screens");
 
     screens.ProductScreenWidget.include({
         click_product: function (product) {
