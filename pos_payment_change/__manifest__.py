fb415b4354929cbc984e2b299b33b5d9e5f3570b
fshah <foram.shah@initos.com>
2021-05-25 17:11:45 +0530

    [MIG] Migrate module pos_payment_change to v14.

--- pos_payment_change/__manifest__.py
+++ pos_payment_change/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Point Of Sale - Change Payments",
-    "version": "12.0.1.0.2",
+    "version": "14.0.1.0.0",
     "summary": "Allow cashier to change order payments, as long as"
     " the session is not closed.",
     "category": "Point Of Sale",
@@ -14,6 +14,7 @@
     "maintainers": ["legalsylvain"],
     "development_status": "Beta",
     "data": [
+        "security/ir.model.access.csv",
         "wizards/view_pos_payment_change_wizard.xml",
         "views/view_pos_config.xml",
         "views/view_pos_order.xml",
