6afa5037dfb07aef78398878d42c3bf1ae1d27b4
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-06-10 13:19:51 +0200

    [IMP] pos_margin : display margin and margin rate in front-end UI
    Co-authored-by: Iván Todorovich <ivan.todorovich@gmail.com>

--- pos_margin/__manifest__.py
+++ pos_margin/__manifest__.py
@@ -5,18 +5,24 @@
 {
     'name': 'PoS Order Margin',
     'summary': 'Margin on PoS Order',
-    'version': '12.0.1.0.0',
+    'version': '12.0.2.0.0',
     'category': 'Point Of Sale',
     'author': "GRAP,"
               "Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/pos',
-    'license': 'AGPL-3',
-    'depends': [
-        'point_of_sale',
-        'sale_margin',
+    "maintainers": ["legalsylvain"],
+    "website": "https://github.com/OCA/pos",
+    "license": "AGPL-3",
+    "depends": [
+        "point_of_sale",
+        "sale_margin",
     ],
     'data': [
+        'views/templates.xml',
+        'views/view_pos_config.xml',
         'views/view_pos_order.xml',
+    ],
+    'qweb': [
+        'static/src/xml/pos_margin.xml',
     ],
     'installable': True,
 }
---
+++ pos_margin/readme/CONFIGURE.rst
@@ -0,0 +1,4 @@
+* If you want to disable the display of the margin, in the front-office UI, you can
+  uncheck the check box in the ``pos.config`` form:
+
+.. figure:: ../static/description/pos_config_form.png
