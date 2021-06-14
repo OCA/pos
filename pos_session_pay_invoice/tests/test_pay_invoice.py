2d5f6e69628ae5b10b3d54af5952c9648bcf76d1
Olga Marco <olga.marco@creublanca.es>
2021-06-14 18:17:40 +0200

    [MIG] pos_session_pay_invoice: Ending migration to 13.0

--- pos_session_pay_invoice/tests/test_pay_invoice.py
+++ pos_session_pay_invoice/tests/test_pay_invoice.py
@@ -1,110 +1,136 @@
 # Copyright 2017 Creu Blanca <https://creublanca.es/>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo.tests.common import SavepointCase
+from odoo.tests import Form
+
+from odoo.addons.point_of_sale.tests.common import TestPointOfSaleCommon
 
 
-class TestSessionPayInvoice(SavepointCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
-        cls.company = cls.env.ref("base.main_company")
-        partner = cls.env.ref("base.partner_demo")
-        cls.invoice_out = cls.env["account.move"].create(
-            {
-                "company_id": cls.company.id,
-                "partner_id": partner.id,
-                "date_invoice": "2016-03-12",
-                "type": "out_invoice",
-            }
-        )
-        account = cls.env["account.account"].create(
+class TestSessionPayInvoice(TestPointOfSaleCommon):
+    def setUp(self):
+        super().setUp()
+        self.pos_config.cash_control = True
+
+        account = self.env["account.account"].create(
             {
                 "code": "test_cash_pay_invoice",
-                "company_id": cls.company.id,
+                "company_id": self.company.id,
                 "name": "Test",
-                "user_type_id": cls.env.ref("account.data_account_type_revenue").id,
+                "user_type_id": self.env.ref("account.data_account_type_revenue").id,
+                "reconcile": True,
             }
         )
-        cls.env["account.move.line"].create(
+
+        self.invoice_out = self.env["account.move"].create(
             {
-                "product_id": cls.env.ref("product.product_delivery_02").id,
-                "invoice_id": cls.invoice_out.id,
-                "account_id": account.id,
-                "name": "Producto de prueba",
-                "quantity": 1.0,
-                "price_unit": 100.0,
+                "company_id": self.company.id,
+                "partner_id": self.partner1.id,
+                "date": "2016-03-12",
+                "type": "out_invoice",
+                "invoice_line_ids": [
+                    0,
+                    0,
+                    {
+                        "product_id": self.product3.id,
+                        "name": "Producto de prueba",
+                        "account_id": account.id,
+                        "quantity": 1.0,
+                        "price_unit": 100.0,
+                    },
+                ],
             }
         )
-        cls.invoice_out._onchange_invoice_line_ids()
-        cls.invoice_out.action_invoice_open()
-        cls.invoice_out.number = "2999/99999"
-        cls.invoice_in = cls.env["account.move"].create(
+        self.invoice_out._onchange_invoice_line_ids()
+        self.invoice_out.post()
+        self.invoice_in = self.env["account.move"].create(
             {
-                "partner_id": partner.id,
-                "company_id": cls.company.id,
+                "partner_id": self.partner4.id,
+                "company_id": self.company.id,
                 "type": "in_invoice",
-                "date_invoice": "2016-03-12",
+                "date": "2016-03-12",
+                "invoice_line_ids": [
+                    0,
+                    0,
+                    {
+                        "product_id": self.product3.id,
+                        "name": "Producto de prueba",
+                        "account_id": account.id,
+                        "quantity": 1.0,
+                        "price_unit": 100.0,
+                    },
+                ],
             }
         )
-        cls.env["account.move.line"].create(
-            {
-                "product_id": cls.env.ref("product.product_delivery_02").id,
-                "invoice_id": cls.invoice_in.id,
-                "name": "Producto de prueba",
-                "account_id": account.id,
-                "quantity": 1.0,
-                "price_unit": 100.0,
-            }
+        self.invoice_in._onchange_invoice_line_ids()
+        self.invoice_in.post()
+        refund_wizard = (
+            self.env["account.move.reversal"]
+            .with_context(
+                active_ids=self.invoice_out.ids,
+                active_id=self.invoice_out.id,
+                active_model=self.invoice_out._name,
+            )
+            .create({})
+            .reverse_moves()
         )
-        cls.invoice_in._onchange_invoice_line_ids()
-        cls.invoice_in.action_invoice_open()
-        cls.invoice_in.number = "2999/99999"
-        cls.config = cls.env.ref("point_of_sale.pos_config_main")
-        cls.config.cash_control = True
+        self.refund = self.env[refund_wizard["res_model"]].browse(
+            refund_wizard["res_id"]
+        )
+        self.refund.post()
 
-        cls.account_cash_differences_id = cls.env["account.account"].create(
-            {
-                "code": "test_cash_differences",
-                "company_id": cls.company.id,
-                "name": "Test Cash Differences",
-                "user_type_id": cls.env.ref("account.data_account_type_revenue").id,
-            }
-        )
-
-    def test_pos_invoice(self):
-        self.config.open_session_cb()
-        session = self.config.current_session_id
+    def test_pos_in_invoice(self):
+        self.assertEqual(self.invoice_in.amount_residual, 100.0)
+        self.pos_config.open_session_cb()
+        session = self.pos_config.current_session_id
         self.assertIsNotNone(session.statement_ids)
         cash_statements = session.statement_ids.filtered(
             lambda x: x.journal_id.type == "cash"
         )
         self.assertEquals(len(cash_statements), 1)
-        journal = cash_statements[0].journal_id
-        journal.profit_account_id = self.account_cash_differences_id
-        journal.loss_account_id = self.account_cash_differences_id
-
         session.action_pos_session_open()
-        in_invoice = (
-            self.env["cash.invoice.in"]
-            .with_context(active_ids=session.ids, active_model="pos.session")
-            .create({"invoice_id": self.invoice_in.id, "amount": 100.0})
+        cash_in = self.env["cash.invoice.in"].with_context(
+            active_ids=session.ids, active_model="pos.session"
         )
-        in_invoice.run()
-        out_invoice = (
-            self.env["cash.invoice.out"]
-            .with_context(active_ids=session.ids, active_model="pos.session")
-            .create({"invoice_id": self.invoice_out.id, "amount": 75.0})
-        )
-        out_invoice.run()
-        box = (
-            self.env["cash.box.in"]
-            .with_context(active_ids=session.ids, active_model="pos.session")
-            .create({"name": "Testing", "amount": 25.0})
-        )
-        box.run()
+        with Form(cash_in) as form:
+            form.invoice_id = self.invoice_in
+            self.assertEqual(form.amount, -100)
+        cash_in.browse(form.id).run()
         session.action_pos_session_closing_control()
-        session.action_pos_session_validate()
-        self.assertEqual(self.invoice_out.residual, 25.0)
-        self.assertEqual(self.invoice_in.residual, 0.0)
+        session._validate_session()
+        self.assertEqual(self.invoice_in.amount_residual, 0.0)
+
+    def test_pos_out_invoice(self):
+        self.assertEqual(self.invoice_out.amount_residual, 100.0)
+        self.pos_config.open_session_cb()
+        session = self.pos_config.current_session_id
+        out_invoice = self.env["pos.box.cash.invoice.out"].with_context(
+            active_ids=session.ids,
+            active_model="pos.session",
+            default_session_id=session.id,
+        )
+        with Form(out_invoice) as form:
+            form.move_id = self.invoice_out
+            self.assertEqual(form.amount, 100)
+            form.amount = 75
+        out_invoice.browse(form.id).run()
+        session.action_pos_session_closing_control()
+        session._validate_session()
+        self.assertEqual(self.invoice_out.amount_residual, 25.0)
+
+    def test_pos_invoice_refund(self):
+        self.assertEqual(self.refund.amount_residual, 100.0)
+        self.pos_config.open_session_cb()
+        session = self.pos_config.current_session_id
+        in_invoice = self.env["pos.box.cash.invoice.in"].with_context(
+            active_ids=session.ids,
+            active_model="pos.session",
+            default_session_id=session.id,
+        )
+        with Form(in_invoice) as form:
+            form.move_id = self.refund
+            self.assertEqual(form.amount, -100)
+        in_invoice.browse(form.id).run()
+        session.action_pos_session_closing_control()
+        session._validate_session()
+        self.refund.refresh()
+        self.assertEqual(self.refund.amount_residual, 0.0)
