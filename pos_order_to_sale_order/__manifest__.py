ce3da8f88b3b5c1d12b24e5d0c6877ecc9dcc911
manu <manuel.regidor@sygel.es>
2021-06-28 16:58:09 +0200

    [IMP] pos_order_to_sale_order: black, isort, prettier

--- pos_order_to_sale_order/__manifest__.py
+++ pos_order_to_sale_order/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "PoS Order To Sale Order",
-    "version": "12.0.1.0.3",
+    "version": "13.0.1.0.3",
     "author": "GRAP,Odoo Community Association (OCA)",
     "category": "Point Of Sale",
     "license": "AGPL-3",
@@ -12,9 +12,6 @@
     "maintainers": ["legalsylvain"],
     "development_status": "Production/Stable",
     "website": "https://github.com/OCA/pos",
-    "data": [
-        "views/view_pos_config.xml",
-        "views/assets.xml",
-    ],
+    "data": ["views/view_pos_config.xml", "views/assets.xml"],
     "qweb": ["static/src/xml/pos_order_to_sale_order.xml"],
 }
