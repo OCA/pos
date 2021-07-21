6f5be4971dc9e22d0a0d2980dc3a029a9b272962
fshah <foram.shah@initos.com>
2021-07-21 15:19:36 +0530

    [IMP] pos_order_remove_line: isort, black , prettier.

--- pos_order_remove_line/static/src/js/order_line.js
+++ pos_order_remove_line/static/src/js/order_line.js
@@ -3,11 +3,11 @@
  *  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
  */
 
-odoo.define("pos_order_remove_line.order_line", function(require) {
+odoo.define("pos_order_remove_line.order_line", function (require) {
     "use strict";
 
     require("point_of_sale.screens").OrderWidget.include({
-        render_orderline: function() {
+        render_orderline: function () {
             var node = this._super.apply(this, arguments);
 
             $(node)
@@ -16,7 +16,7 @@ odoo.define("pos_order_remove_line.order_line", function(require) {
 
             return node;
         },
-        remove_line: function(ev) {
+        remove_line: function (ev) {
             ev.delegateTarget.parentElement.orderline.set_quantity("remove");
         },
     });
