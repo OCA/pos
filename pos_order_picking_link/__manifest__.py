b9043641b3fadb4a830ae8fc0ed8c2424aa3e7c9
Luis Felipe Mileo <mileo@kmee.com.br>
2021-07-24 09:05:14 -0300

    [MIG] Version 12.0

---
+++ pos_order_picking_link/__manifest__.py
@@ -0,0 +1,17 @@
+# © 2016 KMEE INFORMATICA LTDA (https://kmee.com.br)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'POS Order Picking Link',
+    'version': '12.0.1.0.0',
+    'category': 'Point of Sale',
+    'author': 'KMEE, Odoo Community Association (OCA)',
+    'website': 'https://github.com/oca/pos',
+    'license': 'LGPL-3',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/stock_view.xml',
+    ],
+    'maintainers': ['sadamo', 'mileo'],
+    'installable': True,
+}
---
+++ pos_order_picking_link/readme/CONTRIBUTORS.rst
@@ -0,0 +1,3 @@
+* `KMEE <https://kmee.com.br>`__:
+    * Luis Felipe Mileo <mileo@kmee.com.br>
+    * Daniel Sadamo <daniel.sadamo@kmee.com.br>
