7060d172a54c332c2ce88b911556b29f8f1d04a6
hkapatel <helly.kapatel@initos.com>
2021-07-29 12:44:21 +0530

    [IMP] pos_empty_home: black, isort, prettier

--- pos_empty_home/static/src/js/db.js
+++ pos_empty_home/static/src/js/db.js
@@ -5,13 +5,13 @@
     License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 */
 
-odoo.define("pos_empty_home.db", function(require) {
+odoo.define("pos_empty_home.db", function (require) {
     "use strict";
 
     var PosDB = require("point_of_sale.DB");
 
     PosDB.include({
-        get_product_by_category: function(category_id) {
+        get_product_by_category: function (category_id) {
             if (window.posmodel.config.iface_empty_home && category_id == 0) {
                 return [];
             }
