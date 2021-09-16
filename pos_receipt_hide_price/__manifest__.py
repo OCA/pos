6511a49ea8d7202d9cad018f521a4627125c64d5
Florian Mounier <paradoxxx.zero@gmail.com>
2021-09-16 15:33:06 +0200

    [ADD] pos_receipt_hide_price: Add module

---
+++ pos_receipt_hide_price/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Akretion - Florian Mounier
+{
+    "name": "POS Receipt Hide Price",
+    "summary": "Add button to remove price from receipt.",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pos",
+    "category": "Point of Sale",
+    "version": "14.0.1.0.0",
+    "license": "LGPL-3",
+    "depends": ["point_of_sale"],
+    "data": ["views/assets.xml"],
+    "qweb": [
+        "static/src/xml/OrderReceipt.xml",
+        "static/src/xml/HidePrice.xml",
+        "static/src/xml/ReceiptScreen.xml",
+        "static/src/xml/ReprintReceiptScreen.xml",
+    ],
+}
---
+++ pos_receipt_hide_price/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module adds a button in the receipt view to hide price from the receipt for printing and emailing purpose.
