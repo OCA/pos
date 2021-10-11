28636b32afefeee0171784ebee6c3051d0174cf0
fshah <foram.shah@initos.com>
2021-10-11 14:59:21 +0530

    [IMP] pos_supplierinfo_barcode: black, isort, prettier

--- pos_supplierinfo_barcode/static/src/js/db.js
+++ pos_supplierinfo_barcode/static/src/js/db.js
@@ -1,23 +1,23 @@
-odoo.define('pos_supplierinfo_barcode.db', function (require) {
+odoo.define("pos_supplierinfo_barcode.db", function (require) {
     "use strict";
 
-    var PosDB = require('point_of_sale.DB');
-    var models = require('point_of_sale.models');
+    var PosDB = require("point_of_sale.DB");
+    var models = require("point_of_sale.models");
 
     models.load_fields("product.product", ["supplier_barcode_json"]);
 
     PosDB.include({
-        add_products: function(products) {
+        add_products: function (products) {
             var res = this._super(products);
 
-            if(!products instanceof Array){
+            if (!products instanceof Array) {
                 products = [products];
             }
-            for(var i = 0, len = products.length; i < len; i++){
+            for (var i = 0, len = products.length; i < len; i++) {
                 var product = products[i];
                 var supplier_barcode_list = JSON.parse(product.supplier_barcode_json);
 
-                for(var j = 0, jlen = supplier_barcode_list.length; j < jlen; j++){
+                for (var j = 0, jlen = supplier_barcode_list.length; j < jlen; j++) {
                     var supplier_barcode = supplier_barcode_list[j];
                     this.product_by_barcode[supplier_barcode] = product;
                 }
@@ -25,5 +25,4 @@ odoo.define('pos_supplierinfo_barcode.db', function (require) {
             return res;
         },
     });
-
 });
