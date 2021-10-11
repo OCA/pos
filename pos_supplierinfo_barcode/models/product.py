ae47721d66fd64b5176b3b62043d02ce87968ddf
fshah <foram.shah@initos.com>
2021-10-11 15:04:50 +0530

    [MIG] pos_supplierinfo_barcode: Migration to 14.0

--- pos_supplierinfo_barcode/models/product.py
+++ pos_supplierinfo_barcode/models/product.py
@@ -1,6 +1,6 @@
 import json
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
@@ -11,8 +11,8 @@ class ProductTemplate(models.Model):
         "Supplier barcode list", readonly=True, compute="_compute_supplier_barcode_json"
     )
 
-    @api.multi
     def _compute_supplier_barcode_json(self):
         for t in self:
-            supplier_barcode_json = [x for x in t.mapped("seller_ids.barcode") if x]
+            supplier_barcode_json = [
+                x for x in t.mapped("seller_ids.barcode") if x]
             t.supplier_barcode_json = json.dumps(supplier_barcode_json)
