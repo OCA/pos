dd7a0bec861cf85813ea8f2d8b9a850e1be5b8ba
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-12-23 15:22:10 +0100

    [ADD] pos_cash_move_reason_multiple_control : new module to show previous and new difference in the statement when using pos_cash_move_reason

---
+++ pos_cash_move_reason_multiple_control/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2021-Today: GTRAP (<http://www.grap.coop/>)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "POS cash in-out reason With multiple control",
+    "summary": "Glue module between pos_cash_move_reason"
+    " and pos_multiple_control",
+    "author": "GRAP,"
+              "Odoo Community Association (OCA)",
+    "maintainers": ["legalsylvain"],
+    "website": "https://www.github.com/OCA/pos",
+    "category": "Point Of sale",
+    "version": "12.0.3.0.2",
+    "license": "AGPL-3",
+    "depends": [
+        "pos_cash_move_reason",
+        "pos_multiple_control",
+    ],
+    "data": [
+        "wizard/wizard_pos_move_reason.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
