450f235a94064ad503cd4b6fd18c9b161b02599c
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-12-23 19:46:15 +0100

    [FIX] pos_payment_terminal: reject cancellation
    Since we can't cancel the transaction, we return a rejected
    promise. The user should cancel manually which will
    effectively free the transaction and let the user delete it.

--- pos_payment_terminal/static/src/js/payment_terminal.js
+++ pos_payment_terminal/static/src/js/payment_terminal.js
@@ -26,12 +26,12 @@ odoo.define("pos_payment_terminal.payment", function (require) {
 
         send_payment_cancel: function () {
             this._super.apply(this, arguments);
-            this._show_tip(
+            this._show_error(
                 _t(
-                    "Cancel the transaction by pressing the red button on the payment terminal."
+                    "Please press the red button on the payment terminal to cancel the transaction."
                 )
             );
-            return Promise.resolve();
+            return Promise.reject();
         },
 
         _oca_payment_terminal_pay: function () {
@@ -182,13 +182,6 @@ odoo.define("pos_payment_terminal.payment", function (require) {
         _show_error: function (msg, title) {
             Gui.showPopup("ErrorPopup", {
                 title: title || _t("Payment Terminal Error"),
-                body: msg,
-            });
-        },
-
-        _show_tip: function (msg, title) {
-            Gui.showPopup("ErrorPopup", {
-                title: title || _t("Payment Terminal"),
                 body: msg,
             });
         },
