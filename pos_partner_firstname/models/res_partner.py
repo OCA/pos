a784533f9a258a9acc4c5803097ff66ce2767704
dsolanki <dhara.solanki@initos.com>
2021-12-31 15:11:18 +0530

    [IMP] pos_partner_firstname: isort, black, prettier.

--- pos_partner_firstname/models/res_partner.py
+++ pos_partner_firstname/models/res_partner.py
@@ -5,12 +5,12 @@ from odoo import api, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     @api.model
     def create_from_ui(self, partner):
-        if 'is_company' in partner:
-            partner['is_company'] = partner['is_company'] == 'true'
+        if "is_company" in partner:
+            partner["is_company"] = partner["is_company"] == "true"
         return super(ResPartner, self).create_from_ui(partner)
 
     @api.model
--- pos_partner_firstname/static/src/js/models.js
+++ pos_partner_firstname/static/src/js/models.js
@@ -1,8 +1,7 @@
-odoo.define('pos_partner_firstname.models', function (require) {
+odoo.define("pos_partner_firstname.models", function (require) {
     "use strict";
 
-  var models = require('point_of_sale.models');
+    var models = require("point_of_sale.models");
 
-  models.load_fields("res.partner", ["is_company", "firstname", "lastname"]);
-
+    models.load_fields("res.partner", ["is_company", "firstname", "lastname"]);
 });
