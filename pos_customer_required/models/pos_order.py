ec344f8c33b06a3618e6342b964f0d61a2ac640b
KNVx <kniubo@nuobit.com>
2022-01-12 13:54:23 +0100

    [IMP] pos_customer_required: black, isort, prettier

--- pos_customer_required/models/pos_order.py
+++ pos_customer_required/models/pos_order.py
@@ -5,19 +5,20 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models, exceptions, api, _
+from odoo import _, api, exceptions, fields, models
 
 
 class PosOrder(models.Model):
-    _inherit = 'pos.order'
+    _inherit = "pos.order"
 
     require_customer = fields.Selection(
-        related='session_id.config_id.require_customer',
+        related="session_id.config_id.require_customer",
     )
 
-    @api.constrains('partner_id', 'session_id')
+    @api.constrains("partner_id", "session_id")
     def _check_partner(self):
         for rec in self:
-            if rec.require_customer != 'no' and not rec.partner_id:
-                raise exceptions.ValidationError(_(
-                    'Customer is required for this order and is missing.'))
+            if rec.require_customer != "no" and not rec.partner_id:
+                raise exceptions.ValidationError(
+                    _("Customer is required for this order and is missing.")
+                )
