a3fea8fd0d5f0a4833b4a497ac22363f973d88b7
Roberto Fichera <robyf@tekno-soft.it>
2022-01-19 12:32:28 +0530

    [MIG] Migrate module pos_partner_firstname to v14.

--- pos_partner_firstname/models/res_partner.py
+++ pos_partner_firstname/models/res_partner.py
@@ -8,12 +8,6 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     @api.model
-    def create_from_ui(self, partner):
-        if "is_company" in partner:
-            partner["is_company"] = partner["is_company"] == "true"
-        return super(ResPartner, self).create_from_ui(partner)
-
-    @api.model
     def get_names_order(self):
         """Allow POS frontend to retrieve 'partner_names_order'"""
         return self._get_names_order()
