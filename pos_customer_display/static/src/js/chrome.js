82e7dd9a341743614fb62a2055ff2fcd1f16b623
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-02-07 16:47:32 +0100

    [FIX] pos_customer_display : apply pre-commit check

--- pos_customer_display/static/src/js/chrome.js
+++ pos_customer_display/static/src/js/chrome.js
@@ -4,30 +4,26 @@
  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 */
 
-
-odoo.define('pos_customer_display.Chrome', function (require) {
+odoo.define("pos_customer_display.Chrome", function (require) {
     "use strict";
 
-    const Chrome = require('point_of_sale.Chrome');
-    const Registries = require('point_of_sale.Registries');
+    const Chrome = require("point_of_sale.Chrome");
+    const Registries = require("point_of_sale.Registries");
 
     const PosCustomerDisplay = (Chrome) =>
         class extends Chrome {
-
-        async _closePos() {
-            console.log('CLOSE POS START this=');
-            console.log(this);
-            var msg = this.env.pos.proxy.customer_display_proxy._prepare_message_close();
-            console.log(msg);
-            // TODO find a way to display the msg
-            await super._closePos();
-            console.log('CLOSE POS DONE');
-        }
-
-    };
+            async _closePos() {
+                console.log("CLOSE POS START this=");
+                console.log(this);
+                var msg = this.env.pos.proxy.customer_display_proxy._prepare_message_close();
+                console.log(msg);
+                // TODO find a way to display the msg
+                await super._closePos();
+                console.log("CLOSE POS DONE");
+            }
+        };
 
     Registries.Component.extend(Chrome, PosCustomerDisplay);
 
     return Chrome;
-
 });
