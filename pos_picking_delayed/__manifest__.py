1ed326a14d2942caaa61c839b3a257bb0b7043df
fshah <foram.shah@initos.com>
2022-02-15 18:08:25 +0530

    [14.0][MIG] pos_picking_delayed

--- pos_picking_delayed/__manifest__.py
+++ pos_picking_delayed/__manifest__.py
@@ -2,30 +2,29 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Point of Sale - Picking Creation Delayed',
-    'summary': 'Delay the creation of the picking when PoS order is created',
-    'version': '12.0.2.0.1',
-    'category': 'Point Of Sale',
-    'author': 'GRAP, '
-              'Odoo Community Association (OCA)',
-    'maintainers': ['legalsylvain'],
-    'license': 'AGPL-3',
-    'website': 'https://www.github.com/OCA/pos',
-    'depends': [
-        'point_of_sale',
-        'queue_job',
+    "name": "Point of Sale - Picking Creation Delayed",
+    "summary": "Delay the creation of the picking when PoS order is created",
+    "version": "14.0.1.0.0",
+    "category": "Point Of Sale",
+    "author": "GRAP, " "Odoo Community Association (OCA)",
+    "maintainers": ["legalsylvain"],
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/pos",
+    "depends": [
+        "point_of_sale",
+        "queue_job",
     ],
-    'data': [
-        'data/queue_job_channel.xml',
-        'data/queue_job_function.xml',
-        'views/view_pos_config.xml',
-        'views/view_pos_order.xml',
+    "data": [
+        "data/queue_job_channel.xml",
+        "data/queue_job_function.xml",
+        "views/view_pos_config.xml",
+        "views/view_pos_order.xml",
     ],
-    'demo': [
-        'demo/pos_config.xml',
+    "demo": [
+        "demo/pos_config.xml",
     ],
-    'images': [
-        'static/description/pos_order_tree.png',
+    "images": [
+        "static/description/pos_order_tree.png",
     ],
-    'installable': True,
+    "installable": True,
 }
--- pos_picking_delayed/models/pos_order.py
+++ pos_picking_delayed/models/pos_order.py
@@ -5,45 +5,54 @@ from odoo import api, fields, models
 
 
 class PosOrder(models.Model):
-    _inherit = 'pos.order'
+    _inherit = "pos.order"
 
     has_picking_delayed = fields.Boolean(
-        default=False, help="This checkbox is checked if the generation of"
-        " the picking has been delayed. The picking will be created by cron.")
+        default=False,
+        help="This checkbox is checked if the generation of"
+        " the picking has been delayed. The picking will be created by cron.",
+    )
 
     # Overload Section
     @api.model
-    def create_from_ui(self, orders):
-        PosSession = self.env['pos.session']
+    def create_from_ui(self, orders, draft=False):
+        PosSession = self.env["pos.session"]
         for order_data in orders:
-            session_id = order_data.get('data').get('pos_session_id')
+            session_id = order_data.get("data").get("pos_session_id")
             session = PosSession.browse(session_id)
-            order_data['data']['has_picking_delayed'] =\
-                session.config_id.picking_creation_delayed
-        return super(PosOrder, self.with_context(
-            create_from_ui=True)).create_from_ui(orders)
+            order_data["data"][
+                "has_picking_delayed"
+            ] = session.config_id.picking_creation_delayed
 
-    def create_picking(self):
-        if self.env.context.get('create_from_ui', False):
+        res = super(PosOrder, self.with_context(create_from_ui=True)).create_from_ui(
+            orders
+        )
+        return res
+
+    def _create_order_picking(self):
+        self.ensure_one()
+        if self.env.context.get("create_from_ui", False):
             orders = self.filtered(lambda x: not x.has_picking_delayed)
             delayed_orders = self.filtered(lambda x: x.has_picking_delayed)
             delayed_orders.with_delay()._create_delayed_picking()
+            if not orders:
+                orders = delayed_orders
         else:
             orders = self
-        res = super(PosOrder, orders).create_picking()
-        orders.write({'has_picking_delayed': False})
+
+        res = super(PosOrder, orders)._create_order_picking()
+        orders.write({"has_picking_delayed": False})
         return res
 
     @api.model
     def _order_fields(self, ui_order):
         res = super(PosOrder, self)._order_fields(ui_order)
-        res['has_picking_delayed'] = ui_order['has_picking_delayed']
+        res["has_picking_delayed"] = ui_order["has_picking_delayed"]
         return res
 
     # Custom Section
-    @api.multi
     def _create_delayed_picking(self):
-        # make the function idempotent
         orders = self.filtered(lambda x: x.has_picking_delayed)
-        super(PosOrder, orders).create_picking()
-        orders.write({'has_picking_delayed': False})
+        res = super(PosOrder, orders)._create_order_picking()
+        orders.write({"has_picking_delayed": False})
+        return res
