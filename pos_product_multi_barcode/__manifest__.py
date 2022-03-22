fda308e4ded147f7250a4e589e18e46887ee65a5
Pierrick Brun <pierrick.brun@akretion.com>
2022-03-22 12:34:30 +0100

    [ADD] pos_product_multi_barcode

---
+++ pos_product_multi_barcode/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "PoS Product multi barcode",
+    "summary": "Make product multi barcodes usable in the point of sale",
+    "version": "14.0.1.0.0",
+    "category": "Point of Sale",
+    "website": "https://github.com/OCA/pos",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "point_of_sale",
+        "product_multi_barcode",
+    ],
+    "data": [
+        "templates/assets.xml",
+    ],
+    "demo": [],
+    "qweb": [],
+}
