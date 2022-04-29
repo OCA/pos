01af69b53a0d0d25abbf333046cea2c77ad5541e
i1 <ivan@cetmix.com>
2022-04-29 11:52:12 +0200

    [14.0][ADD] pos_disable_pricelist_selection

---
+++ pos_disable_pricelist_selection/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Disable Pricelist selection button in POS",
+    "version": "14.0.1.0.0",
+    "summary": "Disable Pricelist selection button in POS",
+    "author": "Ooops, Cetmix, Odoo Community Association (OCA)",
+    "contributors": "Cetmix",
+    "license": "LGPL-3",
+    "category": "Purchase",
+    "website": "https://github.com/OCA/pos",
+    "depends": ["point_of_sale"],
+    "external_dependencies": {},
+    "demo": [],
+    "data": ["views/assets.xml", "views/pos_config_view.xml"],
+    "qweb": ["static/src/xml/ClientDetailsEdit.xml"],
+    "installable": True,
+    "application": False,
+}
