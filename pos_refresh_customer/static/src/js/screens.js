c80b5bb021105e5cb62fb5ecd95684614c2e4fa8
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-05-05 17:51:11 +0200

    [FIX] pos_refresh_customer: Verify whether client didn't change in background
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- pos_refresh_customer/static/src/js/screens.js
+++ pos_refresh_customer/static/src/js/screens.js
@@ -16,11 +16,22 @@ odoo.define("pos_customer_wallet.screens", function (require) {
                     // of date.
                     var clientlist = self.gui.screen_instances["clientlist"];
                     clientlist.partner_cache = new screens.DomCache();
-                    clientlist.render_list(self.pos.db.get_partners_sorted(1000));
+                    clientlist.render_list(
+                        self.pos.db.get_partners_sorted(1000)
+                    );
 
-                    self.pos
-                        .get_order()
-                        .set_client(self.pos.db.get_partner_by_id(client.id));
+                    // Because this function is run asynchronously, the client
+                    // could have changed in the meantime. Check whether it's
+                    // still the same before proceeding.
+                    if (self.pos.get_client() == client) {
+                        // Override outdated partner model on the order with a
+                        // newer one. The old one will be garbage-collected.
+                        self.pos
+                            .get_order()
+                            .set_client(
+                                self.pos.db.get_partner_by_id(client.id)
+                            );
+                    }
                 });
             }
 
