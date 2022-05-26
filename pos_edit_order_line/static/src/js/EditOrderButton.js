b5ee4995efb7f12a847a82e1ed7734760bead5f8
diga <gabbasov@itlibertas.com>
2022-05-27 03:11:25 +0500

    [ADD] add new module: pos_edit_order_line

---
+++ pos_edit_order_line/static/src/js/EditOrderButton.js
@@ -0,0 +1,75 @@
+odoo.define("pos_edit_order_line.EditOrderButton", function (require) {
+    "use strict";
+
+    const PosComponent = require("point_of_sale.PosComponent");
+    const ProductScreen = require("point_of_sale.ProductScreen");
+    const {useListener} = require("web.custom_hooks");
+    const Registries = require("point_of_sale.Registries");
+    const {_t} = require("web.core");
+
+    class EditOrderButton extends PosComponent {
+        constructor() {
+            super(...arguments);
+            useListener("click", this.onClick);
+        }
+        async onClick() {
+            var self = this;
+            var order = this.env.pos.get_order();
+            var order_lines = order.get_orderlines();
+            if (!order_lines.length) {
+                return this.showPopup("ErrorPopup", {
+                    title: _t("Empty Order"),
+                    body: _t("You need add some products first"),
+                });
+            }
+            var array = [];
+            _.each(order_lines, function (line) {
+                array.push({
+                    id: line.id,
+                    discount: line.discount,
+                    price: line.get_unit_price(),
+                    quantity: line.quantity,
+                    uom: self.env.pos.units_by_id[line.product.uom_id[0]].name,
+                    name: line.get_full_product_name(),
+                });
+            });
+            const {confirmed, payload} = await this.showPopup("EditOrderPopup", {
+                title: this.env._t("Edit Order Line"),
+                array: array,
+            });
+            if (confirmed) {
+                await self.apply_changes(payload);
+            }
+        }
+        async apply_changes(payload) {
+            var order = this.env.pos.get_order();
+            _.each(payload, function (changes, id) {
+                var line = order.get_orderline(parseInt(id));
+                _.each(changes, function (value, key) {
+                    if (key === "quantity") {
+                        line.set_quantity(value);
+                    } else if (key === "price") {
+                        line.set_unit_price(value);
+                    } else if (key === "discount") {
+                        line.set_discount(value);
+                    }
+                });
+            });
+            if (!_.isEmpty(payload)) {
+                order.trigger("change");
+            }
+        }
+    }
+    EditOrderButton.template = "EditOrderButton";
+
+    ProductScreen.addControlButton({
+        component: EditOrderButton,
+        condition: function () {
+            return this.env.pos.config.allow_edit_order_line;
+        },
+    });
+
+    Registries.Component.add(EditOrderButton);
+
+    return EditOrderButton;
+});
---
+++ pos_edit_order_line/static/src/js/EditOrderPopup.js
@@ -0,0 +1,69 @@
+odoo.define("pos_edit_order_line.EditOrderPopup", function (require) {
+    "use strict";
+
+    const {useState} = owl.hooks;
+    const AbstractAwaitablePopup = require("point_of_sale.AbstractAwaitablePopup");
+    const Registries = require("point_of_sale.Registries");
+
+    class EditOrderPopup extends AbstractAwaitablePopup {
+        constructor() {
+            super(...arguments);
+            this._id = 0;
+            this.state = useState({array: this._initialize(this.props.array)});
+            this.changes = {};
+        }
+        _nextId() {
+            return this._id++;
+        }
+        _emptyItem() {
+            return {
+                text: "",
+                _id: this._nextId(),
+            };
+        }
+        _initialize(array) {
+            // If no array is provided, we initialize with one empty item.
+            if (array.length === 0) return [this._emptyItem()];
+            // Put _id for each item. It will serve as unique identifier of each item.
+            return array.map((item) =>
+                Object.assign(
+                    {},
+                    {_id: this._nextId()},
+                    typeof item === "object" ? item : {text: item}
+                )
+            );
+        }
+        _onchange(event) {
+            const {id, changes} = event.detail;
+            this.changes[id] = changes;
+        }
+        getPayload() {
+            return this.changes;
+        }
+        async confirm() {
+            var allowConfirmChanges = true;
+            _.each(Object.values(this.changes), (updates) =>
+                _.each(Object.values(updates), (value) => {
+                    if (isNaN(value)) {
+                        allowConfirmChanges = false;
+                    }
+                })
+            );
+
+            if (allowConfirmChanges) {
+                this.props.resolve({confirmed: true, payload: await this.getPayload()});
+                this.trigger("close-popup");
+            }
+        }
+    }
+    EditOrderPopup.template = "EditOrderPopup";
+    EditOrderPopup.defaultProps = {
+        confirmText: "Save",
+        cancelText: "Cancel",
+        array: [],
+    };
+
+    Registries.Component.add(EditOrderPopup);
+
+    return EditOrderPopup;
+});
