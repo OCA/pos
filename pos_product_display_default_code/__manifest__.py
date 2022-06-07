c7df8c88b905fc61f560904490bdb0bd086ff507
Pierrick Brun <pierrick.brun@akretion.com>
2022-06-07 14:02:39 +0200

    [ADD] pos_product_display_default_code

---
+++ pos_product_display_default_code/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "PoS Product Display Default Code",
+    "summary": "pos: display product default code before product name",
+    "version": "14.0.1.0.1",
+    "category": "Point of Sale",
+    "website": "https://github.com/OCA/pos",
+    "author": "Odoo Community Association (OCA), Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "point_of_sale",
+    ],
+    "data": ["templates/assets.xml"],
+    "demo": [],
+    "qweb": [],
+}
