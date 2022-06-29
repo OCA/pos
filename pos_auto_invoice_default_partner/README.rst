e1c3798a5fd9cac1696230de3e693a40d3ba7b47
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-06-29 11:24:14 +0200

    [FIX] SCRLfs → SC
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- pos_auto_invoice_default_partner/README.rst
+++ pos_auto_invoice_default_partner/README.rst
@@ -41,12 +41,12 @@ Credits
 Authors
 ~~~~~~~
 
-* Coop IT Easy SCRLfs
+* Coop IT Easy SC
 
 Contributors
 ~~~~~~~~~~~~
 
-* `Coop IT Easy SCRLfs <https://coopiteasy.be>`_:
+* `Coop IT Easy SC <https://coopiteasy.be>`_:
 
   * Carmen Bianca Bakker
 
--- pos_auto_invoice_default_partner/__manifest__.py
+++ pos_auto_invoice_default_partner/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Coop IT Easy SCRLfs
+# Copyright 2022 Coop IT Easy SC
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -8,7 +8,7 @@
     "version": "12.0.1.0.0",
     "category": "Point of Sale",
     "website": "https://coopiteasy.be",
-    "author": "Coop IT Easy SCRLfs",
+    "author": "Coop IT Easy SC",
     "license": "AGPL-3",
     "application": False,
     "depends": [
