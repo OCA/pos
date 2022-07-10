7a96fe2bbe922e0e5696430784818d67fa08c2f8
Adrian Segura Trista <asegurat15@gmail.com>
2022-07-10 10:15:45 -0400

    [15.0][FIX] one per module.

--- pos_payment_change/readme/CONTRIBUTORS.rst
+++
@@ -1,3 +0,0 @@
-* Sylvain LE GAL <https://twitter.com/legalsylvain>
-* Julien WESTE
-* Foram Shah <foram.shah@initos.com>
--- pos_payment_change/wizards/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import pos_payment_change_wizard
-from . import pos_payment_change_wizard_new_line
-from . import pos_payment_change_wizard_old_line
