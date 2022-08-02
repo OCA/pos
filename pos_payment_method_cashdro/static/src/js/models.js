703a6c52dca47d54483f2d267ff39ee64bfc1497
david <david.vidal@tecnativa.com>
2022-08-02 11:38:35 +0200

    [IMP] pos_payment_method_cashdro: black, isort, prettier

--- pos_payment_method_cashdro/static/src/js/models.js
+++ pos_payment_method_cashdro/static/src/js/models.js
@@ -3,18 +3,19 @@ odoo.define("pos_payment_method_cashdro.models", function (require) {
 
     var models = require("point_of_sale.models");
 
-    models.load_fields(
-        "account.journal",
-        ["cashdro_payment_terminal", "cashdro_host", "cashdro_user", "cashdro_password"]
-    );
+    models.load_fields("account.journal", [
+        "cashdro_payment_terminal",
+        "cashdro_host",
+        "cashdro_user",
+        "cashdro_password",
+    ]);
 
     var order_super = models.Order.prototype;
 
     models.Order = models.Order.extend({
-        initialize: function(){
+        initialize: function () {
             order_super.initialize.apply(this, arguments);
             this.in_cashdro_transaction = false;
         },
     });
-
 });
