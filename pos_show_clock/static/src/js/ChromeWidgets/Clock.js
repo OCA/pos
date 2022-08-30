a168f9e77c0150091a2064dbf4cc081f298af67c
Luis Felipe Mileo <mileo@kmee.com.br>
2022-08-30 20:41:14 -0300

    [NEW] Pos Show Clock
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

---
+++ pos_show_clock/static/src/js/ChromeWidgets/Clock.js
@@ -0,0 +1,40 @@
+odoo.define("pos_show_clock.Clock", function (require) {
+    "use strict";
+
+    const PosComponent = require("point_of_sale.PosComponent");
+    const Registries = require("point_of_sale.Registries");
+    const {useState} = owl;
+
+    class Clock extends PosComponent {
+        setup() {
+            super.setup();
+            this.state = useState({current_time: ""});
+            this.mounted();
+        }
+        mounted() {
+            this.updateCurrentTime();
+        }
+        updateCurrentTime() {
+            var self = this;
+            const now = Date.now();
+            const next_update = 60050 - parseInt(moment(now).format("ss")) * 1000;
+            // 1 min = 60.000ms
+            // 60000 + 50 (less then 100ms rule)
+            // it's the time we can update the minute without the user noticing
+            // the difference when comparing it to another watch.
+            this.state.current_time = moment(now).format("HH:mm");
+            setTimeout(() => {
+                self.updateCurrentTime();
+            }, next_update);
+        }
+        get time() {
+            return this.state.current_time;
+        }
+    }
+
+    Clock.template = "Clock";
+
+    Registries.Component.add(Clock);
+
+    return Clock;
+});
