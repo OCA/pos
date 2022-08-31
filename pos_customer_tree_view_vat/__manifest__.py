a4485b6813861584d8072b6e54912dc000c5d5e5
Luis Felipe Mileo <mileo@kmee.com.br>
2022-08-30 21:53:20 -0300

    [NEW] Pos Vat Tree
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

---
+++ pos_customer_tree_view_vat/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Pos Vat Tree",
+    "summary": """Point of Sale: Show VAT number at Customer Tree View""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pos",
+    "maintainers": ["mileo"],
+    "depends": [
+        "point_of_sale",
+    ],
+    "qweb": [
+        "static/src/xml/pos.xml",
+    ],
+}
