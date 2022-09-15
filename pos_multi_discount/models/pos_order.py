736e1fc57b9a61df68ecf3fc84b845a08e2a7bd4
Ilyas <irazor147@gmail.com>
2022-09-15 17:52:55 +0500

    [14.0][UPD] price based computed deiscount fields

--- pos_multi_discount/models/pos_order.py
+++ pos_multi_discount/models/pos_order.py
@@ -1,7 +1,18 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PosOrderLine(models.Model):
     _inherit = "pos.order.line"
 
     manual_discount = fields.Float("Manual Discount")
+    manual_amount = fields.Monetary(
+        "Manual Discount Rel.",
+        compute="_compute_relative_discounts",
+        help="Based on product price",
+    )
+
+    @api.depends("qty", "price_unit", "manual_discount")
+    def _compute_relative_discounts(self):
+        for rec in self:
+            subtotal = rec.qty * rec.price_unit
+            rec.manual_amount = subtotal * rec.manual_discount / 100
