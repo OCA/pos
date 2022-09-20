73f3d437b2cf6de21277f19a1f295cef8bdcf134
Ilyas <irazor147@gmail.com>
2022-09-20 21:14:27 +0500

    [14.0][UPD] percent discount feature

--- pos_multi_discount_global/models/pos_order.py
+++ pos_multi_discount_global/models/pos_order.py
@@ -1,16 +1,17 @@
 from odoo import api, fields, models
-from odoo.tools import float_repr, float_round
 
 
 class PosOrder(models.Model):
     _inherit = "pos.order"
 
     total_fixed_discount = fields.Float("Fixed Discount", readonly=True)
+    total_percent_discount = fields.Float("Percent Discount", readonly=True)
 
     @api.model
     def _order_fields(self, ui_order):
         res = super(PosOrder, self)._order_fields(ui_order)
         res["total_fixed_discount"] = ui_order.get("total_fixed_discount")
+        res["total_percent_discount"] = ui_order.get("total_percent_discount")
         return res
 
 
@@ -24,6 +25,13 @@ class PosOrderLine(models.Model):
     fixed_discount_relative_amount = fields.Monetary(
         "Fixed Disc. Amount", help="Based on product price"
     )
+    percent_discount = fields.Float("Percent Discount")
+    percent_discount_relative = fields.Float(
+        "Percent Discount Rel.", help="Based on product price"
+    )
+    percent_discount_relative_amount = fields.Monetary(
+        "Percent Disc. Amount", help="Based on product price"
+    )
 
     @api.depends("qty", "price_unit", "manual_discount", "fixed_discount")
     def _compute_relative_discounts(self):
@@ -35,9 +43,3 @@ class PosOrderLine(models.Model):
             rec.fixed_discount_relative = (
                 100 * rec.fixed_discount_relative_amount / (rec.qty * rec.price_unit)
             )
-    #
-    # def _order_line_fields(self, line, session_id=None):
-    #     res = super(PosOrderLine, self)._order_line_fields(line, session_id)
-    #     if len(res) == 3 and type(res[2]) == dict and res[2].get('discount'):
-    #         res[2]['discount'] = float_round(res[2]['discount'],2)
-    #     return res
