c8e44f84e0bd53b673f033cea683aeabbc4135ec
Daniel Duque <daniel.duque@factorlibre.com>
2022-10-07 11:28:28 +0200

    [MIG] pos_receipt_hide_price: Migration to 16.0

--- pos_receipt_hide_price/__manifest__.py
+++ pos_receipt_hide_price/__manifest__.py
@@ -1,11 +1,12 @@
 # Copyright 2021 Akretion - Florian Mounier
+# Copyright 2022 FactorLibre - Daniel Duque
 {
     "name": "POS Receipt Hide Price",
     "summary": "Add button to remove price from receipt.",
-    "author": "Akretion, Odoo Community Association (OCA)",
+    "author": "Akretion, FactorLibre, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/pos",
     "category": "Point of Sale",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "LGPL-3",
     "depends": ["point_of_sale"],
     "assets": {
@@ -13,8 +14,6 @@
             "pos_receipt_hide_price/static/src/js/ReceiptScreen.js",
             "pos_receipt_hide_price/static/src/js/ReprintReceiptScreen.js",
             "pos_receipt_hide_price/static/src/js/OrderReceipt.js",
-        ],
-        "web.assets_qweb": [
             "pos_receipt_hide_price/static/src/xml/**/*",
         ],
     },
