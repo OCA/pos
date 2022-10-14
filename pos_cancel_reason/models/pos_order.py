6ddcb26a1e8dec753f717281819eb3ab42ee9894
Luiz Felipe do Divino <lf.divino@gmail.com>
2022-10-14 12:25:30 -0300

    [ADD] Fields in _order_fields and unlink validations

--- pos_cancel_reason/models/pos_order.py
+++ pos_cancel_reason/models/pos_order.py
@@ -1,9 +1,8 @@
 # Copyright 2022 KMEE (<http://www.kmee.com.br>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import re
-from typing_extensions import Required
 from odoo import _, api, models, fields
+from odoo.exceptions import ValidationError
 
 
 class PosOrder(models.Model):
@@ -21,6 +20,15 @@ class PosOrder(models.Model):
     )
 
     @api.model
+    def _order_fields(self, ui_order):
+        order_fields = super(PosOrder, self)._order_fields(ui_order)
+        if ui_order.get('cancel_reason_id'):
+            order_fields['cancel_reason_id'] = ui_order.get('cancel_reason_id')
+        if ui_order.get('state') and ui_order['state'] == 'cancel':
+            order_fields['state'] = ui_order.get('state')
+        return order_fields
+
+    @api.model
     def create_from_ui(self, orders, draft=False):
         orders_ids = super(PosOrder, self).create_from_ui(orders)
         for order in orders:
@@ -28,3 +36,11 @@ class PosOrder(models.Model):
                 if cancelled_item:
                     self.env["pos.order.line.cancelled"].cancel_from_ui(cancelled_item)
         return orders_ids
+
+    def unlink(self):
+        if self.cancel_reason_id:
+            raise ValidationError(
+                    _("You can't delete an order with a cancel reason!")
+                )
+
+        return super(PosOrder, self).unlink()
