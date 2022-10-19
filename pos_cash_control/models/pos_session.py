8ecff50e64a45aee3c7ff44f1d613ae586534985
Ygor Carvalho <ygcarvalh@gmail.com>
2022-10-19 11:24:22 -0300

    [FIX] pos_cash_control: Update flow for case tests

--- pos_cash_control/models/pos_session.py
+++ pos_cash_control/models/pos_session.py
@@ -376,73 +376,79 @@ class PosSession(models.Model):
         amount_to_balance=0,
         bank_payment_method_diffs=None,
     ):
-        bank_payment_method_diffs = bank_payment_method_diffs or {}
-        self.ensure_one()
-        sudo = self.user_has_groups("point_of_sale.group_pos_user")
-        if self.order_ids or self.statement_ids.line_ids:
-            self.cash_real_transaction = self.cash_register_total_entry_encoding
-            self.cash_real_expected = self.cash_register_balance_end
-            self.cash_real_difference = self.cash_register_difference
-            if self.state == "closed":
-                raise UserError(_("This session is already closed."))
-            self._check_if_no_draft_orders()
-            self._check_invoices_are_posted()
-            if self.update_stock_at_closing:
-                self._create_picking_at_end_of_session()
-                self.order_ids.filtered(
-                    lambda o: not o.is_total_cost_computed
-                )._compute_total_cost_at_session_closing(self.picking_ids.move_lines)
-            try:
-                data = self.with_company(self.company_id)._create_account_move(
-                    balancing_account, amount_to_balance, bank_payment_method_diffs
-                )
-            except AccessError as e:
-                if sudo:
-                    data = (
-                        self.sudo()
-                        .with_company(self.company_id)
-                        ._create_account_move(
-                            balancing_account,
-                            amount_to_balance,
-                            bank_payment_method_diffs,
-                        )
+        if not self.state == "closed":
+            bank_payment_method_diffs = bank_payment_method_diffs or {}
+            self.ensure_one()
+            sudo = self.user_has_groups("point_of_sale.group_pos_user")
+            if self.order_ids or self.statement_ids.line_ids:
+                self.cash_real_transaction = self.cash_register_total_entry_encoding
+                self.cash_real_expected = self.cash_register_balance_end
+                self.cash_real_difference = self.cash_register_difference
+                self._check_if_no_draft_orders()
+                self._check_invoices_are_posted()
+                if self.update_stock_at_closing:
+                    self._create_picking_at_end_of_session()
+                    self.order_ids.filtered(
+                        lambda o: not o.is_total_cost_computed
+                    )._compute_total_cost_at_session_closing(
+                        self.picking_ids.move_lines
                     )
+                try:
+                    data = self.with_company(self.company_id)._create_account_move(
+                        balancing_account, amount_to_balance, bank_payment_method_diffs
+                    )
+                except AccessError as e:
+                    if sudo:
+                        data = (
+                            self.sudo()
+                            .with_company(self.company_id)
+                            ._create_account_move(
+                                balancing_account,
+                                amount_to_balance,
+                                bank_payment_method_diffs,
+                            )
+                        )
+                    else:
+                        raise e
+
+                try:
+                    balance = sum(self.move_id.line_ids.mapped("balance"))
+                    self.move_id._check_balanced()
+                except UserError:
+                    # Creating the account move is just part of a big database
+                    # transaction
+                    # when closing a session. There are other database changes
+                    # that will happen
+                    # before attempting to create the account move, such as,
+                    # creating the picking
+                    # records.
+                    # We don't, however, want them to be committed when the
+                    # account move creation
+                    # failed; therefore, we need to roll back this transaction
+                    # before showing the
+                    # close session wizard.
+                    self.env.cr.rollback()
+                    return self._close_session_action(balance)
+
+                if self.move_id.line_ids:
+                    self.move_id.sudo().with_company(self.company_id)._post()
+                    # Set the uninvoiced orders' state to 'done'
+                    self.env["pos.order"].search(
+                        [("session_id", "=", self.id), ("state", "=", "paid")]
+                    ).write({"state": "done"})
                 else:
-                    raise e
-
-            try:
-                balance = sum(self.move_id.line_ids.mapped("balance"))
-                self.move_id._check_balanced()
-            except UserError:
-                # Creating the account move is just part of a big database transaction
-                # when closing a session. There are other database changes that will happen
-                # before attempting to create the account move, such as, creating the picking
-                # records.
-                # We don't, however, want them to be committed when the account move creation
-                # failed; therefore, we need to roll back this transaction before showing the
-                # close session wizard.
-                self.env.cr.rollback()
-                return self._close_session_action(balance)
-
-            if self.move_id.line_ids:
-                self.move_id.sudo().with_company(self.company_id)._post()
-                # Set the uninvoiced orders' state to 'done'
-                self.env["pos.order"].search(
-                    [("session_id", "=", self.id), ("state", "=", "paid")]
-                ).write({"state": "done"})
+                    self.move_id.sudo().unlink()
+                self.sudo().with_company(self.company_id)._reconcile_account_move_lines(
+                    data
+                )
             else:
-                self.move_id.sudo().unlink()
-            self.sudo().with_company(self.company_id)._reconcile_account_move_lines(
-                data
-            )
-        else:
-            statement = self.cash_register_id
-            if not self.config_id.cash_control:
-                statement.write({"balance_end_real": statement.balance_end})
-            statement.button_post()
-            statement.button_validate()
-        self.write({"state": "closed"})
-        return True
+                statement = self.cash_register_id
+                if not self.config_id.cash_control:
+                    statement.write({"balance_end_real": statement.balance_end})
+                statement.button_post()
+                statement.button_validate()
+            self.write({"state": "closed"})
+            return True
 
     def _close_session_action(self, amount_to_balance):
         # NOTE This can't handle `bank_payment_method_diffs` because there is
