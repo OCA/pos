600a9ff1bde6fa0779cdd33795325d9781802f18
Nikolaus Weingartmair <niwe@odoovm1>
2022-10-19 10:47:42 +0200

    [MIG] pos_cash_move_reason: Migration to 15.0

--- pos_cash_move_reason/__manifest__.py
+++ pos_cash_move_reason/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "ACSONE SA/NV," "GRAP," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/pos",
     "category": "Point Of sale",
-    "version": "14.0.1.0.3",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["point_of_sale"],
     "data": [
