e69dd6de8224e1869ac1d3a3625e20494b9bf9f6
Ygor Carvalho <ygcarvalh@gmail.com>
2022-10-31 09:02:11 -0300

    [ADD] pos_product_control: initial module files

---
+++ pos_product_control/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Pos Product Control",
+    "summary": """
+        Allows product inventory control during the opening and closing process of a POS session.""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "KMEE,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pos",
+    "depends": [],
+    "data": [],
+    "demo": [],
+}
