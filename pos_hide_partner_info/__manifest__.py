2b726bceb04fdd0196f9a7d47df8382c9e772659
Victor Champonnois <victor@coopiteasy.be>
2022-11-09 10:05:46 +0100

    [ADD] pos_hide_partner_info

---
+++ pos_hide_partner_info/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2017 - 2020 BEES coop SCRLfs
+#   - Elouan Lebars <elouan@coopiteasy.be>
+#   - Rémy Taymans <remy@coopiteasy.be>
+#   - Vincent Van Rossem <vincent@coopiteasy.be>
+#   - Elise Dupont
+#   - Thibault François
+#   - Grégoire Leeuwerck
+#   - Houssine Bakkali <houssine@coopiteasy.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "POS - Hide Partner Info",
+    "summary": """Hides phone and address field in pos customer list.""",
+    "author": "BEES coop - Cellule IT,"
+    " Coop IT Easy SC,"
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pos",
+    "category": "Point Of Sale",
+    "version": "12.0.2.0.0",
+    "depends": ["point_of_sale"],
+    "qweb": ["static/src/xml/templates.xml"],
+    "installable": True,
+    "license": "AGPL-3",
+}
