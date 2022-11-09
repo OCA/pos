61675117909965b4452f155a34067413a8a6c970
Victor Champonnois <victor@coopiteasy.be>
2022-11-09 10:06:44 +0100

    [ADD] pos_partner_no_create

---
+++ pos_partner_no_create/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2017 - 2020 BEES coop SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "POS - Forbid New Customer Creation",
+    "summary": """Forbid customer creation from the POS""",
+    "author": "Odoo Community Association (OCA),"
+              " BEES coop - Cellule IT,"
+              " Coop IT Easy SC",
+    "website": "https://github.com/OCA/pos",
+    "category": "Point Of Sale",
+    "version": "12.0.2.0.0",
+    "depends": ["point_of_sale"],
+    "qweb": ["static/src/xml/templates.xml"],
+    "installable": True,
+    "license": "AGPL-3",
+}
