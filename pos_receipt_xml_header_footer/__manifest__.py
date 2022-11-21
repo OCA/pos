f20edd8de0c459aff79669040c9b31f7ef67c80d
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2022-11-21 16:57:48 +0100

    [ADD] pos_receipt_xml_header_footer
    In Odoo 12, there exist two XML templates that are both receipts:
    PosTicket and XmlReceipt. PosTicket is shown on the cashier's screen,
    and the more tightly defined XmlReceipt is sent to the ESC POC printer.
    In future Odoo versions, I believe they have been unified into
    OrderReceipt.
    
    For reasons unknown to me, PosTicket does not contain support for
    `header_xml` and `footer_xml`, but XmlReceipt does. This module fixes
    that.
    
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

---
+++ pos_receipt_xml_header_footer/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Point of Sale XML in Header and Footer of Receipt",
+    "summary": """
+        Add the ability to use XML/HTML in the header and footer of a receipt""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Coop IT Easy SCRLfs, Odoo Community Association (OCA)",
+    "maintainers": ["carmenbianca"],
+    "website": "https://github.com/OCA/pos",
+    "category": "Point of Sale",
+    "depends": ["point_of_sale"],
+    "data": [],
+    "qweb": ["static/src/xml/pos.xml"],
+    "demo": [],
+}
---
+++ pos_receipt_xml_header_footer/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module adds support for ``header_xml`` and ``footer_xml`` in the PosTicket
+XML template.
