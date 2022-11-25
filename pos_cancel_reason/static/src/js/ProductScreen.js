983e9d8cfbf064454bd469261cdec0bbe5571426
Ygor Carvalho <ygcarvalh@gmail.com>
2022-11-25 14:48:02 -0300

    [FIX] pos_cancel_reason: Not use delayed time for cancellation flow

--- pos_cancel_reason/static/src/js/ProductScreen.js
+++ pos_cancel_reason/static/src/js/ProductScreen.js
@@ -7,7 +7,7 @@ odoo.define("pos_cancel_reason.ProductScreen", function (require) {
     const PosProductScreen = (ProductScreen) =>
         class extends ProductScreen {
             async _setValue(val) {
-                if (this.state.numpadMode === 'quantity') {
+                if (this.state.numpadMode === "quantity") {
                     let val_parsed = 0;
                     if (parseFloat(val)) {
                         val_parsed = parseFloat(val);
@@ -15,11 +15,11 @@ odoo.define("pos_cancel_reason.ProductScreen", function (require) {
                     if (this.env.pos.config.reason_to_cancel) {
                         const selected_orderline = this.env.pos.get_order()
                             .selected_orderline;
-                        const compared_qty = val_parsed <= selected_orderline.quantity;
-                        const compared_time =
-                            (new Date() - new Date(selected_orderline.created_at)) / 1000 >
-                            this.env.pos.config.delay_to_cancel;
-                        if (compared_qty && compared_time) {
+                        const compared_qty = selected_orderline
+                            ? val_parsed <= selected_orderline.quantity
+                            : false;
+                        // TODO: Use delayed time for not ask for password
+                        if (compared_qty) {
                             const cancel_reason_options = [];
                             for (const i in this.env.pos.cancel_reasons) {
                                 cancel_reason_options.push({
@@ -38,7 +38,8 @@ odoo.define("pos_cancel_reason.ProductScreen", function (require) {
                                 list: cancel_reason_options,
                             });
                             if (confirmed) {
-                                const diffecence = selected_orderline.quantity - val_parsed;
+                                const diffecence =
+                                    selected_orderline.quantity - val_parsed;
                                 this.env.pos
                                     .get_order()
                                     .save_cancelled_orderlines_info(
