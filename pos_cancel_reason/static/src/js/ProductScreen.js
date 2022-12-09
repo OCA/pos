4b3d6bfbcbfcc7cce6c21c5bd40b23d1d3001512
Luis Malta <luis.malta@kmee.com.br>
2022-12-09 08:27:24 -0300

    [IMP] Manager permission to cancel
    Signed-off-by: Luis Malta <luis.malta@kmee.com.br>

--- pos_cancel_reason/static/src/js/ProductScreen.js
+++ pos_cancel_reason/static/src/js/ProductScreen.js
@@ -3,10 +3,67 @@ odoo.define("pos_cancel_reason.ProductScreen", function (require) {
 
     const ProductScreen = require("point_of_sale.ProductScreen");
     const Registries = require("point_of_sale.Registries");
+    const NumberBuffer = require("point_of_sale.NumberBuffer");
 
     const PosProductScreen = (ProductScreen) =>
         class extends ProductScreen {
             async _setValue(val) {
+                const employee = this.env.pos.get_cashier();
+
+                if (employee.role === "manager") {
+                    await this.showCancelReasonPopup(val);
+                } else {
+                    await this.askManagerPermissionToCcancel(val);
+                }
+            }
+
+            async askManagerPermissionToCcancel(val) {
+                const numpad_buffer = NumberBuffer.get();
+                const {confirmed, payload: inputPin} = await this.showPopup(
+                    "NumberPopup",
+                    {
+                        isPassword: true,
+                        title: this.env._t("Password ?"),
+                        startingValue: null,
+                    }
+                );
+
+                if (!confirmed) return false;
+
+                const managers = this.env.pos.employees.filter(
+                    (employee) => employee.pin && employee.role == "manager"
+                );
+                let permission = false;
+
+                for (const i in managers) {
+                    // eslint-disable-next-line
+                    if (managers[i].pin == Sha1.hash(inputPin)) {
+                        permission = true;
+                        break;
+                    }
+                }
+
+                if (permission) {
+                    // In the case of using access permission for functionalities
+                    // that also involve the use of NumberBuffer,
+                    // it is possible that there is a buffer conflict.
+                    const currentOrder = this.env.pos.get_order();
+                    if (currentOrder && currentOrder.get_selected_orderline()) {
+                        const orderline = currentOrder.get_selected_orderline();
+                        if (numpad_buffer == "" && orderline.quantity !== 0) {
+                            NumberBuffer.reset();
+                        }
+                    }
+
+                    await this.showCancelReasonPopup(val);
+                } else {
+                    await this.showPopup("ErrorPopup", {
+                        title: this.env._t("Incorrect Password"),
+                    });
+                }
+            }
+
+            async showCancelReasonPopup(val) {
                 if (this.state.numpadMode === "quantity") {
                     // Avoids the need to double click to remove an orderline with zero quantity
                     const selectedOrderline = this.currentOrder.get_selected_orderline();
