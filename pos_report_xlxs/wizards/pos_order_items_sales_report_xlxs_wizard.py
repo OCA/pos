be4607f3269c974a31d6525c002523dbcc219476
Ygor Carvalho <ygcarvalh@gmail.com>
2022-12-13 10:04:59 -0300

    [FIX] Select records within the correct date
    This `or` for the different states of the pos.order ends up pulling
    all the system records, not taking into account the period that was
    stipulated.

--- pos_report_xlxs/wizards/pos_order_items_sales_report_xlxs_wizard.py
+++ pos_report_xlxs/wizards/pos_order_items_sales_report_xlxs_wizard.py
@@ -33,7 +33,7 @@ class PosOrderItemsSalesReportXlxsWizard(models.TransientModel):
             LEFT JOIN product_category pc ON pc.id = pt.pos_categ_id
             WHERE po.id IN (SELECT id FROM pos_order WHERE session_id IN (
             SELECT id FROM pos_session WHERE start_at >= '{self.date_start} 00:00:00'
-            AND start_at <= '{self.date_end} 23:59:59') AND  state = 'paid' OR state = 'done')
+            AND start_at <= '{self.date_end} 23:59:59') AND state IN ('paid', 'done'))
             ORDER BY po.name;
         """
 
