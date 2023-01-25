6848244386487f90ed4ddc9981eccd68b8e31f0f
DorianMAG <dorian.villard@mind-and-go.com>
2023-01-25 15:48:37 +0100

    Adding an AND operator to the domain

--- pos_session_pay_invoice/models/pos_order.py
+++ pos_session_pay_invoice/models/pos_order.py
@@ -1,14 +1,18 @@
-# Copyright <YEAR(S)> <AUTHOR(S)>
+# Copyright <2023> <DorianMAG>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
+from odoo.osv.expression import AND
 
 
 class PosOrder(models.Model):
     _inherit = "pos.order"
-    
+
     @api.model
     def search_paid_order_ids(self, config_id, domain, limit, offset):
-        domain = [("pos_reference", "!=", False), ("lines", "!=", False)]
-        res = super(PosOrder, self).search_paid_order_ids(config_id, domain, limit, offset)
+        domain = AND([domain, [("pos_reference", "!=", False), ("lines", "!=", False)]])
+
+        res = super(PosOrder, self).search_paid_order_ids(
+            config_id, domain, limit, offset
+        )
         return res
