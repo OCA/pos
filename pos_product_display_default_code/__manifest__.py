4cabd7f1840e8fdcd41bc72fe01b2835b4e552ba
Juan Carlos B <juancarlos.bonilla@factorlibre.com>
2023-02-14 16:24:40 +0100

    [ADD] adding a pos.config parameter 'display_default_code' to enable/disable product's internal reference * 'display_default_code' field view in PoS -> settings -> PoS Interface [NEW] new files: contributors.rst, configure.rst

--- pos_product_display_default_code/__manifest__.py
+++ pos_product_display_default_code/__manifest__.py
@@ -4,11 +4,16 @@
     "version": "16.0.1.0.0",
     "category": "Point of Sale",
     "website": "https://github.com/OCA/pos",
-    "author": "Odoo Community Association (OCA), Akretion",
+    "author": "Odoo Community Association (OCA), Akretion, FactorLibre",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
     "depends": [
         "point_of_sale",
     ],
+    "data": [
+        "views/res_config_settings_views.xml",
+    ],
+    "demo": [],
+    "qweb": [],
 }
