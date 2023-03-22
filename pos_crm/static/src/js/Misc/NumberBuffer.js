3da4be6555de107170c33127eae1b2ea98a596ee
Luis Felipe Mileo <mileo@kmee.com.br>
2023-03-22 09:33:05 -0300

    [IMP] pos_crm: black, isort, prettier

--- pos_crm/static/src/js/Misc/NumberBuffer.js
+++ pos_crm/static/src/js/Misc/NumberBuffer.js
@@ -7,33 +7,33 @@ odoo.define("pos_crm.NumberBuffer", function (require) {
     "use strict";
 
     var NumberBuffer = require("point_of_sale.NumberBuffer");
-    const { Gui } = require('point_of_sale.Gui');
+    const {Gui} = require("point_of_sale.Gui");
 
     const _updateBuffer = (input) => {
-        let self = NumberBuffer;
-        const isEmpty = val => {
-            return val === '' || val === null;
+        const self = NumberBuffer;
+        const isEmpty = (val) => {
+            return val === "" || val === null;
         };
         if (input === undefined || input === null) return;
-        let isFirstInput = isEmpty(self.state.buffer);
-        if (input === ',' || input === '.') {
+        const isFirstInput = isEmpty(self.state.buffer);
+        if (input === "," || input === ".") {
             if (isFirstInput) {
-                self.state.buffer = '0' + self.decimalPoint;
-            } else if (!self.state.buffer.length || self.state.buffer === '-') {
-                self.state.buffer += '0' + self.decimalPoint;
+                self.state.buffer = "0" + self.decimalPoint;
+            } else if (!self.state.buffer.length || self.state.buffer === "-") {
+                self.state.buffer += "0" + self.decimalPoint;
             } else if (self.state.buffer.indexOf(self.decimalPoint) < 0) {
                 self.state.buffer = self.state.buffer + self.decimalPoint;
             }
-        } else if (input === 'Delete') {
+        } else if (input === "Delete") {
             if (self.isReset) {
-                self.state.buffer = '';
+                self.state.buffer = "";
                 self.isReset = false;
                 return;
             }
-            self.state.buffer = isEmpty(self.state.buffer) ? null : '';
-        } else if (input === 'Backspace') {
+            self.state.buffer = isEmpty(self.state.buffer) ? null : "";
+        } else if (input === "Backspace") {
             if (self.isReset) {
-                self.state.buffer = '';
+                self.state.buffer = "";
                 self.isReset = false;
                 return;
             }
@@ -41,46 +41,55 @@ odoo.define("pos_crm.NumberBuffer", function (require) {
             if (isEmpty(buffer)) {
                 self.state.buffer = null;
             } else {
-                const nCharToRemove = buffer[buffer.length - 1] === self.decimalPoint ? 2 : 1;
+                const nCharToRemove =
+                    buffer[buffer.length - 1] === self.decimalPoint ? 2 : 1;
                 self.state.buffer = buffer.substring(0, buffer.length - nCharToRemove);
             }
-        } else if (input === '+') {
-            if (self.state.buffer[0] === '-') {
-                self.state.buffer = self.state.buffer.substring(1, self.state.buffer.length);
+        } else if (input === "+") {
+            if (self.state.buffer[0] === "-") {
+                self.state.buffer = self.state.buffer.substring(
+                    1,
+                    self.state.buffer.length
+                );
             }
-        } else if (input === '-') {
+        } else if (input === "-") {
             if (isFirstInput) {
-                self.state.buffer = '-0';
-            } else if (self.state.buffer[0] === '-') {
-                self.state.buffer = self.state.buffer.substring(1, self.state.buffer.length);
+                self.state.buffer = "-0";
+            } else if (self.state.buffer[0] === "-") {
+                self.state.buffer = self.state.buffer.substring(
+                    1,
+                    self.state.buffer.length
+                );
             } else {
-                self.state.buffer = '-' + self.state.buffer;
+                self.state.buffer = "-" + self.state.buffer;
             }
-        } else if (input[0] === '+' && !isNaN(parseFloat(input))) {
-            // when input is like '+10', '+50', etc
+        } else if (input[0] === "+" && !isNaN(parseFloat(input))) {
+            // When input is like '+10', '+50', etc
             const inputValue = parse.float(input.slice(1));
-            const currentBufferValue = self.state.buffer ? parse.float(self.state.buffer) : 0;
+            const currentBufferValue = self.state.buffer
+                ? parse.float(self.state.buffer)
+                : 0;
             self.state.buffer = self.component.env.pos.formatFixed(
                 inputValue + currentBufferValue
             );
         } else if (!isNaN(parseInt(input, 10))) {
             if (isFirstInput) {
-                self.state.buffer = '' + input;
+                self.state.buffer = String(input);
             } else if (self.state.buffer.length > 13) {
-                Gui.playSound('bell');
+                Gui.playSound("bell");
             } else {
                 self.state.buffer += input;
             }
         }
-        if (self.state.buffer === '-') {
-            self.state.buffer = '';
+        if (self.state.buffer === "-") {
+            self.state.buffer = "";
         }
-        // once an input is accepted and updated the buffer,
+        // Once an input is accepted and updated the buffer,
         // the buffer should not be in reset state anymore.
         self.isReset = false;
 
-        self.trigger('buffer-update', self.state.buffer);
-    }
+        self.trigger("buffer-update", self.state.buffer);
+    };
 
     NumberBuffer.__proto__._updateBuffer = _updateBuffer;
 });
