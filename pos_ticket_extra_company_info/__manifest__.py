469bbf7a9e6bb2a2458aeb2e91971ab15bba3e4f
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-04-04 12:19:08 +0200

    [ADD] pos_ticket_extra_company_info module.

---
+++ pos_ticket_extra_company_info/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2023 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Point of Sale - Extra Company Info",
+    "summary": "Add extra company infos on the ticket",
+    "version": "12.0.1.0.1",
+    "category": "Sales/Point Of Sale",
+    "website": "https://github.com/OCA/pos",
+    "author": "GRAP, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["point_of_sale"],
+    "qweb": ["static/src/xml/pos_ticket_extra_company_info.xml"],
+    "installable": True,
+}
---
+++ pos_ticket_extra_company_info/readme/DESCRIPTION.rst
@@ -0,0 +1,24 @@
+This module extends the functionality of Point of Sale module.
+
+It adds missing company fields on PoS ticket.
+
+**In Proxy Mode, it adds:**
+
+- company address
+- company registry
+
+**In Non Proxy Mode, it adds:**
+- company address
+- company VAT ID
+- company Registry
+- company website
+- company email
+
+
+**Without the module**
+
+.. figure:: ../static/description/ticket_no_proxy_before.png
+
+
+**With the module**
+.. figure:: ../static/description/ticket_no_proxy_after.png
