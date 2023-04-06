7b3b910085b47bc559723e5e87dfe4e98d1dd297
Ygor Carvalho <ygcarvalh@gmail.com>
2023-04-06 10:52:05 -0300

    [IMP] pos_crm: fill and shows partner vat in order view

--- pos_crm/models/pos_order.py
+++ pos_crm/models/pos_order.py
@@ -1,13 +1,28 @@
-# Copyright 2022 KMEE - Gabriel Cardoso <gabriel.cardoso@kmee.com.br>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
+# Copyright 2023 KMEE
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PosOrder(models.Model):
+
     _inherit = "pos.order"
 
     partner_vat = fields.Char(
         string="Partner VAT",
-        default="",
+        readonly=True,
+        copy=False,
     )
+
+    @api.model
+    def _order_fields(self, ui_order):
+        res = super(PosOrder, self)._order_fields(ui_order)
+        res["partner_vat"] = ui_order["partner_vat"]
+        return res
+
+    @api.onchange("partner_id")
+    def _onchange_partner_id(self):
+        if self.partner_id:
+            self.partner_vat = self.partner_id.vat
+        else:
+            self.partner_vat = False
