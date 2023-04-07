325ae220d88ca368db5d51cd46e838d63e801bca
Benoit Aimont <benoit.aimont@acsone.eu>
2023-04-07 14:25:48 +0200

    [MIG] pos_tare: Migration to 14.0

--- pos_tare/models/product_template.py
+++ pos_tare/models/product_template.py
@@ -4,14 +4,12 @@
 
 from odoo import fields, models
 
-import odoo.addons.decimal_precision as dp
-
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     tare_weight = fields.Float(
-        digits=dp.get_precision("Product Unit of Measure"),
+        digits="Product Unit of Measure",
         string="Tare Weight",
         help="Set here Constant tare weight"
         " for the given product. This tare will be substracted when"
