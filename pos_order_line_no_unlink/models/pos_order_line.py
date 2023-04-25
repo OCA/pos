cfe0df69d7224d9430b1eaa995dafc465cad5637
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-04-25 22:22:05 +0200

    [IMP] pos_order_line_no_unlink: black, isort, prettier

--- pos_order_line_no_unlink/models/pos_order_line.py
+++ pos_order_line_no_unlink/models/pos_order_line.py
@@ -11,10 +11,11 @@ class PosOrderLine(models.Model):
 
     @api.multi
     def unlink(self):
-        if self.filtered(
-            lambda x: x.order_id.state not in ["draft", "cancel"]
-        ):
-            raise UserError(_(
-                "You can only unlink PoS order lines that are related"
-                " to orders in new or cancelled state."))
+        if self.filtered(lambda x: x.order_id.state not in ["draft", "cancel"]):
+            raise UserError(
+                _(
+                    "You can only unlink PoS order lines that are related"
+                    " to orders in new or cancelled state."
+                )
+            )
         return super().unlink()
