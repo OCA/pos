cb0b0e855d248f17861b3c0a2af4a57d85310001
Mario Solis <56165442+msolis6686@users.noreply.github.com>
2023-04-26 20:05:54 -0300

    pylint fixes v3.4

--- pos_lot_selection/models/pos_config.py
+++ pos_lot_selection/models/pos_config.py
@@ -9,6 +9,5 @@ class PosConfig(models.Model):
     _inherit = "pos.config"
 
     default_location_src_id = fields.Many2one(
-        "stock.location",
-        related="picking_type_id.default_location_src_id"
+        "stock.location", related="picking_type_id.default_location_src_id"
     )
