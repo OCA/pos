acf1745aa1efa7c57eb8272d4adcac158bee466d
Sandip-OSI <svyas@opensourceintegrators.com>
2023-05-10 11:09:07 +0530

    [16.0][ADD] pos_product_sequence

---
+++ pos_product_sequence/models/pos_session.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2022 Open Source Integrators (https://www.opensourceintegrators.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class PosSession(models.Model):
+    _inherit = 'pos.session'
+
+    def _loader_params_product_product(self):
+        result = super()._loader_params_product_product()
+        result['search_params']['fields'].append('pos_sequence')
+        return result
