fad0d3a981b228a29dae7b9ec3cff491b5ec293c
dessanhemrayev <dessanhemrayev@gmail.com>
2023-06-16 00:03:37 +0300

    [ADD] pos_report_discount: add module

---
+++ pos_report_discount/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "POS Report Discount",
+    "summary": "POS Report Discount",
+    "version": "14.0.1.0.0",
+    "category": "Point Of Sale",
+    "maintainers": ["dessanhemrayev", "CetmixGitDrone"],
+    "website": "https://github.com/OCA/pos",
+    "author": "Cetmix, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+    "depends": [
+        "point_of_sale",
+    ],
+    "data": [
+        "views/report_pos_order.xml",
+    ],
+}
---
+++ pos_report_discount/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module add discount to pivot POS orders
