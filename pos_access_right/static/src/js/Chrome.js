3f2c5e9f86c877a3fb578c99c58bda922a26bdaa
jumeldi74 <jumeldi74@gmail.com>
2023-06-20 17:33:48 +0700

    [MIG] pos_access_right: Migration to 16.0 - Add group access right in Cash In/Out Button only for Accounting/Manager (Revision 2).

--- pos_access_right/static/src/js/Chrome.js
+++ pos_access_right/static/src/js/Chrome.js
@@ -1,24 +1,21 @@
 odoo.define("pos_access_right.chrome", function (require) {
     "use strict";
 
-    const Chrome = require("point_of_sale.Chrome");
+    const Chrome = require("pos_hr.chrome");
     const Registries = require("point_of_sale.Registries");
 
     const PosAccessRightChrome = (Chrome) =>
         class extends Chrome {
             showCashMoveButton() {
-                if (this.env.pos.config.module_pos_hr) {
-                    if (this.env.pos.get_cashier().hasGroupCashinoutAction) {
-                        return (
-                            super.showCashMoveButton() ||
-                            this.env.pos.get_cashier().hasGroupCashinoutAction
-                        );
-                    }
-                } else if (this.env.pos.user.hasGroupCashinoutAction)
+                if (
+                    this.env.pos.cashier &&
+                    this.env.pos.cashier.hasGroupCashinoutAction
+                )
                     return (
                         super.showCashMoveButton() ||
-                        this.env.pos.user.hasGroupCashinoutAction
+                        this.env.pos.cashier.hasGroupCashinoutAction
                     );
+                return super.showCashMoveButton();
             }
         };
 
