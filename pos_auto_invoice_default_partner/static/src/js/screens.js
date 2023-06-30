36d72eb8a88e91f593f1638b207554459896420e
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-06-30 14:36:34 +0200

    [UPD] Run prettier

--- pos_auto_invoice_default_partner/static/src/js/screens.js
+++ pos_auto_invoice_default_partner/static/src/js/screens.js
@@ -1,10 +1,10 @@
-odoo.define("pos_auto_invoice_default_partner.screens", function (require) {
+odoo.define("pos_auto_invoice_default_partner.screens", function(require) {
     "use strict";
     var core = require("web.core");
     var screens = require("point_of_sale.screens");
 
     screens.PaymentScreenWidget.include({
-        finalize_validation: function () {
+        finalize_validation: function() {
             var order = this.pos.get_order();
             var client = order.get_client();
             var partner_id = this.pos.config.default_partner_id;
