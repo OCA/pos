1047080050c896ee2fabe14cc59c2e62231b502c
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-06-30 14:36:34 +0200

    [UPD] Run prettier

--- pos_auto_invoice/static/src/js/models.js
+++ pos_auto_invoice/static/src/js/models.js
@@ -1,11 +1,11 @@
-odoo.define("pos_auto_invoice.models", function (require) {
+odoo.define("pos_auto_invoice.models", function(require) {
     "use strict";
 
     var models = require("point_of_sale.models");
 
     var OrderSuper = models.Order;
     models.Order = models.Order.extend({
-        initialize: function (attributes, options) {
+        initialize: function(attributes, options) {
             OrderSuper.prototype.initialize.apply(this, arguments);
             this.to_invoice = true;
         },
