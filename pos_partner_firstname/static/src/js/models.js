868412f0c2325179614002e5d6e678d146c94fd2
Victor Champonnois <victor@coopiteasy.be>
2023-07-26 11:23:59 +0200

    [FIX] deprecated load_fields method
    Update pos_partner_firstname/models/pos_session.py
    
    Co-authored-by: hugues de keyzer <github@hugues.info>

--- pos_partner_firstname/static/src/js/models.js
+++
@@ -1,7 +0,0 @@
-odoo.define("pos_partner_firstname.models", function (require) {
-    "use strict";
-
-    const models = require("point_of_sale.models");
-    models.load_fields("res.partner", ["is_company", "firstname", "lastname"]);
-    return models;
-});
