82625b3b99471079c19b9a65553fae4df1116b5f
Juan Carlos B <juancarlos.bonilla@factorlibre.com>
2023-08-02 16:53:42 +0200

    [FIX] pos_partner_birthdate: update & save partner birthdate from PoS Interface.

---
+++ pos_partner_birthdate/static/src/js/ClientDetailsEdit.esm.js
@@ -0,0 +1,16 @@
+/** @odoo-module **/
+import PartnerDetailsEdit from "point_of_sale.PartnerDetailsEdit";
+import Registries from "point_of_sale.Registries";
+
+const PartnerDetailsEditBirthdate = (OriginalPartnerDetailsEdit) =>
+    class extends OriginalPartnerDetailsEdit {
+        setup() {
+            super.setup();
+            this.changes = {
+                ...this.changes,
+                birthdate_date: this.props.partner.birthdate_date || "",
+            };
+        }
+    };
+
+Registries.Component.extend(PartnerDetailsEdit, PartnerDetailsEditBirthdate);
