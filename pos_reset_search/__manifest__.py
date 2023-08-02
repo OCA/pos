c5923971b23b2f2decf5ba1e3bbb7a70b439cea6
Victor Champonnois <victor@coopiteasy.be>
2023-08-02 15:19:07 +0200

    [MIG] pos_reset_search: Migration to 15.0

--- pos_reset_search/__manifest__.py
+++ pos_reset_search/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Point of Sale - Clear product search on click",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.0",
     "category": "Point of Sale",
     "summary": "Point of Sale - Clear product search when user clicks on a product.",
     "author": "Le Nid, Odoo Community Association (OCA)",
@@ -12,8 +12,8 @@
     "depends": [
         "point_of_sale",
     ],
-    "data": [
-        "views/templates.xml",
-    ],
+    "assets": {
+        "point_of_sale.assets": ["/pos_reset_search/static/src/js/reset.js"],
+    },
     "installable": True,
 }
