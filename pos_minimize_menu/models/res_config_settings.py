1f300d42b17a891b91c5cb3d688766e3b6edc7c0
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-09-15 15:55:42 +0200

    [ADD] pos_minimize_menu : new module to save place to display current ticket

---
+++ pos_minimize_menu/models/res_config_settings.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2023 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    pos_iface_important_buttons = fields.Text(
+        related="pos_config_id.iface_important_buttons",
+        readonly=False,
+    )
