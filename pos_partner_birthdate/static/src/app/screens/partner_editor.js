f58c7c21df72ab089808090f6e815fb438902d20
rousemilano <routhberism@gmail.com>
2023-11-07 19:46:59 -0400

    [FIX] pos_partner_birthdate: 17.0 fix code

--- pos_partner_birthdate/static/src/app/screens/partner_editor.js
+++ pos_partner_birthdate/static/src/app/screens/partner_editor.js
@@ -1,4 +1,5 @@
 /** @odoo-module **/
+
 import {useState} from "@odoo/owl";
 import {patch} from "@web/core/utils/patch";
 import {PartnerDetailsEdit} from "@point_of_sale/app/screens/partner_list/partner_editor/partner_editor";
@@ -12,4 +13,3 @@ patch(PartnerDetailsEdit.prototype, {
         });
     },
 });
-
