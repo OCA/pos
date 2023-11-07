f1cac377e60e11a7e4250bd0ccec3571ba98190f
rousemilano <routhberism@gmail.com>
2023-11-07 19:34:11 -0400

    [FIX] pos_partner_birthdate: Migration to 17.0 fix code

--- pos_partner_birthdate/static/src/app/screens/partner_editor.js
+++ pos_partner_birthdate/static/src/app/screens/partner_editor.js
@@ -3,7 +3,7 @@ import {useState} from "@odoo/owl";
 import {patch} from "@web/core/utils/patch";
 import {PartnerDetailsEdit} from "@point_of_sale/app/screens/partner_list/partner_editor/partner_editor";
 
-patch(PartnerDetailsEdit.prototype,{
+patch(PartnerDetailsEdit.prototype, {
     setup() {
         super.setup();
         this.changes = useState({
