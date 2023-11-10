bf8f1f7cb8743d8d9a335f70848b4fe492c13d96
mymage <stefano.consolaro@mymage.it>
2023-11-10 10:27:32 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (42 of 42 strings)
    
    Translation: pos-14.0/pos-14.0-pos_cash_move_reason
    Translate-URL: https://translation.odoo-community.org/projects/pos-14-0/pos-14-0-pos_cash_move_reason/it/
    
    [ADD] - POS Products Available for each PdV
    
    [RFT] - Changes to pr
    
    [RFT] - Changes to PR
    
    [RFT] - Change to pr
    
    [RFT] - Changes to pr
    
    [RFT] - Changes to pr
    
    [RFT] - Changes to pr
    
    [RFT] - Changes to pr
    
    [RFT] - Changes to pr
    
    [RFT] - Changes to PR
    
    [RFT] - Changes to PR
    
    [RFT] - Changes to PR
    
    [RFT] - Changes to PR
    
    [RFT] - Changes to PR
    
    [RFT] - Changes to PR
    
    [RFT] - Changes to PR
    
    [RFT] - Changes to PR
    
    [RFT] - Changes to PR
    
    [RFT] - Changes to PR
    
    [RFT] - Changes to PR
    
    [RFT] - Changes to PR
    
    [RFT] - Changes to PR
    
    [RFT] - Changes to PR
    
    [ADD] - Setup pos_product_available
    
    [RFT] - Changes to PR
    
    [RFT] - Changes to PR

---
+++ pos_product_available/static/src/js/models.js
@@ -0,0 +1,49 @@
+odoo.define("pos_product_available.models", function (require) {
+    "use strict";
+    var models = require("point_of_sale.models");
+
+    models.PosModel.prototype.models.some(function (model) {
+        if (model.model !== "product.product") {
+            return false;
+        }
+        model.domain = function (self) {
+            var domain = [
+                "&",
+                "&",
+                ["sale_ok", "=", true],
+                ["available_in_pos", "=", true],
+                "|",
+                ["company_id", "=", self.config.company_id[0]],
+                ["company_id", "=", false],
+            ];
+            if (
+                self.config.limit_categories &&
+                self.config.iface_available_categ_ids.length
+            ) {
+                domain.unshift("&");
+                domain.push([
+                    "pos_categ_id",
+                    "in",
+                    self.config.iface_available_categ_ids,
+                ]);
+                if (
+                    self.config.available_product &&
+                    self.config.available_product_ids.length
+                ) {
+                    domain.unshift("&");
+                    domain.push([
+                        "product_tmpl_id",
+                        "in",
+                        self.config.available_product_ids,
+                    ]);
+                }
+            }
+            if (self.config.iface_tipproduct) {
+                domain.unshift(["id", "=", self.config.tip_product_id[0]]);
+                domain.unshift("|");
+            }
+            return domain;
+        };
+        return true;
+    });
+});
