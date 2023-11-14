f3a1f2a685841c1382a759dd920a28ed9ed86dbd
Carlos Franco <cfranco@esment.org>
2023-11-14 14:03:06 +0100

    [ADD] - POS Products Available for each PdV

---
+++ pos_product_available/models/pos_config.py
@@ -0,0 +1,12 @@
+from odoo import fields, models
+
+
+class PosConfig(models.Model):
+    _inherit = "pos.config"
+    _description = "Point of Sale Config Visible Products"
+
+    available_product = fields.Boolean()
+    available_product_ids = fields.Many2many(
+        comodel_name='product.template',
+        string='Available Products',
+    )
---
+++ pos_product_available/models/product.py
@@ -0,0 +1,12 @@
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+    _description = "Point of Sale Visible Product"
+    
+    pos_center_ids = fields.Many2many(
+        comodel_name='pos.config',
+        string='PdV Visibles',
+    )   
+    
