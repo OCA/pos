c9d0423b272421878bbda3a4ae6cf07a5978f17e
Jose Zambudio <jose@aurestic.es>
2024-01-09 11:04:30 +0100

    [IMP] pos_order_to_sale_order: black, isort, prettier

--- pos_order_to_sale_order/__manifest__.py
+++ pos_order_to_sale_order/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "PoS Order To Sale Order",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "GRAP,Odoo Community Association (OCA)",
     "category": "Point Of Sale",
     "license": "AGPL-3",
