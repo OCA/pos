00eb46cb93b424b09f028c53ace692ca93c704e6
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-01-17 01:02:28 +0100

    [IMP] pos_ticket_extra_company_info: pre-commit execution

--- pos_ticket_extra_company_info/models/pos_config.py
+++ pos_ticket_extra_company_info/models/pos_config.py
@@ -2,7 +2,7 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class PosConfig(models.Model):
