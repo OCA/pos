76284db0dade2e9f35c6b6ad9275507694b981a9
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-01-18 14:28:58 +0100

    [FIX] eslint error 'no-shadow' in all modules

--- pos_access_right/static/src/js/ActionpadWidget.js
+++ pos_access_right/static/src/js/ActionpadWidget.js
@@ -4,8 +4,8 @@ odoo.define("pos_access_right.ActionpadWidget", function (require) {
     const Registries = require("point_of_sale.Registries");
     const ActionpadWidget = require("point_of_sale.ActionpadWidget");
 
-    const PosActionpadWidget = (ActionpadWidget) =>
-        class extends ActionpadWidget {
+    const PosActionpadWidget = (OriginalActionpadWidget) =>
+        class extends OriginalActionpadWidget {
             get hasPaymentControlRights() {
                 return this.env.pos.user.hasGroupPayment;
             }
