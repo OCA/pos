0bbb887953610dad52d4e460f26cf5c5ccd0c845
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-01-18 14:14:24 +0100

    [IMP] pos_ticket_extra_company_info_l10n_fr: pre-commit execution

--- pos_ticket_extra_company_info_l10n_fr/static/src/js/db.js
+++ pos_ticket_extra_company_info_l10n_fr/static/src/js/db.js
@@ -4,10 +4,10 @@ Copyright (C) 2023-Today: GRAP (<http://www.grap.coop/>)
 License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 */
 
-odoo.define('pos_ticket_extra_company_info_l10n_fr.db', function (require) {
+odoo.define("pos_ticket_extra_company_info_l10n_fr.db", function (require) {
     "use strict";
 
-    var models = require('point_of_sale.models');
+    var models = require("point_of_sale.models");
 
-    models.load_fields("res.company", ['siret']);
+    models.load_fields("res.company", ["siret"]);
 });
