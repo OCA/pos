03c0c8a00096c7db130b3301d09a44ed94db1d93
dessanhemrayev <dessanhemrayev@gmail.com>
2024-03-16 22:48:38 +0300

    [FIX] pos_sale_order_load: fix bug
    The part of the code where the _loadPartners method is called has been deleted
    
    Fix a bug when loading a order partner

--- pos_sale_order_load/static/src/js/OrderManagementScreen/SaleOrderManagementScreen.js
+++ pos_sale_order_load/static/src/js/OrderManagementScreen/SaleOrderManagementScreen.js
@@ -97,7 +97,7 @@ odoo.define("pos_sale_order_load.SaleOrderManagementScreen", function (require) 
                 const currentPOSOrder = this.env.pos.get_order();
                 const sale_order = await this._getSaleOrder(clickedOrder.id);
                 try {
-                    await this.env.pos.load_new_partners();
+                    await this.env.pos.load_new_partners(sale_order.partner_id[0]);
                 } catch (error) {}
                 const order_partner = this.env.pos.db.get_partner_by_id(
                     sale_order.partner_id[0]
@@ -105,23 +105,13 @@ odoo.define("pos_sale_order_load.SaleOrderManagementScreen", function (require) 
                 if (order_partner) {
                     currentPOSOrder.set_client(order_partner);
                 } else {
-                    try {
-                        await this.env.pos._loadPartners([sale_order.partner_id[0]]);
-                    } catch (error) {
-                        const title = this.env._t("Customer loading error");
-                        const body = _.str.sprintf(
-                            this.env._t(
-                                "There was a problem in loading the %s customer."
-                            ),
-                            sale_order.partner_id[1]
-                        );
-                        await this.showPopup("ErrorPopup", {title, body});
-                    }
-                    currentPOSOrder.set_client(
-                        this.env.pos.db.get_partner_by_id(sale_order.partner_id[0])
+                    const title = this.env._t("Customer loading error");
+                    const body = _.str.sprintf(
+                        this.env._t("There was a problem in loading the %s customer."),
+                        sale_order.partner_id[1]
                     );
+                    await this.showPopup("ErrorPopup", {title, body});
                 }
-
                 const orderFiscalPos = sale_order.fiscal_position_id
                     ? this.env.pos.fiscal_positions.find(
                           (position) => position.id === sale_order.fiscal_position_id[0]
