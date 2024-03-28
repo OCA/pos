6e14f74b4e0a010c3ffa472e0ab6d7f117015ddc
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-03-28 23:11:13 +0100

    [MIG] pos_hide_banknote_button: Migration to 16.0 (from 12.0)

--- pos_hide_banknote_button/__manifest__.py
+++ pos_hide_banknote_button/__manifest__.py
@@ -4,17 +4,17 @@
 
 {
     "name": "Point of Sale - Hide Banknote Buttons",
-    "version": "12.0.1.0.1",
+    "version": "16.0.1.0.0",
     "category": "Point Of Sale",
     "summary": "Hide useless Banknote buttons in the PoS (+10, +20, +50)",
     "author": "GRAP, Odoo Community Association (OCA)",
     "maintainers": ["legalsylvain"],
     "website": "https://github.com/OCA/pos",
     "license": "AGPL-3",
-    "depends": [
-        "point_of_sale",
-    ],
-    "data": [
-        "views/assets.xml",
-    ],
+    "depends": ["point_of_sale"],
+    "assets": {
+        "point_of_sale.assets": [
+            "pos_hide_banknote_button/static/src/scss/pos_hide_banknote_button.scss",
+        ],
+    },
 }
