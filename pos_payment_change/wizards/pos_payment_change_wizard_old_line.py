4a5a3f56b33980fb476ca19f8d9b55b8221d5ca7
Alexis de Lattre <alexis.delattre@akretion.com>
2024-04-12 19:41:20 +0200

    [FIX] pos_payment_change: ondelete='cascade' for O2M fields
    [BOT] post-merge updates

--- pos_payment_change/wizards/pos_payment_change_wizard_old_line.py
+++ pos_payment_change/wizards/pos_payment_change_wizard_old_line.py
@@ -12,6 +12,7 @@ class PosPaymentChangeWizardOldLine(models.TransientModel):
     wizard_id = fields.Many2one(
         comodel_name="pos.payment.change.wizard",
         required=True,
+        ondelete="cascade",
     )
 
     old_payment_method_id = fields.Many2one(
