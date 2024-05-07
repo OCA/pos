a50c7f6e8a024e0688938d62f284585e5aff941f
Carlos Franco <cfranco@esment.org>
2024-05-07 13:34:50 +0200

    [ADD] pos_product_available - Product filter for each POS

---
+++ pos_product_available/models/__init__.py
@@ -0,0 +1,3 @@
+from . import product
+from . import res_config_settings
+from . import pos_session
---
+++ pos_product_available/models/res_config_settings.py
@@ -0,0 +1,26 @@
+from odoo import fields, models, api
+
+
+class PosConfig(models.Model):
+    _inherit = "pos.config"
+
+    available_product = fields.Boolean()
+    available_product_ids = fields.Many2many(
+        comodel_name='product.template',
+        string='Restrict products for this point of sale'
+    )
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+    _description = "Point of Sale Config Visible Products"
+
+    pos_available_product = fields.Boolean(related='pos_config_id.available_product', readonly=False)
+    pos_available_product_ids = fields.Many2many('product.template', string='Restrict products for this point of sale', compute='_compute_pos_available_product_ids', readonly=False, store=True)
+
+    @api.depends('pos_available_product', 'pos_config_id')
+    def _compute_pos_available_product_ids(self):
+        for res_config in self:
+            if not res_config.pos_available_product:
+                res_config.pos_available_product_ids = False
+            else:
+                res_config.pos_available_product_ids = res_config.pos_config_id.available_product_ids
