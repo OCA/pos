ff4ff29ca5b6ae7e3aebfbd65151ba4682634450
Carlos Franco <cfranco@esment.org>
2024-05-08 16:06:56 +0200

    [RFT] pos_product_available - Pre-commit requirements

--- pos_product_available/models/product.py
+++ pos_product_available/models/product.py
@@ -5,6 +5,4 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
     _description = "Point of Sale Visible Product"
 
-    pos_center_ids = fields.Many2many(
-        "pos.config", string="POS Available", store=True
-    )
+    pos_center_ids = fields.Many2many("pos.config", string="POS Available", store=True)
