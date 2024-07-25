27e28603d85cd7f7a615aaf875142574a3946dd2
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-07-25 12:20:35 +0200

    [ADD] new module pos_payment_decription to see payment details in pos.order tree view

---
+++ pos_payment_description/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2024 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "PoS Payment Description",
+    "summary": "Payment description on order tree view",
+    "version": "16.0.1.0.0",
+    "category": "Point Of Sale",
+    "author": "GRAP, Odoo Community Association (OCA)",
+    "maintainers": ["legalsylvain"],
+    "website": "https://github.com/OCA/pos",
+    "license": "AGPL-3",
+    "depends": ["point_of_sale"],
+    "data": [
+        "views/view_pos_order.xml",
+    ],
+    "installable": True,
+}
