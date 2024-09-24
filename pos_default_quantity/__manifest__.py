56c7f825fd60ca0c83ead034b3b446a6ce5ba6f2
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2024-09-24 10:58:05 +0200

    [MIG] pos_default_quantity: Migration to 13.0
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- pos_default_quantity/__manifest__.py
+++ pos_default_quantity/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Set Default Product Quantity in POS",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.0",
     "author": "Coop IT Easy SC, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/pos",
     "license": "AGPL-3",
