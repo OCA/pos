f5fd741cd510723e7181f798c2a8ad5673c31cbb
Telmo Santos <telmo.santos@camptocamp.com>
2024-09-25 14:43:32 +0200

    [ADD] pos_partner_alternative_pricelist_load_background

---
+++ pos_partner_alternative_pricelist_load_background/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Camptocamp (https://www.camptocamp.com).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "POS Partner Alternative Pricelist Load Background",
+    "summary": "Load partner alternative pricelist in background",
+    "version": "16.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pos",
+    "license": "AGPL-3",
+    "category": "Point of Sale",
+    "depends": [
+        "pos_product_pricelist_alternative",
+        "pos_partner_pricelist_load_background",
+    ],
+    "installable": True,
+}
---
+++ pos_partner_alternative_pricelist_load_background/models/pos_session.py
@@ -0,0 +1,42 @@
+# Copyright 2024 Camptocamp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import models
+
+
+class POSSession(models.Model):
+    _inherit = "pos.session"
+
+    def get_pos_ui_partner_pricelist_background(self, pricelist_id, product_ids):
+        params = self._loader_params_product_pricelist()
+        fnames = params["search_params"]["fields"]
+        pricelist_rec = self.env["product.pricelist"].browse(pricelist_id)
+        pricelist = pricelist_rec.read(fnames)[0]
+        pricelist["items"] = []
+
+        products = (
+            self.env["product.product"].browse(product_ids)
+            | pricelist_rec.item_ids.product_id
+        )
+        templates = products.product_tmpl_id | pricelist_rec.item_ids.product_tmpl_id
+        pricelist_item_domain = [
+            ("pricelist_id", "=", pricelist_id),
+            "|",
+            ("product_tmpl_id", "=", False),
+            ("product_tmpl_id", "in", templates.ids),
+            "|",
+            ("product_id", "=", False),
+            ("product_id", "in", products.ids),
+        ]
+        for item in self.env["product.pricelist.item"].search_read(
+            pricelist_item_domain, self._product_pricelist_item_fields()
+        ):
+            pricelist["items"].append(item)
+
+        alternative_pricelists = []
+        for alt_pricelist_rec in pricelist_rec.alternative_pricelist_ids:
+            alternative_pricelists += self.get_pos_ui_partner_pricelist_background(
+                alt_pricelist_rec.id, product_ids
+            )
+        if alternative_pricelists:
+            return [pricelist] + alternative_pricelists
+        return [pricelist]
