5b2790b6c4424f09788be6d0afdef30138acee3a
LorenzoC0 <lorenzo.carta@innoyou.it>
2024-10-14 15:13:30 +0200

    [ADD] pos_full_refund: Added module to autocomplete full refunds on ticket screen.

---
+++ pos_full_refund/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Point of Sale - ",
+    "summary": "Short (1 phrase/line) summary of the module's purpose",
+    "author": "Innovyou, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pos",
+    "development_status": "Alpha",
+    "category": "Point of sale",
+    "maintainers": ["LorenzoC0"],
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["point_of_sale"],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "pos_full_refund/static/src/js/pos_full_refund.js",
+            "pos_full_refund/static/src/xml/pos_full_refund.xml",
+        ],
+    },
+}
