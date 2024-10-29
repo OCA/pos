8e4b119882534bf4a1ea3212c991217ecbe47137
hugues de keyzer <odoo@hugues.info>
2024-10-29 17:19:17 +0100

    [MIG] pos_auto_invoice: migration to 15.0

--- pos_auto_invoice/__manifest__.py
+++ pos_auto_invoice/__manifest__.py
@@ -5,19 +5,17 @@
     "name": "Point of Sale Automatically Invoice",
     "summary": """
         In the POS, set orders as to-invoice by default.""",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Point of Sale",
     "website": "https://coopiteasy.be",
     "author": "Coop IT Easy SC",
     "license": "AGPL-3",
-    "application": False,
     "depends": [
         "point_of_sale",
     ],
-    "excludes": [],
-    "data": [
-        "templates/assets.xml",
-    ],
-    "demo": [],
-    "qweb": [],
+    "assets": {
+        "point_of_sale.assets": [
+            "pos_auto_invoice/static/src/js/*.js",
+        ]
+    },
 }
