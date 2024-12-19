b54a0d93b6cd96c753922a501e41e9302618e0d3
Holger Brunn <mail@hunki-enterprises.com>
2024-12-19 19:35:11 +0100

    [FIX] pos_ticket_extra_company_info_l10n_fr: don't duplicate pos_discount_all

--- pos_ticket_extra_company_info_l10n_fr/static/src/js/models.esm.js
+++ pos_ticket_extra_company_info_l10n_fr/static/src/js/models.esm.js
@@ -4,7 +4,7 @@ Copyright (C) 2024 - Today: GRAP (http://www.grap.coop)
 License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 */
 
-odoo.define("pos_discount_all.models", function (require) {
+odoo.define("pos_ticket_extra_company_info_l10n_fr.models", function (require) {
     const {Order} = require("point_of_sale.models");
     const Registries = require("point_of_sale.Registries");
 
