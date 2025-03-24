1739b5e675f1d3f679f3e1e20a3cffa5cf274b18
Antoni Marroig Campomar <amarroig@apsl.net>
2025-03-24 12:09:45 +0100

    [17.0][FIX] pos_user_restriction: Fix can't close session with POS Assigned User role

--- pos_user_restriction/models/pos_session.py
+++ pos_user_restriction/models/pos_session.py
@@ -10,3 +10,17 @@ class PosSession(models.Model):
         ):
             self = self.with_context(bypass_pos_user=True)
         return super().get_closing_control_data()
+
+    def _validate_session(
+        self,
+        balancing_account=False,
+        amount_to_balance=0,
+        bank_payment_method_diffs=None,
+    ):
+        if self.env.user.has_group(
+            "pos_user_restriction.group_assigned_points_of_sale_user"
+        ):
+            self = self.with_context(bypass_pos_user=True)
+        return super()._validate_session(
+            balancing_account, amount_to_balance, bank_payment_method_diffs
+        )
