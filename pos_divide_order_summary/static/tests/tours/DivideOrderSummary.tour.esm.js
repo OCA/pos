eb5aaad98898137deb80a22194083c74ec7d0910
trisdoan <doanminhtri8183@gmail.com>
2025-03-28 15:33:44 +0700

    [MIG] pos_divide_order_summary: Migration to 18.0

--- pos_divide_order_summary/static/tests/tours/DivideOrderSummary.tour.esm.js
+++ pos_divide_order_summary/static/tests/tours/DivideOrderSummary.tour.esm.js
@@ -1,23 +1,19 @@
-/** @odoo-module */
-
-import * as Chrome from "@point_of_sale/../tests/tours/helpers/ChromeTourMethods";
-import * as ProductScreen from "@point_of_sale/../tests/tours/helpers/ProductScreenTourMethods";
 import {registry} from "@web/core/registry";
+import * as Chrome from "@point_of_sale/../tests/tours/utils/chrome_util";
+import * as Dialog from "@point_of_sale/../tests/tours/utils/dialog_util";
+import * as ProductScreen from "@point_of_sale/../tests/tours/utils/product_screen_util";
+import {inLeftSide} from "@point_of_sale/../tests/tours/utils/common";
 
 registry.category("web_tour.tours").add("DivideOrderSummary", {
-    test: true,
-    url: "/pos/ui",
     steps: () =>
         [
-            ProductScreen.confirmOpeningPopup(),
-            // Go by default to home category
-            ProductScreen.clickHomeCategory(),
+            Chrome.startPoS(),
+            Dialog.confirm("Open Register"),
             ProductScreen.clickDisplayedProduct("Desk Organizer"),
-            {
+            inLeftSide({
                 content: "Check Summary is divided",
                 trigger: ".product-screen:has(div.summary-left)",
-                isCheck: true,
-            },
+            }),
             Chrome.endTour(),
         ].flat(),
 });
