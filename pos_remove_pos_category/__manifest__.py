427aaf552c49d4665dfb4ea9f583be96ad526967
Thibault Rey <thibault.rey@akretion.com>
2025-03-31 22:34:58 +0200

    [MIG] pos_remove_pos_category: Migration to 18.0

--- pos_remove_pos_category/__manifest__.py
+++ pos_remove_pos_category/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "POS Remove POS Category",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Akretion, Camptocamp SA, ACSONE SA/NV, "
     "Odoo Community Association (OCA)",
     "category": "Point of Sale",
