13f6d9ae0aa59f8957362842de2f959d9e49147d
Thibault Rey <thibault.rey@akretion.com>
2025-04-03 16:48:07 +0200

    [MIG] pos_show_config_name: Migration to 18.0

--- pos_show_config_name/__manifest__.py
+++ pos_show_config_name/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "PoS show config name",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Point Of Sale",
     "summary": "Point of sale: show pos config name",
     "author": "Odoo Community Association (OCA)",
