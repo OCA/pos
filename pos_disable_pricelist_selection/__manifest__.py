b7c849f04e5d01cc8fcfcf3f4dafd1ca3c85fe7b
kopeyev <kopeyev07@gmail.com>
2025-07-16 19:06:23 +0500

    [MIG] pos_disable_pricelist_selection: Migration to 17.0

--- pos_disable_pricelist_selection/__manifest__.py
+++ pos_disable_pricelist_selection/__manifest__.py
@@ -1,18 +1,22 @@
 {
-    "name": "Disable Pricelist selection button in POS",
-    "version": "14.0.2.0.1",
+    "name": "POS Disable Pricelist Selection",
     "summary": "Disable Pricelist selection button in POS",
-    "author": "Ooops, Cetmix, Odoo Community Association (OCA)",
-    "contributors": "Cetmix",
-    "license": "LGPL-3",
-    "category": "Purchase",
-    "website": "https://github.com/OCA/pos",
+    "version": "17.0.1.0.0",
+    "category": "Point of Sale",
+    "author": "Ooops, Cetmix, Almas Kopeyev, IT-Projects LLC, Odoo Community Association (OCA)",
+    "contributors": ["Cetmix", "Almas Kopeyev"],
     "maintainers": ["ilyasprogrammer"],
+    "website": "https://github.com/OCA/pos",
+    "license": "LGPL-3",
     "depends": ["point_of_sale"],
-    "external_dependencies": {},
-    "demo": [],
-    "data": ["views/assets.xml", "views/pos_config_view.xml"],
-    "qweb": ["static/src/xml/ClientDetailsEdit.xml"],
+    "data": [
+        "views/pos_config_view.xml"
+    ],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "pos_disable_pricelist_selection/static/src/**/*",
+        ]
+    },
     "installable": True,
-    "application": False,
+    "application": False
 }
---
+++ pos_disable_pricelist_selection/static/src/js/PosStore.js
@@ -0,0 +1,28 @@
+/** @odoo-module **/
+
+import { patch } from "@web/core/utils/patch";
+import { PosStore } from "@point_of_sale/app/store/pos_store";
+
+patch(PosStore.prototype, {
+    async loadServerData(...args) {
+        await super.loadServerData(...args);
+
+        const { config, pricelists_by_id } = this;
+        const initialConfigPricelist = pricelists_by_id[config.pricelist_id?.[0]];
+
+        if (!config.use_pricelist || config.hide_pricelist_button) {
+            this.pricelists = initialConfigPricelist ? [initialConfigPricelist] : [];
+            this.selectable_pricelists = initialConfigPricelist ? [initialConfigPricelist] : [];
+        }
+
+        if (initialConfigPricelist) {
+            this.set_pricelist(initialConfigPricelist);
+        } else if (this.default_pricelist) {
+            this.set_pricelist(this.default_pricelist);
+        } else if (this.pricelists.length > 0) {
+            this.set_pricelist(this.pricelists[0]);
+        } else {
+            this.set_pricelist(null);
+        }
+    },
+});
