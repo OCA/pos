d061de808af690ccc7fec956b471dc28f21ab6a4
Sergio Bustamante <sergio.bustamante@factorlibre.com>
2025-08-22 07:40:03 +0200

    [FIX] pos_default_partner: Post-install test + fallback to load CoA

--- pos_default_partner/tests/test_pos_default_partner.py
+++ pos_default_partner/tests/test_pos_default_partner.py
@@ -9,8 +9,17 @@ from odoo.addons.point_of_sale.tests.common import TestPoSCommon
 @odoo.tests.tagged("post_install", "-at_install")
 class TestPosDefaultPartner(TestPoSCommon):
     @classmethod
-    def setUpClass(cls, chart_template_ref=None):
-        super().setUpClass(chart_template_ref=chart_template_ref)
+    def setUpClass(cls):
+        super().setUpClass()
+        if not cls.env.company.chart_template_id:
+            # Load a CoA if there's none in current company
+            coa = cls.env.ref("l10n_generic_coa.configurable_chart_template", False)
+            if not coa:
+                # Load the first available CoA
+                coa = cls.env["account.chart.template"].search(
+                    [("visible", "=", True)], limit=1
+                )
+            coa.try_loading(company=cls.env.company, install_demo=False)
         cls.config = cls.basic_config
         cls.PosOrder = cls.env["pos.order"]
         # ==== Partners ====
