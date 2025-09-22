cb9879f921f6e93a2923d48bd2a41bd9bfbaff5a
angel <angel.garcia@sygel.es>
2025-09-22 16:08:27 +0200

    [17.0][ADD] pos_early_receipt_printing

---
+++ pos_early_receipt_printing/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "POS Early Receipt Printing",
+    "summary": "Generate bill from Shop",
+    "version": "17.0.1.0.0",
+    "category": "POS",
+    "website": "https://github.com/OCA/pos",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["pos_restaurant"],
+    "data": ["views/pos_config_views.xml"],
+}
