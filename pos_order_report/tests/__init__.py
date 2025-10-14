a015d40944aac9ff0d545f444134bdd02f0ede73
Nguyen Minh Chien <chien@trobz.com>
2025-10-14 16:21:38 +0700

    [MIG] pos_order_report: Migration to 18.0

---
+++ pos_order_report/tests/__init__.py
@@ -0,0 +1,1 @@
+from . import test_pos_order_report_export
--- pos_order_report/wizard/pos_order_report_wizard.py
+++ pos_order_report/wizard/pos_order_report_wizard.py
@@ -1,58 +1,142 @@
-import base64
-import datetime
-import logging
-import time
-from collections import deque
-from io import BytesIO
+import json
 
-import dateutil
-
-from odoo import _, fields, models
-from odoo.exceptions import UserError
-from odoo.tools import safe_eval, ustr
-from odoo.tools.func import lazy
-from odoo.tools.misc import xlwt
-
-_logger = logging.getLogger(__name__)
+from odoo import fields, models
+from odoo.tools import format_date, safe_eval
 
 
 class PosOrderReportWizard(models.TransientModel):
     _name = "pos.order.report.wizard"
-    _description = "Pos Order Report Wizard"
+    _description = "POS Order Pivot Export Wizard"
 
     horizontal_axis_id = fields.Many2one(
-        comodel_name="ir.model.fields",
-        required=True,
-        domain="[('model', '=', 'report.pos.order'), ('store', '=', True), "
-        "('ttype', 'not in', ('float', 'integer'))]",
+        "ir.model.fields",
+        string="Horizontal Axis (Group By)",
+        domain=[
+            ("model", "=", "report.pos.order"),
+            ("store", "=", True),
+            (
+                "ttype",
+                "in",
+                ["many2one", "selection", "char", "boolean", "date", "datetime"],
+            ),
+        ],
     )
     vertical_axis_id = fields.Many2one(
-        comodel_name="ir.model.fields",
-        required=True,
-        domain="[('model', '=', 'report.pos.order'), ('store', '=', True), "
-        "('ttype', 'not in', ('float', 'integer'))]",
+        "ir.model.fields",
+        string="Vertical Axis (Group By)",
+        domain=[
+            ("model", "=", "report.pos.order"),
+            ("store", "=", True),
+            (
+                "ttype",
+                "in",
+                ["many2one", "selection", "char", "boolean", "date", "datetime"],
+            ),
+        ],
     )
     measure_ids = fields.Many2many(
-        comodel_name="ir.model.fields",
-        relation="pos_orw_field_ms_rel",
-        required=True,
+        "ir.model.fields",
+        string="Measures",
         default=lambda self: self.get_default_measure(),
-        domain="[('model', '=', 'report.pos.order'), ('store', '=', True), "
-        "('name', '!=', 'id'), '|', ('ttype', 'in', ('float', 'integer')), ('name', '=', 'order_id')]",
+        domain=[
+            ("model", "=", "report.pos.order"),
+            ("store", "=", True),
+            ("ttype", "in", ["integer", "float", "monetary"]),
+            ("name", "!=", "id"),
+        ],
     )
-    domain = fields.Char(string="Filters")
+    domain = fields.Char(
+        string="Domain (in JSON)",
+        help="Optional domain in JSON format, e.g. [['state','=','paid']]",
+    )
     output_file = fields.Binary()
+
+    def get_default_measure(self, force=False):
+        args = [("model", "=", "report.pos.order"), ("store", "=", True)]
+        if force:
+            args.append(("name", "=", "order_id"))
+        else:
+            args.append(("name", "in", ("order_id", "price_subtotal_excl")))
+        field_names = self.env["ir.model.fields"].search(args)
+        return field_names
 
     def _get_eval_context(self):
         return {
-            "datetime": datetime,
-            "dateutil": dateutil,
-            "time": time,
+            "datetime": safe_eval.datetime,
+            "dateutil": safe_eval.dateutil,
+            "time": safe_eval.time,
             "uid": self.env.uid,
             "user": self.env.user,
         }
 
-    def prepared_data(self, raw_datas):
+    def prepared_data(self, raw_datas):  # noqa: C901
+        """Prepare data for pivot export"""
+        self.ensure_one()
+        model_name = "report.pos.order"
+        Model = self.env[model_name]
+
+        # --- Fetch field metadata
+        field_recs = (
+            self.env["ir.model.fields"].sudo().search([("model", "=", model_name)])
+        )
+        model_fields = {f.name: f for f in field_recs}
+
+        # --- Prepare type + selection maps
+        field_selection_map = {}
+        field_types = {}
+        for field_name, field_def in Model._fields.items():
+            field_types[field_name] = field_def.type
+            if field_def.type == "selection":
+                field_selection_map[field_name] = dict(field_def.selection)
+
+        # User info
+        user = self.env.user
+        user_lang = self.env["res.lang"].search([("code", "=", user.lang)], limit=1)
+        date_format = user_lang.date_format or "%Y-%m-%d"
+        time_format = getattr(user_lang, "time_format", "%H:%M:%S")
+        user_tz = user.tz or "UTC"
+
+        def get_label(field_name):
+            fld = model_fields.get(field_name)
+            return self.env._(fld.field_description) if fld else self.env._(field_name)
+
+        def format_value(field_name, value):
+            if value is None:
+                return ""
+            ftype = field_types.get(field_name)
+
+            # Many2one
+            if isinstance(value, tuple):
+                return value[1]
+
+            # Selection
+            if field_name in field_selection_map:
+                # Note: Odoo selection labels are already translated
+                return field_selection_map[field_name].get(value, value)
+
+            # Date
+            if ftype == "date":
+                try:
+                    if isinstance(value, str):
+                        value = fields.Date.from_string(value)
+                    return format_date(self.env, value, lang_code=user.lang)
+                except Exception:
+                    return str(value)
+
+            # Datetime
+            if ftype == "datetime":
+                try:
+                    if isinstance(value, str):
+                        value = fields.Datetime.from_string(value)
+                    local_dt = fields.Datetime.context_timestamp(
+                        self.with_context(tz=user_tz), value
+                    )
+                    return local_dt.strftime(f"{date_format} {time_format}")
+                except Exception:
+                    return str(value)
+
+            return value
+
         start_id = inc_id = 195
         datas = {
             "headers": [
@@ -60,7 +144,7 @@ class PosOrderReportWizard(models.TransientModel):
                     {
                         "width": 0,
                         "height": 1,
-                        "title": _("Total"),
+                        "title": self.env._("Total"),
                         "id": start_id,
                         "expanded": True,
                     },
@@ -69,7 +153,6 @@ class PosOrderReportWizard(models.TransientModel):
             "measure_row": [],
             "rows": [],
             "nbr_measures": len(self.measure_ids) or 1,
-            "title": _("Point of Sale Analysis"),
         }
         headers = datas["headers"]
         measure_row = datas["measure_row"]
@@ -93,14 +176,10 @@ class PosOrderReportWizard(models.TransientModel):
                 horizontal_names.append(horizontal_name)
                 inc_id += 1
                 col_ids.append(inc_id)
-                horizontal_title = horizontal_name
-                if isinstance(horizontal_name, tuple) and len(horizontal_name) == 2:
-                    if isinstance(horizontal_name[1], lazy):
-                        horizontal_title = horizontal_name[1]._value
                 header_val = {
                     "width": datas["nbr_measures"],
                     "height": 1,
-                    "title": horizontal_title,
+                    "title": str(format_value(field_name, horizontal_name)),
                     "id": inc_id,
                     "expanded": False,
                 }
@@ -110,7 +189,7 @@ class PosOrderReportWizard(models.TransientModel):
                 for measure in self.measure_ids:
                     mrows.append(
                         {
-                            "measure": measure.field_description,
+                            "title": get_label(measure.name),
                             "is_bold": False,
                             "id": inc_id,
                             "field_name": measure.name,
@@ -124,7 +203,7 @@ class PosOrderReportWizard(models.TransientModel):
         for measure in self.measure_ids:
             mrows.append(
                 {
-                    "measure": measure.field_description,
+                    "title": get_label(measure.name),
                     "is_bold": True,
                     "id": start_id,
                     "field_name": measure.name,
@@ -147,18 +226,16 @@ class PosOrderReportWizard(models.TransientModel):
             for raw_data in raw_datas:
                 if raw_data.get(self.vertical_axis_id.name) == vertical_name:
                     raw_data_vertical.append(raw_data)
-            row_datas = self.generate_row(raw_data_vertical, title_datas, measure_row)
+            row_datas = self.generate_row(
+                raw_data_vertical, title_datas, measure_row, format_value
+            )
             inc_id += 1
-            vertical_title = vertical_name
-            if isinstance(vertical_name, tuple) and len(vertical_name) == 2:
-                if isinstance(vertical_name[1], lazy):
-                    vertical_title = vertical_name[1]._value
             rows.append(
                 {
                     "id": inc_id,
                     "col_ids": col_ids,
                     "indent": 1,
-                    "title": vertical_title,
+                    "title": str(format_value(field_name, vertical_name)),
                     "expanded": False,
                     "values": row_datas,
                 }
@@ -174,32 +251,34 @@ class PosOrderReportWizard(models.TransientModel):
                         total_val += r["values"][idx].get("value")
                 total_datas.append({"is_bold": True, "value": total_val})
         else:
-            total_datas = self.generate_row(raw_datas, title_datas, measure_row)
+            total_datas = self.generate_row(
+                raw_datas, title_datas, measure_row, format_value
+            )
         rows.insert(
             0,
             {
                 "id": start_id - 1,
                 "col_ids": col_ids,
                 "indent": 0,
-                "title": _("Total"),
+                "title": self.env._("Total"),
                 "expanded": True,
                 "values": total_datas,
             },
         )
         return datas
 
-    def generate_row(self, raw_datas, title_datas, measure_row):
+    def generate_row(self, raw_datas, title_datas, measure_row, format_value):
         row_datas = []
         for mrow in measure_row[0 : -len(self.measure_ids)]:
             mrow_datas = {"is_bold": False, "value": 0}
             title = self.get_title(title_datas, mrow.get("id"))
             for raw_data in raw_datas:
-                horizontal_name = raw_data.get(self.horizontal_axis_id.name)
-                if isinstance(horizontal_name, tuple) and len(horizontal_name) == 2:
-                    if isinstance(horizontal_name[1], lazy):
-                        horizontal_name = horizontal_name[1]._value
-                    else:
-                        horizontal_name = horizontal_name[1]
+                horizontal_name = str(
+                    format_value(
+                        self.horizontal_axis_id.name,
+                        raw_data.get(self.horizontal_axis_id.name),
+                    )
+                )
                 if horizontal_name != title:
                     continue
                 mrow_datas["value"] = raw_data.get(mrow.get("field_name"), 0)
@@ -220,133 +299,57 @@ class PosOrderReportWizard(models.TransientModel):
                 return d.get("title")
         return None
 
-    def export_xls(self, jdata):
-        nbr_measures = jdata["nbr_measures"]
-        workbook = xlwt.Workbook()
-        worksheet = workbook.add_sheet(jdata["title"])
-        header_bold = xlwt.easyxf(
-            "font: bold on; pattern: pattern solid, fore_colour gray25;"
-        )
-        header_plain = xlwt.easyxf("pattern: pattern solid, fore_colour gray25;")
-        bold = xlwt.easyxf("font: bold on;")
-
-        # Step 1: writing headers
-        headers = jdata["headers"]
-
-        # x,y: current coordinates
-        # carry: queue containing cell information when a cell has a >= 2 height
-        #      and the drawing code needs to add empty cells below
-        x, y, carry = 1, 0, deque()
-        for i, header_row in enumerate(headers):
-            worksheet.write(i, 0, "", header_plain)
-            for header in header_row:
-                while carry and carry[0]["x"] == x:
-                    cell = carry.popleft()
-                    for i in range(nbr_measures):
-                        worksheet.write(y, x + i, "", header_plain)
-                    if cell["height"] > 1:
-                        carry.append({"x": x, "height": cell["height"] - 1})
-                    x = x + nbr_measures
-                style = header_plain if "expanded" in header else header_bold
-                for i in range(header["width"]):
-                    worksheet.write(y, x + i, header["title"] if i == 0 else "", style)
-                if header["height"] > 1:
-                    carry.append({"x": x, "height": header["height"] - 1})
-                x = x + header["width"]
-            while carry and carry[0]["x"] == x:
-                cell = carry.popleft()
-                for i in range(nbr_measures):
-                    worksheet.write(y, x + i, "", header_plain)
-                if cell["height"] > 1:
-                    carry.append({"x": x, "height": cell["height"] - 1})
-                x = x + nbr_measures
-            x, y = 1, y + 1
-
-        # Step 2: measure row
-        if nbr_measures > 1:
-            worksheet.write(y, 0, "", header_plain)
-            for measure in jdata["measure_row"]:
-                style = header_bold if measure["is_bold"] else header_plain
-                worksheet.write(y, x, measure["measure"], style)
-                x = x + 1
-            y = y + 1
-
-        # Step 3: writing data
-        x = 0
-        for row in jdata["rows"]:
-            worksheet.write(
-                y, x, row["indent"] * "     " + ustr(row["title"]), header_plain
-            )
-            for cell in row["values"]:
-                x = x + 1
-                if cell.get("is_bold", False):
-                    worksheet.write(y, x, cell["value"], bold)
-                else:
-                    worksheet.write(y, x, cell["value"])
-            x, y = 0, y + 1
-        content = BytesIO()
-        workbook.save(content)
-        content.seek(0)  # Set index to 0, and start reading
-        out_file = base64.encodestring(content.read())
-        return out_file
-
-    def get_default_measure(self, force=False):
-        args = [("model", "=", "report.pos.order"), ("store", "=", True)]
-        if force:
-            args.append(("name", "=", "order_id"))
-        else:
-            args.append(("name", "in", ("order_id", "price_wo_tax")))
-        field_names = self.env["ir.model.fields"].search(args)
-        return field_names
-
-    def export_report(self):
+    def action_export_xlsx(self):
         self.ensure_one()
-        if not self.measure_ids:
-            self.measure_ids = self.get_default_measure(True)
-        args = self.domain and safe_eval(self.domain, self._get_eval_context()) or []
-        groupby = []
-        if self.horizontal_axis_id:
-            groupby.append(self.horizontal_axis_id.name)
-            if self.horizontal_axis_id in self.measure_ids:
-                self.measure_ids -= self.horizontal_axis_id
+        domain = (
+            self.domain
+            and safe_eval.safe_eval(self.domain, self._get_eval_context())
+            or []
+        )
+        model_name = "report.pos.order"
+        Model = self.env[model_name]
+
+        # --- Prepare groupbys and measures
+        groupbys = []
         if self.vertical_axis_id:
-            groupby.append(self.vertical_axis_id.name)
-            if self.vertical_axis_id in self.measure_ids:
-                self.measure_ids -= self.vertical_axis_id
-        field_list = []
+            groupbys.append(self.vertical_axis_id.name)
+        if self.horizontal_axis_id:
+            groupbys.append(self.horizontal_axis_id.name)
+
+        measures = [f.name for f in self.measure_ids]
+
+        # Identify non-summable fields → replace with count
+        valid_sum_types = ("integer", "float", "monetary")
+        computed_measures = []
+        measures = []
+        measure_labels = {}
         for m in self.measure_ids:
-            val = m.name
-            if val == "order_id":
-                val = "order_id:count_distinct"
-            field_list.append(val)
-        if not field_list:
-            pass
-            # field_list = ["order_id:count_distinct"]
-        raw_data = self.env["report.pos.order"].read_group(
-            args, field_list, groupby, lazy=False
-        )
-        datas = self.prepared_data(raw_data)
-        try:
-            self.output_file = self.export_xls(datas)
-        except Exception as e:
-            _logger.error(
-                "Error when exporting the excel file: %s",
-                str(e),
-            )
-            raise UserError(
-                _(
-                    "There are too many values for the Horizontal Axis which "
-                    "cause the number of column over the limit (256), "
-                    "please use Vertical Axis instead."
-                )
-            )
-        url = "{}/web/content/{}/{}/{}/{}?download=true".format(
-            self.env["ir.config_parameter"].sudo().get_param("web.base.url"),
-            self._name,
-            self.id,
-            "output_file",
-            "table.xls",
-        )
+            field_type = m.ttype
+            measure_name = f"{m.name}:sum"
+            if field_type not in valid_sum_types:
+                # Non-numeric → use count instead
+                measure_name = f"{m.name}:count_distinct"
+            computed_measures.append(m.name)
+            measure_labels[m.name] = self.env._(m.field_description)
+            measures.append(measure_name)
+
+        # --- Read data
+        results = Model.read_group(domain, measures, groupbys, lazy=False)
+        datas = self.prepared_data(results)
+        jdata = {
+            "title": self.env._("Point of Sale Analysis"),
+            "model": model_name,
+            "col_group_headers": datas.get("headers", []),
+            "measure_headers": datas.get("measure_row", []),
+            "origin_headers": [],
+            "rows": datas.get("rows", []),
+            "measure_count": datas.get("nbr_measures", 0),
+            "origin_count": 1,
+        }
+
+        base_url = self.env["ir.config_parameter"].sudo().get_param("web.base.url")
+        url = f"{base_url}/web/pivot/export_xlsx?data={json.dumps(jdata)}"
+
         return {
             "type": "ir.actions.act_url",
             "url": url,
