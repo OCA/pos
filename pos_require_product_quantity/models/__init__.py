a1cb757a7a6de04b0ef272d962e108f6ced6a3eb
Simon Hick <sim@coopiteasy.be>
2025-11-12 11:12:36 +0100

    [MIG] pos_require_product_quantity: Migration to 16.0

--- pos_require_product_quantity/models/__init__.py
+++ pos_require_product_quantity/models/__init__.py
@@ -1,1 +1,2 @@
 from . import pos_config
+from . import res_config_settings
--- pos_require_product_quantity/models/pos_config.py
+++ pos_require_product_quantity/models/pos_config.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2025 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 from odoo import fields, models
 
 
@@ -6,5 +10,5 @@ class PosConfig(models.Model):
 
     require_product_quantity = fields.Boolean(
         string="Require product quantity in POS",
-        default=False,
+        default=True,
     )
