cf2871a4536e4964db2166920c9ee838a443ce02
Mitul Dave <mitul.dave@chef-pixel.com>
2026-04-16 11:05:33 +0530

    [19.0][ADD] New module pos_payment_force_done_confirm

--- pos_payment_force_done_confirm/__manifest__.py
+++ pos_payment_force_done_confirm/__manifest__.py
@@ -1,13 +1,13 @@
-# -*- coding: utf-8 -*-
+# Copyright (C) 2023 - Today: GRAP (http://www.grap.coop)
 # Copyright 2026 CHEF PIXEL
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "POS Payment Force Done Confirmation",
     "version": "19.0.0.0",
     "category": "Point of Sale",
     "summary": "Ask for confirmation before marking a terminal payment as done.",
-    "author": "CHEF PIXEL",
+    "author": "CHEF PIXEL, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/pos",
     "license": "LGPL-3",
     "depends": ["point_of_sale"],
