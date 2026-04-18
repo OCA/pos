71b2d977307198bd7541641ee2e1fa0f3ca68cd4
cristina-hidalgo-tecnativa <cristina.hidalgo@tecnativa.com>
2026-04-18 13:59:04 +0200

    [FIX] pos_product_display_default_code: skip opening control in test tour

--- pos_product_display_default_code/static/tests/tours/product_screen_tour.esm.js
+++ pos_product_display_default_code/static/tests/tours/product_screen_tour.esm.js
@@ -1,5 +1,4 @@
 import * as Chrome from "@point_of_sale/../tests/tours/utils/chrome_util";
-import * as Dialog from "@point_of_sale/../tests/tours/utils/dialog_util";
 import * as ProductScreen from "@point_of_sale/../tests/tours/utils/product_screen_util";
 import {registry} from "@web/core/registry";
 
@@ -7,7 +6,6 @@ registry.category("web_tour.tours").add("SearchProductByDefaultCode", {
     steps: () =>
         [
             Chrome.startPoS(),
-            Dialog.confirm("Open Register"),
             ProductScreen.searchProduct("chair"),
             ProductScreen.clickDisplayedProduct("[CHAIR_01] Test sofa"),
             Chrome.endTour(),
