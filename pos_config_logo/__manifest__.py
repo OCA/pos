25026566a273fa319e7b9750176067b22f3db3c5
stferraro <gerferr83@gmail.com>
2026-04-23 12:44:56 -0400

    [MIG] pos_config_logo: Migration to 19.0

--- pos_config_logo/__manifest__.py
+++ pos_config_logo/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Point of sale logo",
     "summary": "Set logotypes different from the company's one",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Point of Sale",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/pos",
