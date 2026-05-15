794505bea823af92a038969ccee8078f9b76ede8
phucph <phucph@trobz.com>
2026-05-15 15:30:20 +0700

    [ADD] pos_report_session_summary_account

---
+++ pos_report_session_summary_account/__manifest__.py
@@ -0,0 +1,18 @@
+# copyright Odoo SA
+# copyright OCA
+# Licence : LGPL-3
+
+{
+    "name": "POS report Session Summary Account",
+    "version": "18.0.1.0.0",
+    "category": "Point Of Sale",
+    "summary": "Add column account in report",
+    "author": "Akretion, Odoo SA, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "depends": ["pos_report_session_summary"],
+    "data": ["views/report_session_summary.xml"],
+    "installable": True,
+    "application": False,
+    "website": "https://github.com/OCA/pos",
+    "auto_install": False,
+}
