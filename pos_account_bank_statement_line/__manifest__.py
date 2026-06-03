e535f5dd236c5d1eeee69655cfb6ba5138eb4e3d
Quentin Dupont <quentin.dupont@grap.coop>
2026-06-03 14:32:09 +0200

    [16.0][ADD] pos_account_bank_statement_line

---
+++ pos_account_bank_statement_line/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2026 - Today: GRAP (http://www.grap.coop)
+# @author: Quentin DUPONT (quentin.dupont@grap.coop)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "PoS Account Bank Statement Line",
+    "version": "16.0.1.0.0",
+    "category": "Point of Sale",
+    "website": "https://github.com/OCA/pos",
+    "license": "AGPL-3",
+    "author": "GRAP,Odoo Community Association (OCA)",
+    "depends": ["point_of_sale"],
+    "data": [
+        "views/account_bank_statement_line.xml",
+        "views/menu.xml",
+    ],
+}
---
+++ pos_account_bank_statement_line/readme/DESCRIPTION.rst
@@ -0,0 +1,7 @@
+This module creates a decent view for account_bank_statement_line
+
+.. figure:: ../static/description/pos_account_bank_statement_line.png
+
+And a menu to get all PoS account_bank_statement_line
+
+.. figure:: ../static/description/pos_account_bank_statement_line_menu.png
