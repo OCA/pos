cbcafa815261477f98ba5109b2330181ef28e973
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-07-21 15:39:07 +0200

    [FIX] pos_cash_move_reason: avoid error if user misconfigured cash move reason. (the journal entry must always have exactly one journal item involving the bank/cash account)

--- pos_cash_move_reason/models/pos_move_reason.py
+++ pos_cash_move_reason/models/pos_move_reason.py
@@ -2,7 +2,8 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class PosMoveReason(models.Model):
@@ -43,3 +44,29 @@ class PosMoveReason(models.Model):
         default=lambda x: x._default_company_id(),
         required=True,
     )
+
+    @api.constrains("journal_ids", "income_account_id", "expense_account_id")
+    def _check_accounts(self):
+        for journal in self.journal_ids:
+            if self.income_account_id == journal.default_account_id:
+                raise UserError(
+                    _(
+                        "You can't set as an income account"
+                        " the account %(account_code)s - %(account_name)s"
+                        " as it is the default account of the journal %(journal_name)s.",
+                        account_code=self.income_account_id.code,
+                        account_name=self.income_account_id.name,
+                        journal_name=journal.name,
+                    )
+                )
+            if self.expense_account_id == journal.default_account_id:
+                raise UserError(
+                    _(
+                        "You can't set as an expense account"
+                        " the account %(account_code)s - %(account_name)s"
+                        " as it is the default account of the journal %(journal_name)s.",
+                        account_code=self.expense_account_id.code,
+                        account_name=self.expense_account_id.name,
+                        journal_name=journal.name,
+                    )
+                )
