9fa1fa798a78a9191ef12daeba9e2fc29c8119ca
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-07-21 14:45:05 +0200

    [IMP] pos_cash_move_reason: allow inheritance

--- pos_cash_move_reason/models/pos_move_reason.py
+++ pos_cash_move_reason/models/pos_move_reason.py
@@ -44,7 +44,7 @@ class PosMoveReason(models.Model):
     company_id = fields.Many2one(
         string="Company",
         comodel_name="res.company",
-        default=_default_company_id,
+        default=lambda x: x._default_company_id(),
         required=True,
     )
 
